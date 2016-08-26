7bc506f6efe0a1a56df2d0bea975d3d1954c2032
M1rotvorez <vokik91@gmail.com>
2016-08-26 19:53:34 +0600

    cleanup code

--- assets/js/twin_conf.js
+++ assets/js/twin_conf.js
@@ -1,40 +1,3 @@
-/**
- * Created by M1rotvorez on 12.08.16.
- */
-
-var $hand = $('#hand');
-var $container = $('.poster');
-
-$container.on('mousemove', function(e) {
-		var w = $(window).width() , //window width
-				h = $(window).height(), //window height
-				offsetX = 0.5 - e.pageX / w, //cursor position X
-				offsetY = 0.5 - e.pageY / h; //cursor position Y
-		$hand.each(function() {
-				var $this = $(this),
-						offsetLayer = 140,
-						transformLayer = 'rotateY('+ (offsetX * offsetLayer) +'deg) rotateX('+ (offsetY * 30) +'deg)' ;
-				$this.css({
-								'transform' : transformLayer,
-								'transition-duration': '.1s'
-						}
-				);
-		});
-});
-
-$container.mouseout(function () {
-		$hand.css({
-				'transform': 'rotateY(0deg) rotateX(0deg)',
-				'transition-duration': '.7s'
-		})
-});
-
-$container.mouseenter(function () {
-		$hand.css({
-				'transition-duration': '.9s'
-		})
-});
-
 var tmax_options = {
 		delay: 0,
 		paused: false,
@@ -84,8 +47,6 @@ var tmax_options = {
 		onRepeatParams: []
 };
 
-
-
 var tl = new TimelineMax(tmax_options),
 		drow_words = new TimelineMax(tmax_options),
 		path = document.getElementById("han_d"),
@@ -95,9 +56,6 @@ var tl = new TimelineMax(tmax_options),
 tl.to(path, 2, {css:{'stroke-dashoffset': 0},	ease: Power2.easeInOut})
 	.to(git, .5, {css:{'fill-opacity': 1}})
 	.to(path, 1, {css:{'fill-opacity': 1}});
-
-
-
 
 drow_words.staggerTo(".word_drow", 2, {css:{'stroke-dashoffset': 0}, ease: Power2.easeInOut})
 					.to(insta, .5, {css:{'fill-opacity': 1}})
