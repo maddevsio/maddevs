8adf1e979b0f91f70d3124b91bfb2657b9ef26fa
M1rotvorez <volkik91@gmail.com>
2016-10-27 17:16:21 +0600

    add twitter logo in footer with link (no-follow, _blank)

--- assets/js/twin_conf.js
+++ assets/js/twin_conf.js
@@ -5,7 +5,7 @@ var tmax_options = {
 				console.log('animation is complete');
 		},
 		onCompleteScope: function() {
-				console.log('animation scope is complete');
+				console.log('animation sco0pe is complete');
 		},
 		tweens: [],
 		stagger: 0,
@@ -47,18 +47,51 @@ var tmax_options = {
 		onRepeatParams: []
 };
 
+var tmax_options_icons = {
+		delay: 2,
+		paused: false,
+		tweens: [],
+		stagger: 0,
+		align: 'normal',
+		useFrames: false,
+		autoRemoveChildren: false,
+		smoothChildTiming: false,
+		repeat: 0,
+		repeatDelay: 0,
+		yoyo: false,
+		onCompleteParams: [],
+		onReverseCompleteParams: [],
+		onStartParams: [],
+		onUpdateParams: [],
+		onRepeatParams: []
+};
+
 var tl = new TimelineMax(tmax_options),
 		drow_words = new TimelineMax(tmax_options),
+
+		icons_animate_git = new TimelineMax (tmax_options_icons),
+		icons_animate_twitter = new TimelineMax (tmax_options_icons),
+		icons_animate_inst = new TimelineMax (tmax_options_icons),
+
 		path = document.getElementById("han_d"),
 		insta = document.getElementById("instagram_stroke"),
-		git = document.getElementById("git_stroke");
+		git = document.getElementById("git_stroke"),
+		twitter = document.getElementById("twitter_stroke");
 
-tl.to(path, 2, {css:{'stroke-dashoffset': 0},	ease: Power2.easeInOut})
-	.to(git, .5, {css:{'fill-opacity': 1}})
-	.to(path, 1, {css:{'fill-opacity': 1}});
+tl
+		.to(path, 2, {css:{'stroke-dashoffset': 0},	ease: Power2.easeInOut})
+		.to(path, 1, {css:{'fill-opacity': 1}});
 
-drow_words.staggerTo(".word_drow", 2, {css:{'stroke-dashoffset': 0}, ease: Power2.easeInOut})
-					.to(insta, .5, {css:{'fill-opacity': 1}})
-					.staggerTo(".word_drow", 1 ,{css:{'fill-opacity': 1}});
+drow_words
+		.staggerTo(".word_drow", 2, {css:{'stroke-dashoffset': 0}, ease: Power2.easeInOut})
+		.staggerTo(".word_drow", 1 ,{css:{'fill-opacity': 1}});
 
+icons_animate_inst
+		.to(insta, .5, {css:{'fill-opacity': 1}});
+
+icons_animate_git
+		.to(git, .5, {css:{'fill-opacity': 1}});
+
+icons_animate_twitter
+		.to(twitter, .5, {css:{'fill-opacity': 1}});
 
