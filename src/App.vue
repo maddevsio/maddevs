0cee4349517fee6f60adf05a9ab4ed6a4b9a1abd
iamDenisoed <denisod93@gmail.com>
2018-05-30 12:01:03 +0600

    Added started files with style for first block

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+    <!-- <Footer /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  @import "./assets/styles/index.css";
+</style>
