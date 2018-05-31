7bf064bf3d154af98d616536ba00c80f74da20ad
iamDenisoed <denisod93@gmail.com>
2018-05-31 15:22:50 +0600

    MD-340 added basic style for Best Practices section

---
+++ src/components/Home/bestPractices.vue
@@ -0,0 +1,34 @@
+<template>
+  <section id="best-practices" class="best-practices">
+    <div class="container">
+        <div class="best-practices_wrap">
+            <div class="best-practices_wrap-preview">
+                <div class="best-practices_wrap-preview_header">
+                    <div class="p-close"></div>
+                    <div class="p-hide"></div>
+                    <div class="p-full"></div>
+                </div>
+            </div>
+            <div class="best-practices_wrap-title">
+                <h3>
+                    Мы применяем лучшие <span>практики разработки</span>, а также несем ответственность за <span style="color: #a1d9ff;">качество</span> и <span style="color: #e8b8ff;">стабильность</span> работы продукта
+                </h3>
+            </div>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'best-practices',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
