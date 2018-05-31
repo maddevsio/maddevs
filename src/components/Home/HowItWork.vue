2bea53fb241be66a6391415eca97cb3c497bd59c
iamDenisoed <denisod93@gmail.com>
2018-05-31 13:54:49 +0600

    MD-340 added styles for "How do we do this?"

--- src/components/Home/HowItWork.vue
+++ src/components/Home/HowItWork.vue
@@ -1,11 +1,26 @@
 <template>
   <section id="how-it-work" class="how-it-work">
-      <div class="container">
-        <div class="how-it-work_wrap">
-          <h2>Как мы это делаем?</h2>
-          <p>Мы верим в полную прозрачность работы, профессионализм и в наличие четких процессов</p>
+    <div class="how-it-work_wrap">
+      <h2>Как мы это делаем?</h2>
+      <p>Мы верим в полную прозрачность работы, профессионализм и в наличие четких процессов</p>
+      <div class="how-it-work_wrap-content">
+        <div class="container">
+          <h3 class="how-it-work_wrap-content-title">
+            Посмотрите как мы подходим к работе с  <span>командами</span> , как мы занимается их <span>менеджментом</span> , и как <span>коммуницируем</span> внутри и с клиентами
+          </h3>
+          <div class="how-it-work_wrap-content-slider">
+            <div class="how-it-work_wrap-content-slider_header">
+              <div class="s-close"></div>
+              <div class="s-hide"></div>
+              <div class="s-full"></div>
+            </div>
+            <div class="how-it-work_wrap-content-slides">
+
+            </div>
+          </div>
         </div>
       </div>
+    </div>
   </section>
 </template>
 
