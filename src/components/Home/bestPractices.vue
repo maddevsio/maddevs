d69c7fb0a76938325c485e4df677b24ca48082e5
iamDenisoed <denisod93@gmail.com>
2018-06-01 12:48:46 +0600

    MD-342  add styles for block with line number

--- src/components/Home/bestPractices.vue
+++ src/components/Home/bestPractices.vue
@@ -10,9 +10,19 @@
                 </div>
             </div>
             <div class="best-practices_wrap-title">
-                <h3>
-                    Мы применяем лучшие <span>практики разработки</span>, а также несем ответственность за <span style="color: #a1d9ff;">качество</span> и <span style="color: #e8b8ff;">стабильность</span> работы продукта
-                </h3>
+                <p>0 func (r *REST) addCommand (c echo.Context,) {</p>
+                <div class="line-number_text">
+                    <div class="line-number">
+                        <span>1</span>
+                        <span>2</span>
+                        <span>3</span>
+                        <span>4</span>
+                        <span class="last-child">5</span>
+                    </div>
+                    <h3>
+                        Мы применяем лучшие <span>практики разработки</span>, а также несем ответственность за <span style="color: #a1d9ff;">качество</span> и <span style="color: #e8b8ff;">стабильность</span> работы продукта
+                    </h3>
+                </div>
             </div>
         </div>
     </div>
