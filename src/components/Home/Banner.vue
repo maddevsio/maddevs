afcecfde0053f46101101bc118eaed2f8d9dc775
iamDenisoed <denisod93@gmail.com>
2018-06-05 09:47:48 +0600

    MD-345 added script for set full screen iframe video

--- src/components/Home/Banner.vue
+++ src/components/Home/Banner.vue
@@ -1,13 +1,9 @@
 <template>
   <section id="banner" class="banner">
     <div class="video-background">
-      <iframe id="bgVideo"
-        src="https://www.youtube.com/embed/lmGJTVyOfYM?loop=1&autoplay=1&mute=1&enablejsapi=1&controls=0&showinfo=0"
-        frameborder="0"
-        allowfullscreen="true"
-        webkitallowfullscreen="true"
-        mozallowfullscreen="true"
-      ></iframe>
+      <div class="tv">
+          <div class="screen mute" id="tv"></div>
+      </div>
     </div>
     <div class="container">
       <div class="banner-wrap">
@@ -181,9 +177,60 @@ export default {
         console.log(res)
       })
     },
+    setBgVideo() {
+      function vidRescale(){
+        var w = $(window).width()+200,
+          h = $(window).height()+200;
+
+        if (w/h > 16/9){
+          tv.setSize(w, w/16*9);
+          $('.tv .screen').css({'left': '0px'});
+        } else {
+          tv.setSize(h/9*16, h);
+          $('.tv .screen').css({'left': -($('.tv .screen').outerWidth()-w)/2});
+        }
+      }
+
+      $(window).on('load resize', function(){
+        vidRescale();
+      });
+
+      function onPlayerReady(){
+        tv.loadVideoById(vid[currVid]);
+        tv.mute();
+      }
+
+      function onPlayerStateChange(e) {
+        if (e.data === 1){
+          $('#tv').addClass('active');
+        } else if (e.data === 2){
+          $('#tv').removeClass('active');
+          if(currVid === vid.length - 1){
+            currVid = 0;
+          } else {
+            currVid++;  
+          }
+          tv.loadVideoById(vid[currVid]);
+          tv.seekTo(vid[currVid].startSeconds);
+        }
+      }
+      var vid = [
+        {'videoId': 'lmGJTVyOfYM', 'startSeconds': 0, 'suggestedQuality': 'hd720'},
+       /*  {'videoId': 'lmGJTVyOfYM', 'startSeconds': 465, 'endSeconds': 657, 'suggestedQuality': 'hd720'},
+        {'videoId': 'lmGJTVyOfYM', 'startSeconds': 0, 'endSeconds': 240, 'suggestedQuality': 'hd720'},
+        {'videoId': 'lmGJTVyOfYM', 'startSeconds': 19, 'endSeconds': 241, 'suggestedQuality': 'hd720'} */
+      ],
+      randomVid = Math.floor(Math.random() * vid.length),
+      currVid = randomVid;
+
+      var tv, playerDefaults = {autoplay: 1, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, disablekb: 1, enablejsapi: 0, iv_load_policy: 3};
+      // var tv, playerDefaults = {autoplay: 1, loop: 1};
+      tv = new YT.Player('tv', {events: {'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange}, playerVars: playerDefaults});
+    }
   },
   mounted () {
-    db.loadDatabase()
+    db.loadDatabase();
+    this.setBgVideo();
   }
 }
 </script>
