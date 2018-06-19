63136a98513a3d468fc02816208ce9f7d268fe1a
iamDenisoed <denisod93@gmail.com>
2018-06-19 11:40:35 +0600

    MD-359 added html markup, content and styles for footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,60 @@
 <template>
-  <footer id="footer">
-    <h1>Footer</h1>
+  <footer id="footer" class="footer">
+    <div class="container">
+
+      <div class="footer-wrap">
+        <div class="footer-wrap_list">
+          <div class="footer-wrap_list-links">
+            <h5>Услуги</h5>
+            <a href="#">Разработка и тестирование</a>
+            <a href="#">DevOps</a>
+            <a href="#">Мобильные приложения</a>
+            <a href="#">UI/UX дизайн</a>
+            <a href="#">Управление проектами</a>
+            <a href="#">ИТ консалтинг</a>
+          </div>
+          <div class="footer-wrap_list-links">
+            <h5>Портфолио</h5>
+            <a href="#">Проекты</a>
+            <a href="#">Партнеры</a>
+            <a href="#">Success stores</a>
+            <a href="#">Домашние проекты Open source</a>
+          </div>
+          <div class="footer-wrap_list-links">
+            <h5>Блог</h5>
+            <a href="#">Publications</a>
+            <a href="#">Projects</a>
+            <a href="#">Dev Tips & Tricks</a>
+            <a href="#">Events</a>
+            <a href="#">Career</a>
+          </div>
+          <div class="footer-wrap_list-links">
+            <h5>о нас</h5>
+            <a href="#">Ценности</a>
+            <a href="#">Видение</a>
+            <a href="#">Миссия</a>
+            <a href="#">Процессы</a>
+            <a href="#">Команда</a>
+          </div>
+          <div class="footer-wrap_list-links">
+            <h5>как работаем</h5>
+            <a href="#">Менеджмент команд</a>
+            <a href="#">Коммуникация</a>
+            <a href="#">Практики разработки</a>
+            <a href="#">Используемые технологии</a>
+          </div>
+          <div class="footer-wrap_list-links">
+            <h5>вакансии</h5>
+            <a href="#">Открытые вакансии</a>
+            <a href="#">Стажировка</a>
+            <a href="#">Ищем таланты</a>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <p>© Mad Devs 🤘 2018</p>
+      </div>
+    </div>
   </footer>
 </template>
 
