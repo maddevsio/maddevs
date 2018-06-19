a59a3f0d501a71b0adc9fb5db28a4f388b887179
iamDenisoed <denisod93@gmail.com>
2018-06-19 12:27:13 +0600

    MD-360 added html markup/styles/content for contacts section

---
+++ src/components/Home/Contacts.vue
@@ -0,0 +1,64 @@
+<template>
+  <section id="contacts" class="contacts">
+    <div class="container">
+      <div class="contacts-wrap">
+        <h2 class="sec-title">Контакты</h2>
+        <div class="contacts-wrap_list">
+            <div class="contacts-wrap_list-item">
+                <h5>адрес</h5>
+                <p>Bishkek, Ibraimova, 103, Business <br> center Victory, 5th floor</p>
+            </div>
+            <div class="contacts-wrap_list-item phone-mail">
+                <h5>Связаться</h5>
+                <a href="tel:+996 555 429 055">+996 555 429 055</a>
+                <a href="mailto:rock@maddevs.io">rock@maddevs.io</a>
+            </div>
+            <div class="contacts-wrap_list-item social-icons">
+                <h5>Соц. Сети</h5>
+                <a href="https://github.com/maddevsio">
+                    <img src="../../assets/img/svg/footer/git-copy.svg" alt="Github">
+                </a>
+                <a href="https://www.facebook.com/maddevsio">
+                    <img src="../../assets/img/svg/footer/fb-copy.svg" alt="Facebook">
+                </a>
+                <a href="https://www.instagram.com/maddevsio/">
+                    <img src="../../assets/img/svg/footer/insta-copy.svg" alt="Instagram">
+                </a>
+                <a href="https://blog.maddevs.io/">
+                    <img src="../../assets/img/svg/footer/medium-copy.svg" alt="Medium">
+                </a>
+                <a href="https://twitter.com/MadDevsIO">
+                    <img src="../../assets/img/svg/footer/tw-copy.svg" alt="Twitter">
+                </a>
+                <a href="https://www.slideshare.net/maddevs/presentations">
+                    <img src="../../assets/img/svg/footer/slide-copy.svg" alt="Slideshare">
+                </a>
+                <a href="https://www.youtube.com/playlist?list=PLsmdb5W8ytypyXt1ut3lfBOnOZnDNqYIN">
+                    <img src="../../assets/img/svg/footer/yt-copy-8.svg" alt="Youtube">
+                </a>
+                <a href="https://www.behance.net/maddevs">
+                    <img src="../../assets/img/svg/footer/beh-copy.svg" alt="Behance">
+                </a>
+                <a href="https://t.me/maddevsio">
+                    <img src="../../assets/img/svg/footer/tel-copy.svg" alt="Telegram">
+                </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'contacts',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
