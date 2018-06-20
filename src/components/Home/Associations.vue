98acc2236b6dd6f5954752470137a0e41aafc4da
iamDenisoed <denisod93@gmail.com>
2018-06-20 11:40:36 +0600

    MD-361 added hml markup and styles for "Press", "Associations in which we belong"  section

---
+++ src/components/Home/Associations.vue
@@ -0,0 +1,28 @@
+<template>
+  <section id="associations" class="associations">
+    <div class="container">
+      <div class="associations-wrap">
+        <h3>Ассоциации в которых мы состоим</h3>
+        <div class="associations-wrap_list">
+          <img src="../../assets/img/svg/associations/pht.svg" alt="">
+          <img src="../../assets/img/svg/associations/kssda.svg" alt="">
+          <img src="../../assets/img/svg/associations/amcham.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'associations',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
---
+++ src/components/Home/Press.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="press" class="press">
+    <div class="container">
+      <div class="press-wrap">
+        <h2 class="sec-title">Пресса о нас</h2>
+        <img src="../../assets/img/svg/press/cnbc.svg" alt="">
+        <a href="">https://www.cnbc.com/2018/01/29/belt…</a>
+        <p>…globally successful companies were born in the capital city of Bishkek: <span>Mad Devs</span>, a <span>developer</span>  outsourcer that boasts clients from <span>Silicon Valley</span> to <span>Singapore</span>…</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'press',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
