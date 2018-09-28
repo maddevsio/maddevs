a5b751b9d237ad903d62a05e516aba9725c0efb0
Denisoed <denisod93@gmail.com>
2018-09-28 12:30:10 +0600

    MND-68 added images in carousel NowItWork

--- src/components/Home/HowItWork.vue
+++ src/components/Home/HowItWork.vue
@@ -10,9 +10,27 @@
           </h3>
           <div class="how-it-work_wrap-content-slider">
             <carousel :per-page="1" :paginationEnabled="false" mouse-drag:false class="how-it-work_wrap-content-slides">
-              <slide class="how-it-work_wrap-content-slide">Hello 1</slide>
-              <slide class="how-it-work_wrap-content-slide">Hello 2</slide>
-              <slide class="how-it-work_wrap-content-slide">Hello 3</slide>
+              <slide class="how-it-work_wrap-content-slide">
+                <picture>
+                    <source srcset="../../assets/img/png/how-it-work/hiw1.png"
+                            media="(max-width: 992px)">
+                    <img src="../../assets/img/png/how-it-work/hiw1@2x.png">
+                </picture>
+              </slide>
+              <slide class="how-it-work_wrap-content-slide">
+                <picture>
+                    <source srcset="../../assets/img/png/how-it-work/hiw2.png"
+                            media="(max-width: 992px)">
+                    <img src="../../assets/img/png/how-it-work/hiw2@2x.png">
+                </picture>
+              </slide>
+              <slide class="how-it-work_wrap-content-slide">
+                <picture>
+                    <source srcset="../../assets/img/png/how-it-work/hiw3.png"
+                            media="(max-width: 992px)">
+                    <img src="../../assets/img/png/how-it-work/hiw3@2x.png">
+                </picture>
+              </slide>
             </carousel>
           </div>
         </div>
