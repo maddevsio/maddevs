52025a05a16836b9eec3d0e3241e2479431a12d1
Anton Grushkin <merynes345@gmail.com>
2018-09-30 23:40:37 +0600

    MND-75 added new icons for Our partners

--- src/components/Home/Partners.vue
+++ src/components/Home/Partners.vue
@@ -6,50 +6,107 @@
         <div class="partners_wrap-icons">
           <!-- Row -->
           <div class="partners_wrap-icons_row">
-            <a href="#" class="item-left">
+            <a class="item-left">
               <img src="../../assets/img/svg/partners/veeqo-logo.svg" alt="">
             </a>
-            <a href="#">
+            <a>
               <img src="../../assets/img/svg/partners/zentist.svg" alt="">
             </a>
-            <a href="#">
+            <a>
               <img src="../../assets/img/svg/partners/localize.svg" alt="">
             </a>
-            <a href="#">
+            <a>
               <img src="../../assets/img/svg/partners/eatigo.svg" alt="">
             </a>
-            <a href="#" class="item-right scentomania">
+            <a class="item-right scentomania">
               <img src="../../assets/img/svg/partners/scento.svg" alt="">
             </a>
-            <a href="#" class="item-left">
+            <a class="item-left">
               <img src="../../assets/img/svg/partners/namba.svg" alt="">
             </a>
-            <a href="#">
+            <a>
               <img src="../../assets/img/svg/partners/yourcast.svg" alt="">
             </a>
-            <a href="#">
+            <a>
               <img src="../../assets/img/svg/partners/iotsploit-copy.svg" alt="">
             </a>
-            <a href="#">
+            <a>
               <img src="../../assets/img/svg/partners/kaf.svg" alt="">
             </a>
-            <a href="#" class="item-right">
+            <a class="item-right">
               <img src="../../assets/img/svg/partners/namba-taxi.svg" alt="">
             </a>
-            <a href="#" class="partner-icon-fake">
-              <!-- fake -->
-            </a>
-            <a href="#">
+            <a class="item-left">
               <img src="../../assets/img/svg/partners/nambafood.svg" alt="">
             </a>
-            <a href="#">
+            <a>
               <img src="../../assets/img/svg/partners/silkroadexplore.svg" alt="">
             </a>
-            <a href="#" class="last-icon">
+            <a class="last-icon">
               <img src="../../assets/img/svg/partners/besmart.svg" alt="">
             </a>
-            <a href="#" class="partner-icon-fake">
-              <!-- fake -->
+            <a>
+              <img src="../../assets/img/svg/partners/kickico.svg" alt="">
+            </a>
+            <a class="item-right">
+              <img src="../../assets/img/svg/partners/neureal.svg" alt="">
+            </a>
+            <a class="item-left">
+              <img src="../../assets/img/svg/partners/teacherly-copy.svg" alt="">
+            </a>
+            <a>
+              <img src="../../assets/img/svg/partners/terbine.svg" alt="">
+            </a>
+            <a>
+              <img src="../../assets/img/svg/partners/zofice-copy.svg" alt="">
+            </a>
+            <a>
+              <img src="../../assets/img/svg/partners/idmatch.svg" alt="">
+            </a>
+            <a class="item-right">
+              <img src="../../assets/img/svg/partners/inn-4-pet.svg" alt="">
+            </a>
+            <a class="item-left tengri">
+              <img src="../../assets/img/svg/partners/tengri.svg" alt="">
+            </a>
+            <a>
+              <img src="../../assets/img/svg/partners/admitad.svg" alt="">
+            </a>
+            <a>
+              <img src="../../assets/img/svg/partners/elcart.svg" alt="">
+            </a>
+            <a>
+              <img src="../../assets/img/svg/partners/triggermesh.svg" alt="">
+            </a>
+            <a class="item-right">
+              <img src="../../assets/img/svg/partners/smk.svg" alt="">
+            </a>
+            <a class="item-left">
+              <img src="../../assets/img/svg/partners/mythril-platform-0.svg" alt="">
+            </a>
+             <a>
+              <img src="../../assets/img/svg/partners/smb.svg" alt="">
+            </a>
+             <a>
+              <img src="../../assets/img/svg/partners/diesel.svg" alt="">
+            </a>
+             <a>
+              <img src="../../assets/img/png/partners/lakediamond.png" alt="">
+            </a>
+            <a class="item-right">
+              <img src="../../assets/img/png/partners/nappy-club.png" alt="">
+            </a>
+            <a class="item-left">
+              <img src="../../assets/img/png/partners/nuntiux.png" alt="">
+            </a>
+             <a class="photobook">
+              <img src="../../assets/img/png/partners/photobook.png" alt="">
+            </a>
+            <a>
+              <img src="../../assets/img/png/partners/pickd.png" alt="">
+            </a>
+            <a class="item-right">
+              <img src="../../assets/img/png/partners/socialshopwave.png" alt="">
             </a>
           </div>
         </div>
