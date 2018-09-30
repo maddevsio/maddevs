0bfc296fd03b4b1ef4ad4172a2eb5c02465ddeec
Denisoed <denisod93@gmail.com>
2018-09-30 18:39:18 +0600

    MND-23 added second block for section Press

--- src/components/Home/Press.vue
+++ src/components/Home/Press.vue
@@ -1,11 +1,25 @@
 <template>
   <section id="press" class="press">
     <div class="container">
+      <h2 class="sec-title">{{$t('Press about us')}}</h2>
       <div class="press-wrap">
-        <h2 class="sec-title">{{$t('Press about us')}}</h2>
-        <img src="../../assets/img/svg/press/cnbc.svg" alt="">
-        <a href="">https://www.cnbc.com/2018/01/29/belt…</a>
-        <p>…globally successful companies were born in the capital city of Bishkek: <span>Mad Devs</span>, a <span>developer</span>  outsourcer that boasts clients from <span>Silicon Valley</span> to <span>Singapore</span>…</p>
+
+        <div class="press-wrap_item">
+          <div class="press-wrap_item_img">
+            <img src="../../assets/img/svg/press/cnbc.svg" alt="">
+          </div>
+          <a href="">https://www.cnbc.com/2018/01/29/belt…</a>
+          <p>…globally successful companies were born in the capital city of Bishkek: <span>Mad Devs</span>, a <span>developer</span>  outsourcer that boasts clients from <span>Silicon Valley</span> to <span>Singapore</span>…</p>
+        </div>
+        
+        <div class="press-wrap_item">
+          <div class="press-wrap_item_img">
+            <img src="../../assets/img/svg/press/ozy.svg" alt="">
+          </div>
+          <a href="">https://www.ozy.com/rising-stars/this-unlikely-tech-queen…</a>
+          <p>This unlikely <span>tech queen</span> wants to build a <span>new global hub in Kyrgyzstan</span>. Because behind every great app there’s a great <span>team of back-end developers…</span></p>
+        </div>
+
       </div>
     </div>
   </section>
