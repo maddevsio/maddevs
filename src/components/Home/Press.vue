55b350e74454f55a759d61c8013b040d0a3f380f
Denisoed <denisod93@gmail.com>
2018-09-30 18:53:51 +0600

    MND-23 added styles for mobile screen

--- src/components/Home/Press.vue
+++ src/components/Home/Press.vue
@@ -3,7 +3,6 @@
     <div class="container">
       <h2 class="sec-title">{{$t('Press about us')}}</h2>
       <div class="press-wrap">
-
         <div class="press-wrap_item">
           <div class="press-wrap_item_img">
             <img src="../../assets/img/svg/press/cnbc.svg" alt="">
@@ -11,7 +10,6 @@
           <a href="">https://www.cnbc.com/2018/01/29/belt…</a>
           <p>…globally successful companies were born in the capital city of Bishkek: <span>Mad Devs</span>, a <span>developer</span>  outsourcer that boasts clients from <span>Silicon Valley</span> to <span>Singapore</span>…</p>
         </div>
-        
         <div class="press-wrap_item">
           <div class="press-wrap_item_img">
             <img src="../../assets/img/svg/press/ozy.svg" alt="">
@@ -19,7 +17,6 @@
           <a href="">https://www.ozy.com/rising-stars/this-unlikely-tech-queen…</a>
           <p>This unlikely <span>tech queen</span> wants to build a <span>new global hub in Kyrgyzstan</span>. Because behind every great app there’s a great <span>team of back-end developers…</span></p>
         </div>
-
       </div>
     </div>
   </section>
