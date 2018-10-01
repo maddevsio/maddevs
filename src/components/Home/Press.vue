b896b9fe4ba273e36c633bffac81a1567b756104
Denisoed <denisod93@gmail.com>
2018-10-01 10:05:16 +0600

    MND-75 added links for blocks in section Press

--- src/components/Home/Press.vue
+++ src/components/Home/Press.vue
@@ -3,20 +3,20 @@
     <div class="container">
       <h2 class="sec-title">{{$t('Press about us')}}</h2>
       <div class="press-wrap">
-        <div class="press-wrap_item">
+        <a href="https://www.google.com/amp/s/www.cnbc.com/amp/2018/01/29/belt-and-road-tech-entrepreneurship-in-kyrgyzstan.html" target="_blank" class="press-wrap_item">
           <div class="press-wrap_item_img">
             <img src="../../assets/img/svg/press/cnbc.svg" alt="">
           </div>
-          <a href="">https://www.cnbc.com/2018/01/29/belt…</a>
+          <a>https://www.cnbc.com/2018/01/29/belt…</a>
           <p>…globally successful companies were born in the capital city of Bishkek: <span>Mad Devs</span>, a <span>developer</span>  outsourcer that boasts clients from <span>Silicon Valley</span> to <span>Singapore</span>…</p>
-        </div>
-        <div class="press-wrap_item">
+        </a>
+        <a href="https://www.ozy.com/rising-stars/this-unlikely-tech-queen-wants-to-build-a-new-global-hub-in-kyrgyzstan/88948" target="_blank" class="press-wrap_item">
           <div class="press-wrap_item_img">
             <img src="../../assets/img/svg/press/ozy.svg" alt="">
           </div>
-          <a href="">https://www.ozy.com/rising-stars/this-unlikely-tech-queen…</a>
+          <a>https://www.ozy.com/rising-stars/this-unlikely-tech-queen…</a>
           <p>This unlikely <span>tech queen</span> wants to build a <span>new global hub in Kyrgyzstan</span>. Because behind every great app there’s a great <span>team of back-end developers…</span></p>
-        </div>
+        </a>
       </div>
     </div>
   </section>
