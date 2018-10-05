4144303167d85bf39b5e65aa26571531370a3efd
Denisoed <denisod93@gmail.com>
2018-10-05 12:59:38 +0600

    MND-85 changed sec title on desktop

--- src/components/Home/Associations.vue
+++ src/components/Home/Associations.vue
@@ -2,7 +2,7 @@
   <section id="associations" class="associations">
     <div class="container">
       <div class="associations-wrap">
-        <h3>{{$t('Membership in associations')}}</h3>
+        <h2 class="sec-title">{{$t('Membership in associations')}}</h2>
         <div class="associations-wrap_list">
           <div class="associations-wrap_list-item">
             <img src="../../assets/img/svg/associations/pht.svg" alt="">
--- src/components/Home/Reviews.vue
+++ src/components/Home/Reviews.vue
@@ -13,9 +13,10 @@
             mouse-drag:false
             autoplayHoverPause
             class="reviews_wrap-carousel"
+            ref="carousel"
         >
             <slide class="reviews_wrap-carousel-slide">
-                <p class="reviews_wrap-carousel-slide_desc">{{$t('Review-1 text')}}</p>
+                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-1 text')}}</p>
                 <div class="sender">
                     <img style="margin-bottom: -5px" src="../../assets/img/png/reviews/socialshopwave.png" alt="">
                     <div class="sender-name">
@@ -24,7 +25,7 @@
                 </div>
             </slide>
             <slide class="reviews_wrap-carousel-slide">
-                <p class="reviews_wrap-carousel-slide_desc">{{$t('Review-2 text')}}</p>
+                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-2 text')}}</p>
                 <div class="sender">
                     <img style="margin-bottom: -2px" src="../../assets/img/svg/reviews/teacherly.svg" alt="">
                     <div class="sender-name">
@@ -33,7 +34,7 @@
                 </div>
             </slide>
             <slide class="reviews_wrap-carousel-slide">
-                <p class="reviews_wrap-carousel-slide_desc">{{$t('Review-3 text')}}</p>
+                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-3 text')}}</p>
                 <div class="sender">
                     <img style="margin-top: -2px" src="../../assets/img/svg/reviews/veeqo.svg" alt="">
                     <div class="sender-name">
@@ -42,7 +43,7 @@
                 </div>
             </slide>
             <slide class="reviews_wrap-carousel-slide">
-                <p class="reviews_wrap-carousel-slide_desc">{{$t('Review-4 text')}}</p>
+                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-4 text')}}</p>
                 <div class="sender">
                     <img style="margin-top: -5px; height: 32px;" src="../../assets/img/svg/reviews/nambafood.svg" alt="">
                     <div class="sender-name">
@@ -51,7 +52,7 @@
                 </div>
             </slide>
             <slide class="reviews_wrap-carousel-slide">
-                <p class="reviews_wrap-carousel-slide_desc">{{$t('Review-5 text')}}</p>
+                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-5 text')}}</p>
                 <div class="sender">
                     <img src="../../assets/img/svg/reviews/diesel.svg" alt="">
                     <div class="sender-name">
@@ -60,7 +61,7 @@
                 </div>
             </slide>
             <slide class="reviews_wrap-carousel-slide">
-                <p class="reviews_wrap-carousel-slide_desc">{{$t('Review-6 text')}}</p>
+                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-6 text')}}</p>
                 <div class="sender">
                     <img style="margin-top: -2px; height: 24px;" src="../../assets/img/svg/reviews/inn-4-pet.svg" alt="">
                     <div class="sender-name">
