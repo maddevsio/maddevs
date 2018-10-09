88f83b882adeaf1cb8b2196488687e2e50b43de4
Denisoed <denisod93@gmail.com>
2018-10-09 18:16:26 +0600

    MND-87 removed reviews

--- src/components/Home/Reviews.vue
+++ src/components/Home/Reviews.vue
@@ -16,15 +16,6 @@
             ref="carousel"
         >
             <slide class="reviews_wrap-carousel-slide">
-                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-1 text')}}</p>
-                <div class="sender">
-                    <img style="margin-bottom: -5px" src="../../assets/img/png/reviews/socialshopwave.png" alt="">
-                    <div class="sender-name">
-                        <p>{{$t('Review-1 name')}}</p>
-                    </div>
-                </div>
-            </slide>
-            <slide class="reviews_wrap-carousel-slide">
                 <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-2 text')}}</p>
                 <div class="sender">
                     <img style="margin-bottom: -2px" src="../../assets/img/svg/reviews/teacherly.svg" alt="">
@@ -39,33 +30,6 @@
                     <img style="margin-top: -2px" src="../../assets/img/svg/reviews/veeqo.svg" alt="">
                     <div class="sender-name">
                         <p>{{$t('Review-3 name')}}</p>
-                    </div>
-                </div>
-            </slide>
-            <slide class="reviews_wrap-carousel-slide">
-                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-4 text')}}</p>
-                <div class="sender">
-                    <img style="margin-top: -5px; height: 32px;" src="../../assets/img/svg/reviews/nambafood.svg" alt="">
-                    <div class="sender-name">
-                        <p>{{$t('Review-4 name')}}</p>
-                    </div>
-                </div>
-            </slide>
-            <slide class="reviews_wrap-carousel-slide">
-                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-5 text')}}</p>
-                <div class="sender">
-                    <img src="../../assets/img/svg/reviews/diesel.svg" alt="">
-                    <div class="sender-name">
-                        <p>{{$t('Review-5 name')}}</p>
-                    </div>
-                </div>
-            </slide>
-            <slide class="reviews_wrap-carousel-slide">
-                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-6 text')}}</p>
-                <div class="sender">
-                    <img style="margin-top: -2px; height: 24px;" src="../../assets/img/svg/reviews/inn-4-pet.svg" alt="">
-                    <div class="sender-name">
-                        <p>{{$t('Review-6 name')}}</p>
                     </div>
                 </div>
             </slide>
