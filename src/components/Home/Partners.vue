855fbfd3fb69b7330b11101816865a7680705100
Denisoed <denisod93@gmail.com>
2018-10-11 12:18:15 +0600

    MND-77 changed logo in section Our Partners

--- src/components/Home/Partners.vue
+++ src/components/Home/Partners.vue
@@ -6,7 +6,7 @@
         <div class="partners_wrap-icons">
           <!-- Row -->
           <div class="partners_wrap-icons_row">
-            <a class="item-left">
+            <a class="small-image">
               <img src="../../assets/img/svg/partners/veeqo-logo.svg" alt="">
             </a>
             <a>
@@ -18,14 +18,14 @@
             <a>
               <img src="../../assets/img/svg/partners/eatigo.svg" alt="">
             </a>
-            <a class="item-right scentomania">
+            <a class="scentomania">
               <img src="../../assets/img/svg/partners/scento.svg" alt="">
             </a>
-            <a class="item-left">
+            <a>
               <img src="../../assets/img/svg/partners/namba.svg" alt="">
             </a>
             <a>
-              <img src="../../assets/img/svg/partners/yourcast.svg" alt="">
+              <img src="../../assets/img/svg/partners/zofice-copy.svg" alt="">
             </a>
             <a>
               <img src="../../assets/img/svg/partners/iotsploit-copy.svg" alt="">
@@ -33,80 +33,74 @@
             <a>
               <img src="../../assets/img/svg/partners/kaf.svg" alt="">
             </a>
-            <a class="item-right">
+            <a>
               <img src="../../assets/img/svg/partners/namba-taxi.svg" alt="">
             </a>
-            <a class="item-left">
+            <a class="small-image">
+              <img src="../../assets/img/svg/partners/teacherly-copy.svg" alt="">
+            </a>
+            <a class="small-image">
               <img src="../../assets/img/svg/partners/nambafood.svg" alt="">
             </a>
-            <a>
+            <a class="small-image">
               <img src="../../assets/img/svg/partners/silkroadexplore.svg" alt="">
             </a>
-            <a class="last-icon">
+            <a>
               <img src="../../assets/img/svg/partners/besmart.svg" alt="">
             </a>
             <a>
               <img src="../../assets/img/svg/partners/kickico.svg" alt="">
             </a>
-            <a class="item-right">
-              <img src="../../assets/img/svg/partners/neureal.svg" alt="">
-            </a>
-            <a class="item-left">
-              <img src="../../assets/img/svg/partners/teacherly-copy.svg" alt="">
-            </a>
             <a>
               <img src="../../assets/img/svg/partners/terbine.svg" alt="">
             </a>
-            <a>
-              <img src="../../assets/img/svg/partners/zofice-copy.svg" alt="">
+            <a class="lakediamond">
+              <picture>
+                <source media="(min-width: 465px)" srcset="../../assets/img/png/partners/lakediamond@2x.png">
+                <img src="../../assets/img/png/partners/lakediamond.png" alt="lakediamond" />
+              </picture>
             </a>
             <a>
               <img src="../../assets/img/svg/partners/idmatch.svg" alt="">
             </a>
-            <a class="item-right">
+            <a>
               <img src="../../assets/img/svg/partners/inn-4-pet.svg" alt="">
             </a>
-            <a class="item-left tengri">
+            <a>
+              <img src="../../assets/img/svg/partners/socialshopwave.svg" alt="">
+            </a>
+            <a class="tengri">
               <img src="../../assets/img/svg/partners/tengri.svg" alt="">
+            </a>
+            <a class="small-image">
+              <img src="../../assets/img/svg/partners/nappy.svg" alt="">
             </a>
             <a>
               <img src="../../assets/img/svg/partners/admitad.svg" alt="">
             </a>
-            <a>
-              <img src="../../assets/img/svg/partners/elcart.svg" alt="">
+            <a class="photobook">
+              <img src="../../assets/img/svg/partners/photobook.svg" alt="">
             </a>
-            <a>
-              <img src="../../assets/img/svg/partners/triggermesh.svg" alt="">
-            </a>
-            <a class="item-right">
+            <a class="small-image">
               <img src="../../assets/img/svg/partners/smk.svg" alt="">
             </a>
-            <a class="item-left">
-              <img src="../../assets/img/svg/partners/mythril-platform-0.svg" alt="">
+            <a class="small-image">
+              <img src="../../assets/img/svg/partners/pickd.svg" alt="">
             </a>
-             <a>
+             <a class="small-image">
               <img src="../../assets/img/svg/partners/smb.svg" alt="">
             </a>
-             <a>
+             <a class="small-image">
               <img src="../../assets/img/svg/partners/diesel.svg" alt="">
             </a>
-             <a>
-              <img src="../../assets/img/png/partners/lakediamond.png" alt="">
+            <a class="small-image">
+              <img src="../../assets/img/svg/partners/triggermesh.svg" alt="">
             </a>
-            <a class="item-right">
-              <img src="../../assets/img/png/partners/nappy-club.png" alt="">
+            <a class="small-image">
+              <img src="../../assets/img/svg/partners/elcart.svg" alt="">
             </a>
-            <a class="item-left">
-              <img src="../../assets/img/png/partners/nuntiux.png" alt="">
-            </a>
-             <a class="photobook">
-              <img src="../../assets/img/png/partners/photobook.png" alt="">
-            </a>
-            <a>
-              <img src="../../assets/img/png/partners/pickd.png" alt="">
-            </a>
-            <a class="item-right">
-              <img src="../../assets/img/png/partners/socialshopwave.png" alt="">
+            <a class="small-image">
+              <img src="../../assets/img/svg/partners/nuntiux.svg" alt="">
             </a>
           </div>
         </div>
