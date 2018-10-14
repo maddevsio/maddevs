de97201e4b4bd6258b8647ac96c3983501ea32e9
Denisoed <denisod93@gmail.com>
2018-10-14 20:20:01 +0600

    Changed position elements in blocks section Press

--- src/components/Home/Press.vue
+++ src/components/Home/Press.vue
@@ -7,15 +7,15 @@
           <div class="press-wrap_item_img">
             <img src="../../assets/img/svg/press/cnbc.svg" alt="">
           </div>
-          <a>https://www.cnbc.com/2018/01/29/belt…</a>
           <p>…globally successful companies were born in the capital city of Bishkek: <span>Mad Devs</span>, a <span>developer</span>  outsourcer that boasts clients from <span>Silicon Valley</span> to <span>Singapore</span>…</p>
+          <a>https://www.cnbc.com/2018/01/29/belt…</a>
         </a>
         <a href="https://www.ozy.com/rising-stars/this-unlikely-tech-queen-wants-to-build-a-new-global-hub-in-kyrgyzstan/88948" target="_blank" class="press-wrap_item">
           <div class="press-wrap_item_img">
             <img src="../../assets/img/svg/press/ozy.svg" alt="">
           </div>
-          <a>https://www.ozy.com/rising-stars/this-unlikely-tech-queen…</a>
           <p>This unlikely <span>tech queen</span> wants to build a <span>new global hub in Kyrgyzstan</span>. Because behind every great app there’s a great <span>team of back-end developers…</span></p>
+          <a>https://www.ozy.com/rising-stars/this-unlikely-tech-queen…</a>
         </a>
       </div>
     </div>
