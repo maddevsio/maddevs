be143840f2cbbe6f49f50cdb7f3189362262bdaf
Denisoed <denisod93@gmail.com>
2018-10-14 21:36:23 +0600

    MND-102 Added Russian text

--- src/components/Home/Partners.vue
+++ src/components/Home/Partners.vue
@@ -7,14 +7,14 @@
           <div class="partners-wrap_item_img">
             <img src="../../assets/img/svg/partners/neureal.svg" alt="">
           </div>
-          <p>Artificial intelligence + blockchain. A limitless ability to forecast the future</p>
+          <p>{{ $t('Partners-block1') }}</p>
           <a>https://neureal.net/</a>
         </a>
         <a href=" https://mythril.ai" target="_blank" class="partners-wrap_item">
           <div class="partners-wrap_item_img">
             <img src="../../assets/img/svg/partners/mythril.svg" alt="">
           </div>
-          <p>Mythril Platform enables a secure and thriving ecosystem of Ethereum dapps & smarts contracts</p>
+          <p>{{ $t('Partners-block2') }}</p>
           <a>https://mythril.ai</a>
         </a>
       </div>
