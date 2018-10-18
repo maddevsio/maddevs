20f0d111aad2e32f91e4d0c61201f8861bc9a9e5
Denisoed <denisod93@gmail.com>
2018-10-18 21:56:32 +0600

    MND-116 added hight resolution images for mobile

--- src/components/Home/BestPractices.vue
+++ src/components/Home/BestPractices.vue
@@ -21,6 +21,8 @@
                     <picture>
                         <source srcset="../../assets/img/png/best-practices/bp1.png"
                                 media="(max-width: 992px)">
+                        <source srcset="../../assets/img/png/best-practices/bp1@2x.png 2x"
+                                media="(max-width: 992px)">
                         <img @click.prevent="nextSlide" src="../../assets/img/png/best-practices/bp1.png" srcset="../../assets/img/png/best-practices/bp1@2x.png 2x">
                     </picture>
                   </slide>
@@ -28,12 +30,16 @@
                     <picture>
                         <source srcset="../../assets/img/png/best-practices/bp2.png"
                                 media="(max-width: 992px)">
+                        <source srcset="../../assets/img/png/best-practices/bp2@2x.png 2x"
+                                media="(max-width: 992px)">
                         <img @click.prevent="nextSlide" src="../../assets/img/png/best-practices/bp2.png" srcset="../../assets/img/png/best-practices/bp2@2x.png 2x">
                     </picture>
                   </slide>
                   <slide class="best-practices_wrap-preview-slide">
                     <picture>
                         <source srcset="../../assets/img/png/best-practices/bp3.png"
+                                media="(max-width: 992px)">
+                        <source srcset="../../assets/img/png/best-practices/bp3@2x.png 2x"
                                 media="(max-width: 992px)">
                         <img @click.prevent="nextSlide" src="../../assets/img/png/best-practices/bp3.png" srcset="../../assets/img/png/best-practices/bp3@2x.png 2x">
                     </picture>
