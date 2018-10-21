76d5286caacda121b11b33ad07f899139c08d450
Denisoed <denisod93@gmail.com>
2018-10-21 16:42:18 +0600

    changed text for blocks in section Blog

--- src/components/Home/OurBlog.vue
+++ src/components/Home/OurBlog.vue
@@ -29,7 +29,7 @@
             </picture>
             <div class="our-blog_sender">
               <div class="our-blog_sender-img">
-                <img src="../../assets/img/jpg/our-blog/OlegK.jpg" srcset="../../assets/img/jpg/our-blog/OlegK.jpg 2x" alt="">
+                <img src="../../assets/img/png/our-blog/AndrewS.png" srcset="../../assets/img/png/our-blog/AndrewS@2x.png 2x" alt="">
               </div>
               <div class="our-blog_sender-info">
                 <p class="our-blog_sender-name">{{$t('blog2-sender-name')}}</p>
@@ -46,7 +46,7 @@
             </picture>
             <div class="our-blog_sender">
               <div class="our-blog_sender-img">
-                <img src="../../assets/img/png/our-blog/AlexV.png" srcset="../../assets/img/png/our-blog/AlexV@2x.png 2x" alt="">
+                <img src="../../assets/img/jpg/our-blog/OlegK.jpg" alt="">
               </div>
               <div class="our-blog_sender-info">
                 <p class="our-blog_sender-name">{{$t('blog3-sender-name')}}</p>
@@ -63,7 +63,7 @@
             </picture>
             <div class="our-blog_sender">
               <div class="our-blog_sender-img">
-                <img src="../../assets/img/png/our-blog/AllaK.png" srcset="../../assets/img/png/our-blog/AllaK@2x.png 2x" alt="">
+                <img src="../../assets/img/jpg/our-blog/GennadyK.jpg" alt="">
               </div>
               <div class="our-blog_sender-info">
                 <p class="our-blog_sender-name">{{$t('blog4-sender-name')}}</p>
@@ -75,15 +75,12 @@
           <!-- Item 5 -->
           <a :href="$t('blog5-link')" target="_blank" class="our-blog-wrap_list-item">
             <picture>
-              <source v-if="language === 'ru'" media="(max-width: 576px)" srcset="../../assets/img/png/our-blog/5@2x.png">
-              <source v-else media="(max-width: 576px)" srcset="../../assets/img/png/our-blog/7@2x.png">
-              <img v-if="language === 'ru'" src="../../assets/img/png/our-blog/5.png" srcset="../../assets/img/png/our-blog/5@2x.png 2x" alt="">
-              <img v-else src="../../assets/img/png/our-blog/7.png" srcset="../../assets/img/png/our-blog/7@2x.png 2x" alt="">
+              <source media="(max-width: 576px)" srcset="../../assets/img/png/our-blog/5@2x.png">
+              <img src="../../assets/img/png/our-blog/7.png" srcset="../../assets/img/png/our-blog/7@2x.png 2x" alt="">
             </picture>
             <div class="our-blog_sender">
               <div class="our-blog_sender-img">
-                <img v-if="language === 'ru'" src="../../assets/img/png/our-blog/TimurZ.png" srcset="../../assets/img/png/our-blog/TimurZ@2x.png 2x" alt="">
-                <img v-else src="../../assets/img/jpg/our-blog/AlexeyM.jpg" srcset="../../assets/img/jpg/our-blog/AlexeyM.jpg 2x" alt="">
+                <img src="../../assets/img/png/our-blog/AndrewS.png" srcset="../../assets/img/png/our-blog/AndrewS@2x.png 2x" alt="">
               </div>
               <div class="our-blog_sender-info">
                 <p class="our-blog_sender-name">{{$t('blog5-sender-name')}}</p>
@@ -95,14 +92,11 @@
           <!-- Item 5 -->
           <a :href="$t('blog6-link')" target="_blank" class="our-blog-wrap_list-item">
             <picture>
-              <source v-if="language === 'ru'" media="(max-width: 576px)" srcset="../../assets/img/png/our-blog/6@2x.png">
-              <source v-else media="(max-width: 576px)" srcset="../../assets/img/png/our-blog/8@2x.png">
-              <img v-if="language === 'ru'" src="../../assets/img/png/our-blog/6.png" srcset="../../assets/img/png/our-blog/6@2x.png 2x" alt="">
-              <img v-else src="../../assets/img/png/our-blog/8.png" srcset="../../assets/img/png/our-blog/8@2x.png 2x" alt="">
+              <source media="(max-width: 576px)" srcset="../../assets/img/png/our-blog/6@2x.png">
+              <img src="../../assets/img/png/our-blog/8.png" srcset="../../assets/img/png/our-blog/8@2x.png 2x" alt="">
             </picture>
             <div class="our-blog_sender">
               <div class="our-blog_sender-img">
-                <img v-if="language === 'ru'" src="../../assets/img/png/our-blog/AndrewS.png" srcset="../../assets/img/png/our-blog/AndrewS@2x.png 2x" alt="">
                 <img src="../../assets/img/png/our-blog/AndrewM.png" srcset="../../assets/img/png/our-blog/AndrewM@2x.png 2x" alt="">
               </div>
               <div class="our-blog_sender-info">
