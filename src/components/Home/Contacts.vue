2135bd1f75958a0fca3b9e0eb3590e835e731206
Denisoed <denisod93@gmail.com>
2018-10-22 16:40:54 +0600

    MND-116 moved position soc icon in two row

--- src/components/Home/Contacts.vue
+++ src/components/Home/Contacts.vue
@@ -15,33 +15,37 @@
             </div>
             <div class="contacts-wrap_list-item social-icons">
                 <div class="social-icons_list">
-                    <a href="https://github.com/maddevsio" target="_blank">
-                        <img src="../../assets/img/svg/footer/git.svg" alt="Github">
-                    </a>
-                    <a href="https://www.facebook.com/maddevsio" target="_blank">
-                        <img src="../../assets/img/svg/footer/fb.svg" alt="Facebook">
-                    </a>
-                    <a href="https://www.instagram.com/maddevsio/" target="_blank">
-                        <img src="../../assets/img/svg/footer/insta.svg" alt="Instagram">
-                    </a>
-                    <a href="https://blog.maddevs.io/" target="_blank">
-                        <img src="../../assets/img/svg/footer/medium.svg" alt="Medium">
-                    </a>
-                    <a href="https://twitter.com/MadDevsIO" target="_blank">
-                        <img src="../../assets/img/svg/footer/tw.svg" alt="Twitter">
-                    </a>
-                    <a href="https://www.slideshare.net/maddevs/presentations" target="_blank">
-                        <img src="../../assets/img/svg/footer/slide.svg" alt="Slideshare">
-                    </a>
-                    <a href="https://www.youtube.com/playlist?list=PLsmdb5W8ytypyXt1ut3lfBOnOZnDNqYIN" target="_blank">
-                        <img src="../../assets/img/svg/footer/yt.svg" alt="Youtube">
-                    </a>
-                    <a href="https://www.behance.net/maddevs" target="_blank">
-                        <img src="../../assets/img/svg/footer/beh.svg" alt="Behance">
-                    </a>
-                    <a href="https://t.me/maddevsio" target="_blank">
-                        <img src="../../assets/img/svg/footer/tel.svg" alt="Telegram">
-                    </a>
+                    <div class="social-icons_list-row">
+                        <a href="https://github.com/maddevsio" target="_blank">
+                            <img src="../../assets/img/svg/footer/git.svg" alt="Github">
+                        </a>
+                        <a href="https://www.facebook.com/maddevsio" target="_blank">
+                            <img src="../../assets/img/svg/footer/fb.svg" alt="Facebook">
+                        </a>
+                        <a href="https://www.instagram.com/maddevsio/" target="_blank">
+                            <img src="../../assets/img/svg/footer/insta.svg" alt="Instagram">
+                        </a>
+                        <a href="https://blog.maddevs.io/" target="_blank">
+                            <img src="../../assets/img/svg/footer/medium.svg" alt="Medium">
+                        </a>
+                    </div>
+                    <div class="social-icons_list-row">    
+                        <a href="https://twitter.com/MadDevsIO" target="_blank">
+                            <img src="../../assets/img/svg/footer/tw.svg" alt="Twitter">
+                        </a>
+                        <a href="https://www.slideshare.net/maddevs/presentations" target="_blank">
+                            <img src="../../assets/img/svg/footer/slide.svg" alt="Slideshare">
+                        </a>
+                        <a href="https://www.youtube.com/playlist?list=PLsmdb5W8ytypyXt1ut3lfBOnOZnDNqYIN" target="_blank">
+                            <img src="../../assets/img/svg/footer/yt.svg" alt="Youtube">
+                        </a>
+                        <a href="https://www.behance.net/maddevs" target="_blank">
+                            <img src="../../assets/img/svg/footer/beh.svg" alt="Behance">
+                        </a>
+                        <a href="https://t.me/maddevsio" target="_blank">
+                            <img src="../../assets/img/svg/footer/tel.svg" alt="Telegram">
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
