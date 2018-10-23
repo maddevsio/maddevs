d2a478ec2ef27771c90ac38c346ec3458096369c
Denisoed <denisod93@gmail.com>
2018-10-23 12:44:09 +0600

    MND-117 added poster for video bg

--- src/components/Home/Banner.vue
+++ src/components/Home/Banner.vue
@@ -1,9 +1,9 @@
 <template>
   <section id="banner" class="banner">
     <div class="video-background">
-      <video autoplay muted loop id="myVideo">
+      <video autoplay muted loop id="BgVideo">
         <source src="../../assets/media/Mad Devs Working Process.mp4" type="video/mp4">
-        <!--<source src="../../assets/media/videoplayback.webm" type='video/webm; codecs="vp8, vorbis"' />-->
+        <source src="../../assets/media/videoplayback.webm" type='video/webm; codecs="vp8, vorbis"' />
       </video>
     </div>
     <div class="container">
