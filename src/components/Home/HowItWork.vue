99d625a0c10de1d357058e1c16d80b227c406467
Denisoed <denisod93@gmail.com>
2018-10-31 19:36:08 +0600

    MND-142 moved all text to local files in HowItWork component

--- src/components/Home/HowItWork.vue
+++ src/components/Home/HowItWork.vue
@@ -2,23 +2,23 @@
   <section id="how-it-work" class="how-it-work">
     <div class="how-it-work_wrap">
       <div class="container">
-        <h2 class="sec-title">{{ $t('How do we work?') }}</h2>
+        <h2 class="sec-title">{{ $t('Working processes') }}</h2>
         <p class="sec-desc">{{ $t('Since we are working remotely we strongly believe in the transparency of work, pay special attention to communication and keep strict compliance to working processes') }}</p>
       </div>
       <div class="how-it-work_wrap-content">
         <div class="container">
           <h3 v-if="lang === 'en'" class="how-it-work_wrap-content-title sec-desc">
-            We <span @click="setAtiveSlide(0)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 0 }">design</span> a technical team for you,
-            <span @click="setAtiveSlide(1)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 1 }">manage</span> it and closely
-            <span @click="setAtiveSlide(2)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 2 }">communicate</span>
-            with you and inside the team.
+            {{ $t('We') }} <span @click="setAtiveSlide(0)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 0 }">{{ $t('design') }}</span> {{ $t('a technical team for you') }},
+            <span @click="setAtiveSlide(1)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 1 }">{{ $t('manage') }}</span> {{ $t('it and closely') }}
+            <span @click="setAtiveSlide(2)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 2 }">{{ $t('communicate') }}</span>
+            {{ $t('with you and inside the team') }}.
           </h3>
           <h3 v-else class="how-it-work_wrap-content-title sec-desc">
-            Посмотрите как мы подходим к работе с
-            <span @click="setAtiveSlide(0)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 0 }">командами</span>,
-            как мы занимается их <span @click="setAtiveSlide(1)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 1 }">менеджментом</span>,
-            и как <span @click="setAtiveSlide(2)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 2 }">коммуницируем</span>
-            внутри и с клиентами.
+            {{ $t('We') }}
+            <span @click="setAtiveSlide(0)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 0 }">{{ $t('design') }}</span>,
+            {{ $t('a technical team for you') }} <span @click="setAtiveSlide(1)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 1 }">{{ $t('manage') }}</span>,
+            {{ $t('it and closely') }} <span @click="setAtiveSlide(2)" :class="{ 'how-it-work_wrap-content-slide_active': activeSlide == 2 }">{{ $t('communicate') }}</span>
+            {{ $t('with you and inside the team') }}.
           </h3>
           <div class="how-it-work_wrap-content-slider">
             <carousel
