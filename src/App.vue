278f1f9d273792287d0f8172971810992a3735bc
Denisoed <denisod93@gmail.com>
2018-11-05 18:36:08 +0600

    MND-145 added notification window with cookie info

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <cookie-law
+      theme="blood-orange"
+      :message="$t('cookie-message')"
+      :buttonText="$t('cookie-button')"
+    ></cookie-law>
     <Header @EventLanguage="getLanguage"/>
     <router-view :language="this.lang"/>
     <Footer />
@@ -11,6 +16,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import './assets/styles/index.scss';
 import i18n from 'i18next';
+import CookieLaw from 'vue-cookie-law';
 
 export default {
   name: 'App',
@@ -19,7 +25,7 @@ export default {
       lang: i18n.language,
     };
   },
-  components: { Header, Footer },
+  components: { Header, Footer, CookieLaw },
   methods: {
     getLanguage(lang) {
       this.lang = lang;
