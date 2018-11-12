b6ce60591222e3354713c5fe858f661d542d2f36
Denisoed <denisod93@gmail.com>
2018-11-12 23:23:37 +0600

    MND-118 changed slider in Reviews component

--- src/components/Home/Reviews.vue
+++ src/components/Home/Reviews.vue
@@ -3,58 +3,57 @@
     <div class="container">
       <div class="reviews_wrap">
         <h2 class="sec-title">{{$t('Сlient feedback ')}}</h2>
-         <carousel
-            :per-page="1"
-            :loop=true
-            :autoplayLoop="true"
-            :autoplay="true"
-            :autoplayTimeout="5000"
-            loop:true
-            mouse-drag:false
-            autoplayHoverPause
-            class="reviews_wrap-carousel"
-            ref="carousel"
-        >
-            <slide class="reviews_wrap-carousel-slide">
-                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-2 text')}}</p>
-                <div class="sender">
-                    <img style="margin-bottom: -2px" src="../../assets/img/svg/reviews/teacherly.svg" alt="">
-                    <div class="sender-name">
-                        <p>{{$t('Review-2 name')}}</p>
-                    </div>
+        <slick
+          class="reviews_wrap-carousel"
+          ref="slick"
+          :options="slickOptions">
+          <div class="reviews_wrap-carousel-slide">
+            <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-2 text')}}</p>
+            <div class="sender">
+                <img style="margin-bottom: -2px" src="../../assets/img/svg/reviews/teacherly.svg" alt="">
+                <div class="sender-name">
+                    <p>{{$t('Review-2 name')}}</p>
                 </div>
-            </slide>
-            <slide class="reviews_wrap-carousel-slide">
-                <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-3 text')}}</p>
-                <div class="sender">
-                    <img style="margin-top: -2px" src="../../assets/img/svg/reviews/veeqo.svg" alt="">
-                    <div class="sender-name">
-                        <p>{{$t('Review-3 name')}}</p>
-                    </div>
+            </div>
+          </div>
+          <div class="reviews_wrap-carousel-slide">
+            <p @click.prevent="nextSlide" class="reviews_wrap-carousel-slide_desc">{{$t('Review-3 text')}}</p>
+            <div class="sender">
+                <img style="margin-top: -2px" src="../../assets/img/svg/reviews/veeqo.svg" alt="">
+                <div class="sender-name">
+                    <p>{{$t('Review-3 name')}}</p>
                 </div>
-            </slide>
-        </carousel>
+            </div>
+          </div>
+        </slick>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import {
-  Carousel,
-  Slide
-} from 'vue-carousel';
+import Slick from 'vue-slick';
+import 'slick-carousel/slick/slick.css';
 
 export default {
   name: 'reviews',
+  data() {
+    return {
+      slickOptions: {
+        slidesToShow: 1,
+        autoplay: false,
+        arrows: false,
+        dots: true
+      },
+    };
+  },
   components: {
-    Carousel,
-    Slide
+    Slick,
   },
   methods: {
     nextSlide() {
-      this.$refs.carousel.goToPage(this.$refs.carousel.getNextPage());
-    }
+      this.$refs.slick.next();
+    },
   }
 };
 
