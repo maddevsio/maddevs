c4d7ce21fb6791f50f01dda52bd7e5e80d2b6d5f
Denisoed <denisod93@gmail.com>
2018-11-18 17:16:58 +0600

    MND-181 removed box-shadow, changed img size

--- src/components/Home/Press.vue
+++ src/components/Home/Press.vue
@@ -5,14 +5,14 @@
       <div class="press-wrap">
         <a href="https://www.google.com/amp/s/www.cnbc.com/amp/2018/01/29/belt-and-road-tech-entrepreneurship-in-kyrgyzstan.html" target="_blank" class="press-wrap_item">
           <div class="press-wrap_item_img">
-            <img src="../../assets/img/svg/press/cnbc.svg" alt="">
+            <img class="press-cnbc" src="../../assets/img/svg/press/cnbc.svg" alt="">
           </div>
           <p>…globally successful companies were born in the capital city of Bishkek: <span>Mad Devs</span>, a <span>developer</span>  outsourcer that boasts clients from <span>Silicon Valley</span> to <span>Singapore</span>…</p>
           <a>https://www.cnbc.com/2018/01/29/belt…</a>
         </a>
         <a href="https://www.ozy.com/rising-stars/this-unlikely-tech-queen-wants-to-build-a-new-global-hub-in-kyrgyzstan/88948" target="_blank" class="press-wrap_item">
           <div class="press-wrap_item_img">
-            <img src="../../assets/img/svg/press/ozy.svg" alt="">
+            <img class="press-ozy" src="../../assets/img/svg/press/ozy.svg" alt="">
           </div>
           <p>This unlikely <span>tech queen</span> wants to build a <span>new global hub in Kyrgyzstan</span>. Because behind every great app there’s a great <span>team of back-end developers…</span></p>
           <a>https://www.ozy.com/rising-stars/this-unlikely-tech-queen…</a>
