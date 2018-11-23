08a632178e2df33cc05dcd175587bfe9dd738a82
Denisoed <denisod93@gmail.com>
2018-11-23 12:36:07 +0600

    MND-201 added mark for section Intern/added block batton--black

---
+++ src/components/Jobs/Intern.vue
@@ -0,0 +1,29 @@
+<template>
+  <section id="intern" class="intern">
+    <div class="container">
+      <h2 class="sec-title">For interns and junior-developers</h2>
+      <p>The chance to get into our team is higher if you are constantly contributing to one of our open source projects you liked.</p>
+      <ButtonLinkBlack :data="this.button" />
+    </div>
+  </section>
+</template>
+
+<script>
+import ButtonLinkBlack from '../blocks/button-link--black';
+
+export default {
+  name: 'Intern',
+  data() {
+    return {
+      button: {
+        link: '#',
+        text: 'Mad Devs on GitHub',
+        icon: 'github-light.svg'
+      }
+    };
+  },
+  components: {
+    ButtonLinkBlack
+  }
+};
+</script>
---
+++ src/components/blocks/button-link--black.vue
@@ -0,0 +1,17 @@
+<template>
+  <a :href="this.data.link" class="mnd-button-link--black">
+    <img :src="this.icon" alt="Icon">
+    {{ this.data.text }}
+  </a>
+</template>
+<script>
+export default {
+  name: 'Banner',
+  props: ['data'],
+  data() {
+    return {
+      icon: require('../../assets/img/common/' + this.data.icon)
+    };
+  },
+};
+</script>
