ac130d864c0c1df36204a114ec365dc57a280df0
Denisoed <denisod93@gmail.com>
2018-11-25 18:11:58 +0600

    MND-198 added styles for mob screen

--- src/components/Jobs/Advantages.vue
+++ src/components/Jobs/Advantages.vue
@@ -6,27 +6,27 @@
             <div class="advantages_list">
                <div class="advantages_list-item">
                   <img src="../../assets/img/Jobs/svg/Advantages/cash.svg" alt="Cash">
-                  <h4>Competitive salary</h4>
+                  <h4>Competitive<br>salary</h4>
                </div>
                <div class="advantages_list-item">
                   <img src="../../assets/img/Jobs/svg/Advantages/rocket.svg" alt="Rocket">
-                  <h4>Projects with AI/ML/Blockchain</h4>
+                  <h4>Projects with AI/<br>ML/Blockchain</h4>
                </div>
                <div class="advantages_list-item">
                   <img src="../../assets/img/Jobs/svg/Advantages/pet.svg" alt="Pet">
-                  <h4>69+ open source projects</h4>
+                  <h4>69+ open source<br>projects</h4>
                </div>
                <div class="advantages_list-item">
                   <img src="../../assets/img/Jobs/svg/Advantages/remote.svg" alt="Remote">
-                  <h4>Remote work</h4>
+                  <h4>Remote<br>work</h4>
                </div>
                <div class="advantages_list-item">
                   <img src="../../assets/img/Jobs/svg/Advantages/commu.svg" alt="Communication">
-                  <h4>Well-established working processes </h4>
+                  <h4>Well-established<br>working processes </h4>
                </div>
                <div class="advantages_list-item">
                   <img src="../../assets/img/Jobs/svg/Advantages/schedule.svg" alt="Schedule">
-                  <h4>Flexible schedule</h4>
+                  <h4>Flexible<br>schedule</h4>
                </div>
             </div>
          </div>
