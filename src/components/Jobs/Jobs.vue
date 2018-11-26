43ed633061f27ac86dc5b3c70213460ec8f41471
Denisoed <denisod93@gmail.com>
2018-11-26 21:58:13 +0600

    Added links for jobs blocks/fixed styles for button red

--- src/components/Jobs/Jobs.vue
+++ src/components/Jobs/Jobs.vue
@@ -3,21 +3,21 @@
         <div class="container">
             <h2 class="sec-title">Now we are looking for</h2>
             <div class="jobs_list">
-                <div class="jobs_list-item">
+                <a href="https://blog.maddevs.io/middle-senior-backend-developer-e5bf0d97c037" class="jobs_list-item" target="_blank">
                     <h4>Middle/Senior Backend Developer</h4>
                     <p>Мы ищем Middle/Senior Backend разработчика, который хочет изучить Go, а сейчас пишет на C/C++/Java с пониманием алгоритмов…</p>
                     <h6>Nov 12</h6>
-                </div>
-                <div class="jobs_list-item">
+                </a>
+                <a href="https://blog.maddevs.io/middle-senior-frontend-developer-323773c91134" class="jobs_list-item" target="_blank">
                     <h4>Middle/Senior Frontend Developer</h4>
                     <p>Приветствую. Мы ищем Middle Frontend разработчика. Надеемся, что вакансия заинтересует вас или ваших друзей, родных и близких. Пожалуйста…</p>
                     <h6>Nov 15</h6>
-                </div>
-                <div class="jobs_list-item">
+                </a>
+                <a href="https://blog.maddevs.io/senior-php-developer-86cb0d11ae3c" class="jobs_list-item" target="_blank">
                     <h4>Вакансия Senior PHP разработчика в Mad Devs</h4>
                     <p>Мы ищем опытного PHP разработчика. Надеемся, что вакансия заинтересует вас…</p>
                     <h6>Nov 18</h6>
-                </div>
+                </a>
             </div>
         </div>
     </section>
--- src/components/Jobs/Team.vue
+++ src/components/Jobs/Team.vue
@@ -4,7 +4,7 @@
           <h2 class="sec-title">Do you want to be a part of our team? </h2>
           <p>
             If you just like us are passinoate about what you are doing and believe that working hard is the only way to get better and gain relevant experince, write to
-            <a href="mailto:hr@maddevs.io" class="">hr@maddevs.io</a>
+            <a href="mailto:hr@maddevs.io">hr@maddevs.io</a>
           </p>
           <ButtonLink :data="this.button" />
       </div>
