49c3e2801e6908c460ac7ddae80e93de3f6ae824
Denisoed <denisod93@gmail.com>
2018-11-26 11:51:11 +0600

    MND-202 added slick carousel

--- src/components/Jobs/Employees.vue
+++ src/components/Jobs/Employees.vue
@@ -3,33 +3,67 @@
       <div class="container">
           <h2 class="sec-title">We care about employees</h2>
           <p>In our team we have flat management system and pay special attention to each employee. We support the initiatives and help to grow through mentorship.</p>
-          <div class="employees_content">
-              <div class="employees_content-item">
+          <slick
+              class="employees_content"
+              ref="slick"
+              @beforeChange="handleBeforeChange"
+              :options="slickOptions">
+              <div @click.prevent="nextSlide" class="employees_content-item">
                   <img src="../../assets/img/Jobs/jpg/Employees/employees-1.jpg" srcset="
                     ../../assets/img/Jobs/jpg/Employees/employees-1@2x.jpg 2x,
                     ../../assets/img/Jobs/jpg/Employees/employees-1@3x.jpg 3x" alt="Our employees">
               </div>
-              <div class="employees_content-item">
+              <div @click.prevent="nextSlide" class="employees_content-item">
                   <img src="../../assets/img/Jobs/jpg/Employees/employees-2.jpg" srcset="
                     ../../assets/img/Jobs/jpg/Employees/employees-2@2x.jpg 2x,
                     ../../assets/img/Jobs/jpg/Employees/employees-2@3x.jpg 3x" alt="Our employees">
               </div>
-              <div class="employees_content-item">
+              <div @click.prevent="nextSlide" class="employees_content-item">
                   <img src="../../assets/img/Jobs/jpg/Employees/employees-3.jpg" srcset="
                     ../../assets/img/Jobs/jpg/Employees/employees-3@2x.jpg 2x,
                     ../../assets/img/Jobs/jpg/Employees/employees-3@3x.jpg 3x" alt="Our employees">
               </div>
-              <div class="employees_content-item">
+              <div @click.prevent="nextSlide" class="employees_content-item">
                   <img src="../../assets/img/Jobs/jpg/Employees/employees-4.jpg" srcset="
                     ../../assets/img/Jobs/jpg/Employees/employees-4@2x.jpg 2x,
                     ../../assets/img/Jobs/jpg/Employees/employees-4@3x.jpg 3x" alt="Our employees">
               </div>
-          </div>
+          </slick>
       </div>
   </section>
 </template>
 <script>
+import Slick from 'vue-slick';
+import 'slick-carousel/slick/slick.css';
+
 export default {
   name: 'Employees',
+  data() {
+    return {
+      activeSlide: 0,
+      slickOptions: {
+        slidesToShow: 1,
+        autoplay: false,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
+      },
+    };
+  },
+  components: {
+    Slick
+  },
+  methods: {
+    nextSlide() {
+      this.$refs.slick.next();
+    },
+    setAtiveSlide(index) {
+      this.activeSlide = index;
+      this.$refs.slick.goTo(index);
+    },
+    handleBeforeChange(event, slick, currentSlide, nextSlide) {
+      this.setAtiveSlide(nextSlide);
+    },
+  }
 };
 </script>
