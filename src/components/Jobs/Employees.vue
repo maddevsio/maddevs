9791330c8fc911f56d23fe0491cca1a288a9e408
Denisoed <denisod93@gmail.com>
2018-11-26 12:48:40 +0600

    Fixed styles for carousel/ added mob styles for sec Team and Intern

--- src/components/Jobs/Employees.vue
+++ src/components/Jobs/Employees.vue
@@ -3,8 +3,30 @@
       <div class="container">
           <h2 class="sec-title">We care about employees</h2>
           <p>In our team we have flat management system and pay special attention to each employee. We support the initiatives and help to grow through mentorship.</p>
+          <div class="employees_content">
+              <div @click.prevent="nextSlide" class="employees_content-item">
+                  <img src="../../assets/img/Jobs/jpg/Employees/employees-1.jpg" srcset="
+                    ../../assets/img/Jobs/jpg/Employees/employees-1@2x.jpg 2x,
+                    ../../assets/img/Jobs/jpg/Employees/employees-1@3x.jpg 3x" alt="Our employees">
+              </div>
+              <div @click.prevent="nextSlide" class="employees_content-item">
+                  <img src="../../assets/img/Jobs/jpg/Employees/employees-2.jpg" srcset="
+                    ../../assets/img/Jobs/jpg/Employees/employees-2@2x.jpg 2x,
+                    ../../assets/img/Jobs/jpg/Employees/employees-2@3x.jpg 3x" alt="Our employees">
+              </div>
+              <div @click.prevent="nextSlide" class="employees_content-item">
+                  <img src="../../assets/img/Jobs/jpg/Employees/employees-3.jpg" srcset="
+                    ../../assets/img/Jobs/jpg/Employees/employees-3@2x.jpg 2x,
+                    ../../assets/img/Jobs/jpg/Employees/employees-3@3x.jpg 3x" alt="Our employees">
+              </div>
+              <div @click.prevent="nextSlide" class="employees_content-item">
+                  <img src="../../assets/img/Jobs/jpg/Employees/employees-4.jpg" srcset="
+                    ../../assets/img/Jobs/jpg/Employees/employees-4@2x.jpg 2x,
+                    ../../assets/img/Jobs/jpg/Employees/employees-4@3x.jpg 3x" alt="Our employees">
+              </div>
+          </div>
           <slick
-              class="employees_content"
+              class="employees_content-slider"
               ref="slick"
               @beforeChange="handleBeforeChange"
               :options="slickOptions">
