0c693ba7df489973fa70b8de0dad697dc24855e8
Denisoed <denisod93@gmail.com>
2018-11-27 20:13:33 +0600

    Added Russian text

--- src/components/Jobs/Jobs.vue
+++ src/components/Jobs/Jobs.vue
@@ -4,19 +4,19 @@
             <h2 class="sec-title">{{ $t('jobs-title') }}</h2>
             <div class="jobs_list">
                 <a href="https://blog.maddevs.io/middle-senior-backend-developer-e5bf0d97c037" class="jobs_list-item" target="_blank">
-                    <h4>Middle/Senior Backend Developer</h4>
-                    <p>Мы ищем Middle/Senior Backend разработчика, который хочет изучить Go, а сейчас пишет на C/C++/Java с пониманием алгоритмов…</p>
-                    <h6>Nov 12</h6>
+                    <h4>{{ $t('jobs1-subtitle') }}</h4>
+                    <p>{{ $t('jobs1-desc') }}</p>
+                    <h6>{{ $t('jobs1-date') }}</h6>
                 </a>
                 <a href="https://blog.maddevs.io/middle-senior-frontend-developer-323773c91134" class="jobs_list-item" target="_blank">
-                    <h4>Middle/Senior Frontend Developer</h4>
-                    <p>Приветствую. Мы ищем Middle Frontend разработчика. Надеемся, что вакансия заинтересует вас или ваших друзей, родных и близких. Пожалуйста…</p>
-                    <h6>Nov 15</h6>
+                    <h4>{{ $t('jobs2-subtitle') }}</h4>
+                    <p>{{ $t('jobs2-desc') }}</p>
+                    <h6>{{ $t('jobs2-date') }}</h6>
                 </a>
                 <a href="https://blog.maddevs.io/senior-php-developer-86cb0d11ae3c" class="jobs_list-item" target="_blank">
-                    <h4>Вакансия Senior PHP разработчика в Mad Devs</h4>
-                    <p>Мы ищем опытного PHP разработчика. Надеемся, что вакансия заинтересует вас…</p>
-                    <h6>Nov 18</h6>
+                    <h4>{{ $t('jobs3-subtitle') }}</h4>
+                    <p>{{ $t('jobs3-desc') }}</p>
+                    <h6>{{ $t('jobs3-date') }}</h6>
                 </a>
             </div>
         </div>
