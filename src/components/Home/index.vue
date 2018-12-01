9a37b61cbb5b876b806c01bec58f842a4f7bdc4e
Denisoed <denisod93@gmail.com>
2018-12-01 19:54:03 +0600

    MND-217 added dynamic og data for home/ jobs pages

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -38,7 +38,9 @@ export default {
   props: ['language'],
   data() {
     return {
-      description: this.$t('meta-desc'),
+      title: this.$t('title-home'),
+      description: this.$t('meta-desc_home'),
+      ogUrl: this.$t('og_meta-url_home'),
     };
   },
   components: {
@@ -58,9 +60,23 @@ export default {
     Contacts
   },
   head: {
+    title: function() {
+      return {
+        inner: this.title,
+        separator: ' ', // Leave empty separator
+        complement: ' ', // Leave empty complement
+      };
+    },
     meta: function () {
       return [
         {name: 'description', content: this.description},
+        {name: 'description', content: this.description},
+        // Facebook / Open Graph
+        {property: 'og:url', content: this.ogUrl},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: this.title},
+        {property: 'og:description', content: this.description},
+        {property: 'og:image', content: 'https://maddevs.io/static/Open-Graph.png'},
       ];
     }
   },
