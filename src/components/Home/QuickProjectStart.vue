163cc5850566b851cb17330cd260fbaf149edcaf
Denisoed <denisod93@gmail.com>
2018-12-03 10:56:20 +0600

    MND-215 added structure for section Quick Project start

---
+++ src/components/Home/QuickProjectStart.vue
@@ -0,0 +1,40 @@
+<template>
+  <section id="quick" class="quick">
+    <div class="container">
+      <h2 class="sec-title">{{$t('quick-title')}}</h2>
+      <div class="quick-list">
+        <div class="quick-list_item">
+          <img src="" alt="">
+          <h4>Contact</h4>
+          <p>Submit your project request or project idea to us</p>
+        </div>
+        <div class="quick-list_item">
+          <img src="" alt="">
+          <h4>Analysis</h4>
+          <p>Submit your project request or project idea to us</p>
+        </div>
+        <div class="quick-list_item">
+          <img src="" alt="">
+          <h4>Proposal</h4>
+          <p>Submit your project request or project idea to us</p>
+        </div>
+        <div class="quick-list_item">
+          <img src="" alt="">
+          <h4>Assemble a team</h4>
+          <p>Submit your project request or project idea to us</p>
+        </div>
+        <div class="quick-list_item">
+          <img src="" alt="">
+          <h4>Getting started</h4>
+          <p>Submit your project request or project idea to us</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'quick-project-start'
+};
+</script>
