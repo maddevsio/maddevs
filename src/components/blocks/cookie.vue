2e61088299537eeaeabb29fdb0852d496dd0da95
Denisoed <denisod93@gmail.com>
2018-12-26 12:25:43 +0600

    Remove cookie plugin/added custom cookie block

---
+++ src/components/blocks/cookie.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="Cookie" id="cookie-notif" class="cookie-notif hide_cookie">
+    <p>{{ $t('cookie-message') }}</p>
+    <button ref="CookieButton" class="cookie-notif_button"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cookie',
+  props: ['data'],
+  updated() {
+    const cookie = this.$refs.Cookie;
+    const button = this.$refs.CookieButton;
+    if (cookie !== undefined && window.localStorage.getItem('cookie') !== 'false') {
+      cookie.classList.remove('hide_cookie');
+      button.addEventListener('click', () => {
+        localStorage.setItem('cookie', 'false');
+        cookie.classList.add('hide_cookie');
+      });
+    }
+  },
+};
+</script>
