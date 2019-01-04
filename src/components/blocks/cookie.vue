09fef6061cce6f8889414be4c0d1dd28c87f6f6f
Denisoed <denisod93@gmail.com>
2019-01-04 11:57:54 +0600

    MND-231 hide block cookie before render vue | added mobile styles

--- src/components/blocks/cookie.vue
+++ src/components/blocks/cookie.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="Cookie" id="cookie-notif" class="cookie-notif hide_cookie">
+  <div ref="Cookie" id="cookie-notif" style="display: none;" class="cookie-notif hide_cookie">
     <p>{{ $t('cookie-message') }}</p>
     <button ref="CookieButton" class="cookie-notif_button"></button>
   </div>
@@ -9,7 +9,7 @@
 export default {
   name: 'cookie',
   props: ['data'],
-  updated() {
+  mounted() {
     const cookie = this.$refs.Cookie;
     const button = this.$refs.CookieButton;
     if (cookie !== undefined && window.localStorage.getItem('cookie') !== 'false') {
@@ -21,4 +21,10 @@ export default {
     }
   },
 };
+document.addEventListener('DOMContentLoaded', () => {
+  const Cookie = document.getElementById('cookie-notif');
+  setTimeout(() => {
+    Cookie.removeAttribute('style');
+  }, 2000);
+});
 </script>
