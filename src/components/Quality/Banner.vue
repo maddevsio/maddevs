e354ada749a2ff9a92daf75a02fa50485e706b9d
Denisoed <denisod93@gmail.com>
2019-04-01 21:27:26 +0600

    MND-280 added start files and settings for Quality page

---
+++ src/components/Quality/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+  <section id="quality-banner" class="quality-banner">
+    <div class="container">
+      <h1>Мы несем ответственность за качество и юзабилити продукта</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/styles/variables';
+
+  .quality-banner {
+    width: 100%;
+    min-height: 610px;
+    position: relative;
+    background-color: $bgcolor--black;
+  }
+</style>
