5463f584848eba66cb8ebbdfd87568bccf2a0068
Denisoed <denisod93@gmail.com>
2019-04-02 19:23:48 +0600

    MND-280 added plugin for terminal

---
+++ src/components/blocks/terminal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="terminal">
+    <div class="terminal__head">
+      <button></button>
+      <button></button>
+      <button></button>
+    </div>
+    <div class="terminal__content">
+      <vue-terminal :command-list="commandList" :task-list="this.defaultTask" style="width:50%;margin:0 auto"></vue-terminal>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueTerminal from 'vue-terminal';
+
+export default {
+  name: 'Terminal',
+  components: { VueTerminal }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
