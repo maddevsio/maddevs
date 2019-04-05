681fc9b38b04a52f272da9adbe9f04099619b5fa
Denisoed <denisod93@gmail.com>
2019-04-05 15:37:29 +0600

    MND-269 moved style for ui components to component

--- components/ui/button-link.vue
+++ components/ui/button-link.vue
@@ -3,10 +3,44 @@
     {{ this.data.text }}
   </a>
 </template>
-<script>
 
+<script>
 export default {
   name: 'Banner',
   props: ['data']
 };
 </script>
+
+<style lang="scss" scoped>
+  @import '../../assets/styles/vars';
+
+  .mnd-button-link {
+    width: 340px;
+    max-height: 70px;
+    min-height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #ec2227;
+    border-radius: 5px;
+    color: #fff;
+    font-size: 24px;
+    font-family: 'MADEEvolveSans-medium', sans-serif;
+    font-weight: normal;
+    text-decoration: none;
+    padding: 20px 40px;
+    box-sizing: border-box;
+    transition: all .2s ease;
+    &:hover {
+      background-color: rgb(202, 34, 40);
+    }
+  }
+
+  @media only screen and (max-width: 480px) {
+    .mnd-button-link {
+      width: 288px;
+      height: 70px;
+    }
+  }
+</style>
+
