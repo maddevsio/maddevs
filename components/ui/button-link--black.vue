cdacb0baca7a70180c058ab8c37e6761ed455484
Denisoed <denisod93@gmail.com>
2019-04-05 15:37:29 +0600

    MND-269 moved style for ui components to component

--- components/ui/button-link--black.vue
+++ components/ui/button-link--black.vue
@@ -15,3 +15,43 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  @import '../../assets/styles/vars';
+
+  .mnd-button-link--black {
+    max-width: 340px;
+    max-height: 70px;
+    min-height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px 40px;
+    border-radius: 5px;
+    background-color: #1e1e1e;
+    font-size: 24px;
+    font-family: 'MADEEvolveSans-medium',
+    sans-serif;
+    color: $text-color--white;
+    text-decoration: none;
+    box-sizing: border-box;
+    margin: 0 auto;
+    margin-top: 40px;
+    transition: all .2s ease;
+    white-space: nowrap;
+
+    &:hover {
+      background-color: $bgcolor--black;
+    }
+
+    img {
+      margin-right: 15px;
+    }
+  }
+  @media only screen and (max-width: 480px) {
+    .mnd-button-link--black {  
+      width: 288px;
+      height: 70px;
+    }
+  }
+</style>
