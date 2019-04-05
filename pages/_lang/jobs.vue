1d3a341def36f8ef12515793fee3eb4041eb28b3
Denisoed <denisod93@gmail.com>
2019-04-05 16:25:59 +0600

    MND-269 connected jobs component

--- pages/_lang/jobs.vue
+++ pages/_lang/jobs.vue
@@ -1,23 +1,62 @@
 <template>
-  <div class="Content">
-    <div class="container">
-      <h1 class="Content__Title">
-        {{ $t('about.title') }}
-      </h1>
-      <p>{{ $t('about.introduction') }}</p>
-    </div>
-  </div>
+  <main class="main">
+    <Banner />
+    <Advantages />
+    <Growth />
+    <Employer />
+    <Intern />
+    <Employees />
+    <Jobs />
+    <Team />
+    <Contacts />
+  </main>
 </template>
 
 <script>
+import Banner from '@/components/Jobs/Banner';
+import Advantages from '@/components/Jobs/Advantages';
+import Growth from '@/components/Jobs/Growth';
+import Employer from '@/components/Jobs/Employer';
+import Intern from '@/components/Jobs/Intern';
+import Employees from '@/components/Jobs/Employees';
+import Jobs from '@/components/Jobs/Jobs';
+import Team from '@/components/Jobs/Team';
+import Contacts from '@/components/Contacts';
+
 export default {
-  head () {
+  name: 'main-jobs',
+  props: ['language'],
+  components: {
+    Banner,
+    Advantages,
+    Growth,
+    Employer,
+    Intern,
+    Employees,
+    Jobs,
+    Team,
+    Contacts
+  },
+  data() {
     return {
-      title: 'About Us - Nuxt.js',
+      title: this.$t('title-jobs'),
+      description: this.$t('meta-desc_jobs'),
+      ogUrl: this.$t('og_meta-url_jobs'),
+    };
+  },
+  head() {
+    return {
+      title: this.title,
       meta: [
-        { hid: 'description', name: 'description', content: 'About our company Nuxt.js ' }
+        {name: 'description', content: this.description},
+        // Facebook / Open Graph
+        {property: 'og:url', content: this.ogUrl},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: this.title},
+        {property: 'og:description', content: this.description},
+        {property: 'og:image', content: 'https://maddevs.io/static/Open-Graph.png'}
       ]
     }
   }
-}
+};
 </script>
