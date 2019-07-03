3811138cfb5d6152902143e4fb3ebafcd6f04b21
denisoed <denisoed@gmail.com>
2019-07-03 13:22:25 +0600

    MND-318 added mobile styles

--- components/Management/Transparency.vue
+++ components/Management/Transparency.vue
@@ -2,14 +2,27 @@
   <section>
     <div class="container">
       <h2 class="sec-title">Мы гарантируем прозрачность процессов</h2>
-      <p>Для этого мы создаем и настраиваем команде окружение для коммуникации, включая рабочие комнаты в <span>Slack</span> и системы для планирования задач, такие как <span>JIRA</span>, <span>Asana</span>, <span>BaseCamp</span>, <span>Trello</span>. </p>
+      <p>Для этого мы создаем и настраиваем команде окружение для коммуникации, включая рабочие комнаты в <span><slack /> Slack</span> и системы для планирования задач, такие как <span><jira /> JIRA,</span> <span><asana /> Asana,</span> <span><basecamp /> BaseCamp,</span> <span><trello /> Trello.</span> </p>
     </div>
   </section>
 </template>
 
 <script>
+import slack from '@/components/svg/slack';
+import jira from '@/components/svg/jira';
+import asana from '@/components/svg/asana';
+import basecamp from '@/components/svg/basecamp';
+import trello from '@/components/svg/trello';
+
 export default {
-  name: 'Transparency'
+  name: 'Transparency',
+  components: {
+    slack,
+    jira,
+    asana,
+    basecamp,
+    trello
+  }
 };
 </script>
 
@@ -27,9 +40,36 @@ export default {
       font-size: 24px;
       line-height: 1.46;
       font-family: 'MADEEvolveSans-regular', sans-serif;
+      margin-top: 30px;
 
       span {
+        display: inline-flex;
+        align-items: center;
+        white-space: nowrap;
         font-family: 'MADEEvolveSans-bold', sans-serif;
+        
+        svg {
+          margin-right: 4px;
+        }
+      }
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    section {
+      padding: 60px 0;
+
+      p {
+        font-size: 22px;
+        margin-top: 20px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 480px) {
+    section {
+      p {
+        font-size: 18px;
       }
     }
   }
