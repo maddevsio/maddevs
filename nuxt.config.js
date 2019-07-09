6d4202c2d42c3106876dccea166f88d64b264d6d
denisoed <denisoed@gmail.com>
2019-07-09 15:33:05 +0600

    MND-261 added route for management page in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ module.exports = {
     }
   ],
   generate: {
-    routes: ['/', '/en', '/en/jobs', '/ru', '/ru/jobs']
+    routes: ['/', '/en', '/en/jobs', '/en/management', '/ru', '/ru/jobs', '/ru/management']
   },
   css: [
     {
