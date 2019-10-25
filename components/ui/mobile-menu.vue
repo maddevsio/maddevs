a3697259c8134c86f8415b8e827c44c12e7c4f7c
denisoed <denisoed@gmail.com>
2019-10-25 14:53:11 +0600

    MND-340 removed Bishkek address | removed Group from Mad Devs Group Ltd

--- components/ui/mobile-menu.vue
+++ components/ui/mobile-menu.vue
@@ -18,10 +18,9 @@
         </router-link>
       </div>
       <div class="header-mobile-menu_address">
-        <p>{{$t('address-footer-kg')}}</p>
         <div class="header-mobile-menu_address-phonemail">
-          <a href="tel:+996551752797">+996 551 75 27 97</a>
-          <a href="mailto:team@maddevs.io">team@maddevs.io</a>
+          <p>{{$t('address-footer-uk')}}</p>
+          <a href="tel:+442039848555">+44 20 3984 8555</a>
         </div>
       </div>
       <div class="header-mobile-menu_soc-icons">
