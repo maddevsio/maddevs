86cb5ed25364c758599008882a71457814ac1ca7
denisoed <denisoed@gmail.com>
2019-11-07 08:21:38 +0600

    MND-343 added start files for Vacancies Page

---
+++ components/Vacancies/Banner.vue
@@ -0,0 +1,47 @@
+<template>
+  <section id="vacancies-banner" class="vacancies-banner">
+    <div class="container vacancies-banner_wrap">
+      <h1>👋 Привет, давай знакомиться.</h1>
+      <p>Здесь всегда полно работы - скучно точно не будет!</p>
+      <div class="vacancies-banner_type">
+        <span>Итак, ты</span>
+        <div class="vacancies-banner_staff-item" v-for="(type, i) in types" :key="i">{{ type }}</div>
+        <span>и ты в правильной команде!</span>
+      </div>
+      <UIButton @UIButtonClick="openForm" />
+    </div>
+  </section>
+</template>
+
+<script>
+import UIButton from '@/components/ui/UIButton';
+
+export default {
+  name: 'Banner',
+  components: { UIButton },
+  data() {
+    return {
+      types: ['Senior', 'Middle', 'Junior', 'Intern']
+    };
+  },
+  methods: {
+    openForm() {}
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/styles/vars';
+
+  .vacancies-banner {
+    width: 100%;
+    padding-top: 185px;
+
+    &_wrap {
+      text-align: center;
+    }
+  }
+
+  @media only screen and (max-width: 768px) {}
+</style>
+
---
+++ pages/_lang/vacancies.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="main">
+    <Banner />
+    <Contacts />
+  </main>
+</template>
+
+<script>
+import Banner from '@/components/Vacancies/Banner';
+import Contacts from '@/components/Contacts';
+
+export default {
+  name: 'main-vacancies',
+  props: ['language'],
+  components: {
+    Banner,
+    Contacts
+  },
+  data() {
+    return {
+      title: this.$t('title-jobs'),
+      description: this.$t('meta-desc_jobs'),
+      ogUrl: this.$t('og_meta-url_jobs'),
+    };
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { name: 'description', content: this.description },
+        // Facebook / Open Graph
+        { property: 'og:url', content: this.ogUrl },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: this.title },
+        { property: 'og:description', content: this.description },
+        { property: 'og:image', content: 'https://maddevs.io/static/Open-Graph.png' }
+      ]
+    }
+  }
+};
+</script>
