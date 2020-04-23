c5bc2f24c68b7495f4ca70d00de87623e10f1d84
AntonGrushkin <merynes345@gmail.com>
2020-04-23 16:19:37 +0600

    MND-524 added part of mark up for header

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,25 @@
 <template>
-  <header class="header" :class="{ 'header--dark': headerDark }">
+  <header class="header">
     <div class="container">
-      <div class="header-wrap">
-        <router-link :to="`/${this.lang}`">
-          <img src="../assets/img/common/logo.svg" alt="Logotype" class="header-logo">
-        </router-link>
-        <div class="header-wrap_right-block">
-          <nav class="header_links">
-            <router-link :to="`/${this.lang}/services`">Services</router-link>
+      <div class="header__header-content_wrap">
+        <div class="header__left-nav_bar">
+          <router-link :to="`/`">
+            <img src="../assets/img/common/logo.svg" alt="Logotype" class="header__header-logo">
+          </router-link>
+          <nav class="header__header-routes_links">
+            <router-link :to="`/`">About</router-link>
+            <router-link :to="`/services`">Services</router-link>
+            <router-link :to="`/projects`">Projects</router-link>
+            <router-link :to="`/careers`">Careers</router-link>
+            <router-link :to="`/blog`">Blog</router-link>
           </nav>
+        </div>
+        <div class="header__right-content">
+          <div class="header__soc-links_wrap">
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+          </div>
         </div>
       </div>
     </div>
@@ -17,85 +28,7 @@
 
 <script>
 export default {
-  name: 'main-header',
-  data() {
-    return {
-      lang: 'en',
-      mobileMenuActive: false,
-      headerDark: false
-    };
-  },
-  created() {
-    this.lang = this.$nuxt.$router.history.current.params.lang === undefined ? 'en' : this.$store.state.locale;
-    this.$store.commit('SET_LANG', this.lang);
-    if (process.browser) {
-      window.addEventListener('resize', this.toggleScrollOnBody);
-    }
-    this.changeColorHeader();
-  },
-  watch: {
-    '$route' (to, from) {
-      this.changeColorHeader();
-    }
-  },
-  beforeDestroy() {
-    if (process.browser) {
-      window.removeEventListener('resize', this.toggleScrollOnBody);
-    }
-  },
-  methods: {
-    changeColorHeader() {
-      if (this.$nuxt.$router.history.current.path.includes('management')) {
-        this.headerDark = true;
-      } else {
-        this.headerDark = false;
-      }
-    },
-    switchLanguage(locale) {
-      const beforePath = this.$nuxt.$router.history.current.path;
-      this.$store.commit('SET_LANG', locale);
-      this.lang = this.$store.state.locale;
-      let result = '';
-      result = beforePath.replace('/en', '');
-      result = result.replace('/ru', '');
-      if ( locale == 'ru' || locale == 'en' ) {
-        this.$nuxt.$router.replace({ path: '/' + locale + result });
-      } else {
-        if ( result == '/' ) {
-          this.$nuxt.$router.replace({ path: '/' + locale });
-        } else {
-          this.$nuxt.$router.replace({ path: '/' + locale + result });
-        }
-      }
-    },
-    toggleMobileMenu(bool) {
-      const chat = document.getElementById('tidio-chat');
-      if (this.mobileMenuActive === false) {
-        this.mobileMenuActive = true;
-        this.disableScrollOnBody();
-        chat.style.display = 'none';
-      } else {
-        chat.style.display = 'block';
-        this.mobileMenuActive = false;
-        this.enableScrollOnBody();
-      }
-    },
-    toggleScrollOnBody() {
-      if (window.innerWidth >= 480) {
-        this.enableScrollOnBody();
-      } else if (window.innerWidth < 480) {
-        if (self.mobileMenuActive === true) {
-          this.disableScrollOnBody();
-        }
-      }
-    },
-    disableScrollOnBody() {
-      document.body.classList.add('scrollDisabled');
-    },
-    enableScrollOnBody() {
-      document.body.classList.remove('scrollDisabled');
-    }
-  }
+  name: 'main-header'
 };
 </script>
 
@@ -108,49 +41,29 @@ export default {
     z-index: 2;
     padding-top: 30px;
 
-    &_links {
+    &__left-nav_bar {
+      display: flex;
+      align-items: center;
+    }
+
+    &__header-routes_links {
       position: relative;
+      margin-top: -32px;
+      padding-left: 55px;
 
       a {
-        color: $accent-color--red;
+        color: $text-color--grey;
         text-decoration: none;
-        margin-right: 15px;
-        font-size: 16px;
-        font-family: 'MADEEvolveSans-regular',
+        margin-right: 26px;
+        font-size: 18px;
+        font-family: 'Hoves-Regular',
         sans-serif;
       }
-
     }
 
-    &-wrap {
-      display: flex;
-      align-items: flex-start;
-      justify-content: space-between;
-
-      &_right-block {
-        display: flex;
-        align-items: center;
-        padding: 24px 0;
-      }
-    }
-
-    &-logo {
+    &__header-logo {
       width: 40px;
       height: 70px;
-    }
-  }
-
-  .header--dark {
-    .container {
-      max-width: 100%;
-
-      .header-wrap_right-block {
-        .header_links {
-          a {
-            color: $text-color--black;
-          }
-        }
-      }
     }
   }
 </style>
