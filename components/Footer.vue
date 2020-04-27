bbf595ac08008b10d424eb71bf254625d9caaf8f
AntonGrushkin <merynes345@gmail.com>
2020-04-27 20:55:23 +0600

    MND-518 added parth of markup for footer

---
+++ components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+			<div class="footer__content">
+				<div class="footer__text-content">
+					<div class="footer__title-wrap">
+						<h2 class="footer__title title">Get In Touch</h2>
+						<a class="footer__mailto-link" href="mailto:team@maddevs.io">team@maddevs.io</a>
+					</div>
+				</div>
+				<div class="footer__form-wrap">
+					<footerForm />
+				</div>
+			</div>
+			<div class="footer__bottom-links-line">
+				<p class="footer__company-name">© MadDevs - 2020</p>
+				<div class="footer__left-bottom_links">
+					<a href="#" class="footer__bottom-link">GDPR Compliance Commitment</a>
+					<a href="#" class="footer__bottom-link">Privacy Policy</a>
+					<a href="#" class="footer__bottom-link">Non-Disclosure Agreement (NDA)</a>
+				</div>
+			</div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import footerForm from '@/components/ui/footer-form';
+
+export default {
+  name: 'Footer',
+  components: {
+    footerForm
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/ui/footer-form.vue
@@ -0,0 +1,44 @@
+<template>
+	<form class="form"> 
+		<div class="fields-list">
+			<div class="field-item">
+				<p class="field-name">First name and surname</p>
+				<input type="text" class="filed-input" placeholder="John Smith">
+			</div>
+			<div class="field-item">
+				<p class="field-name">Work email</p>
+				<input type="email" class="filed-input" placeholder="your@company.co|">
+			</div>
+			<div class="field-item">
+				<p class="field-name">Project Info</p>
+				<input type="text" class="filed-input" placeholder="Describe your project...">
+			</div>
+		</div>
+		<div class="form-checkboxes">
+			<input class="form-checkbox-input" id="privacy-policy" type="checkbox" name="Privacy policy">
+  		<label class="form-checkbox-label" for="privacy-policy">I confirm that I have read and accepted Mad Devs’ Privacy Policy.</label>
+			<input class="form-checkbox-input" id="marketing-communications" type="checkbox" name="Marketing communications">
+  		<label class="form-checkbox-label" for="marketing-communications">I agree to get Mad Devs’ discount offers and other marketing communications.</label>
+		</div>
+		<buttonTrigger :buttonInnerText="buttonInnerText" />
+	</form>
+</template>
+
+<script>
+import buttonTrigger from '@/components/ui/button-trigger';
+
+export default {
+  name: 'footer-form',
+  components: {
+    buttonTrigger
+  },
+  data() {
+    return {
+      buttonInnerText: 'Order a project now'
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
