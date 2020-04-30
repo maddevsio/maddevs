06798d28fa58b00209b824d95a1d74b10cf44f7c
AntonGrushkin <merynes345@gmail.com>
2020-04-30 21:05:27 +0600

    MND-477 added mark for section

--- components/About/WeCreateItProjectFor.vue
+++ components/About/WeCreateItProjectFor.vue
@@ -14,13 +14,16 @@
           <li class="we-create-it-project__project-item">Transportation</li>
         </ul>
         <div class="we-create-it-project__country-list_wrap">
-          <div class="we-create-it-project__country-list_title">We work with customers in:</div>
+          <div class="we-create-it-project__country-list_title">We work with 
+            <br> customers in:
+          </div>
           <ul class="we-create-it-project__country-list">
             <li v-for="(country, i) in countries" :key="i" class="we-create-it-project__country-item">
               <img :src="require(`@/assets/img/Flags/${country.iconName}.svg`)" :alt="`${country.countryName}`" />
               {{ country.countryName }}
             </li>
           </ul>
+          <buttonTrigger :buttonInnerText="buttonInnerText" />
         </div>
       </div>
     </div>
@@ -28,9 +31,13 @@
 </template>
 
 <script>
+import buttonTrigger from '@/components/ui/button-trigger';
 
 export default {
   name: 'WeCreateItProjectFor',
+  components: {
+    buttonTrigger
+  },
   data() {
     return {
       countries: [
@@ -39,50 +46,51 @@ export default {
           countryName: 'USA'
         },
         {
-          iconName: 'united-kingdom',
-          countryName: 'UK'
-        },
-        {
-          iconName: 'austria',
-          countryName: 'Austria'
-        },
-        {
-          iconName: 'switzerland',
-          countryName: 'Switzerland'
-        },
-        {
-          iconName: 'belarus',
-          countryName: 'Belarus'
+          iconName: 'australia',
+          countryName: 'Australia'
         },
         {
           iconName: 'vietnam',
           countryName: 'Vietnam'
         },
         {
-          iconName: 'russia',
-          countryName: 'Russia'
-        },
-        {
-          iconName: 'kazakhstan',
-          countryName: 'Kazakhstan'
+          iconName: 'united-kingdom',
+          countryName: 'UK'
         },
         {
           iconName: 'poland',
           countryName: 'Poland'
         },
         {
-          iconName: 'australia',
-          countryName: 'Australia'
+          iconName: 'russia',
+          countryName: 'Russia'
+        },
+        {
+          iconName: 'austria',
+          countryName: 'Austria'
         },
         {
           iconName: 'france',
           countryName: 'France'
         },
         {
+          iconName: 'belarus',
+          countryName: 'Belarus'
+        },
+        {
+          iconName: 'switzerland',
+          countryName: 'Switzerland'
+        },
+        {
           iconName: 'singapore',
           countryName: 'Singapore'
+        },
+        {
+          iconName: 'kazakhstan',
+          countryName: 'Kazakhstan'
         }
-      ]
+      ],
+      buttonInnerText: 'Get case studies'
     };
   }
 };
@@ -91,32 +99,277 @@ export default {
 <style lang="scss" scoped>
   @import '../../assets/styles/vars';
 
-  .it-consulting {
-    padding-top: 127px;
-    padding-bottom: 151px;
-    background-color: $bgcolor--white;
+  .we-create-it-project {
+    padding-top: 105px;
 
-    &__flex-wrapper {
-      margin-top: -40px;
+    &__content-wrap {
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-end;
     }
 
     &__main-title {
-      color: $text-color--black;
+      color: $text-color--red;
+      padding-bottom: 100px;
+    }
+
+    &__projects-type_list {
+      padding-right: 60px;
+    }
+
+    &__country-list_wrap {
+      max-width: 770px;
+
+      button {
+        width: 100%;
+        margin-bottom: 15px;
+        color: $text-color--red;
+        border: 1px solid $border-color--red;
+      }
+    }
+
+    &__country-list_title {
+      padding-bottom: 30px;
+      color: $text-color--grey;
+      font-size: 50px;
+
+      br {
+        display: none;
+      }
+    }
+
+    &__country-list {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-column-gap: 83px;
+      grid-row-gap: 10px;
+      padding-bottom: 60px;
+    }
+
+    &__country-item {
+      display: flex;
+      color: $text-color--grey;
+      font-size: 32px;
+
+      img {
+        padding-right: 10px;
+      }
+    }
+
+    &__project-item,
+    &__country-list_title,
+    &__country-item {
+      font-family: 'Hoves-Bold';
+    }
+
+    &__project-item {
+      color: $text-color--white;
+      font-size: 70px;
+    }
+  }
+
+  @media only screen and (max-width: 1440px) {
+    .we-create-it-project {
+      &__country-list_wrap {
+        max-width: 680px;
+      }
+
+      &__country-list {
+        grid-column-gap: 40px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 1340px) {
+    .we-create-it-project {
+      &__country-list_wrap {
+        max-width: 625px;
+      }
+
+      &__country-list {
+        grid-column-gap: 30px;
+      }
+
+      &__country-list_title {
+        font-size: 47px;
+      }
+
+      &__projects-type_list {
+        padding-right: 40px;
+      }
+
+      &__country-item {
+        font-size: 30px;
+      }
+
+      &__project-item {
+        font-size: 68px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 1260px) {
+    .we-create-it-project {
+      &__country-list_wrap {
+        max-width: 575px;
+      }
+
+      &__country-list {
+        grid-column-gap: 25px;
+      }
+
+      &__country-list_title {
+        font-size: 43px;
+      }
+
+      &__country-item {
+        font-size: 28px;
+      }
+
+      &__project-item {
+        font-size: 66px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 1180px) {
+    .we-create-it-project {
+      &__country-list_wrap {
+        max-width: 485px;
+      }
+
+      &__country-list_title {
+        font-size: 35px;
+      }
+
+      &__country-list {
+        grid-column-gap: 20px;
+      }
+
+      &__country-item {
+        font-size: 22px;
+      }
+
+      &__project-item {
+        font-size: 59px;
+      }
     }
   }
 
   @media only screen and (max-width: 1024px) {
-    .it-consulting {
-      &__flex-wrapper {
-        flex-wrap: wrap;
+    .we-create-it-project {
+      &__content-wrap {
+        flex-direction: column;
+        align-items: flex-start;
+      }
+
+      &__country-list_wrap {
+        width: 100%;
+        max-width: 100%;
+        padding-top: 40px;
+      }
+
+      &__country-list {
+        width: max-content;
+      }
+
+      &__country-item {
+        &:nth-child(3n+3) {
+          margin-left: -20px;
+        }
+      }
+    }
+  }
+
+  @media only screen and (max-width: 640px) {
+    .we-create-it-project {
+      &__main-title {
+        font-size: 95px;
+
+        br {
+          display: none;
+        }
       }
     }
   }
 
   @media only screen and (max-width: 520px) {
-    .it-consulting {
-      padding-top: 60px;
-      padding-bottom: 75px;
+    .we-create-it-project {
+      &__main-title {
+        font-size: 80px;
+      }
+
+      &__country-list_title {
+        font-size: 32px;
+      }
+
+      &__country-list {
+        grid-template-columns: repeat(2, 1fr);
+      }
+
+      &__country-item {
+        font-size: 25px;
+
+        &:nth-child(3n+3) {
+          margin-left: 0;
+        }
+
+        &:nth-child(4) {
+          grid-row-start: 2;
+        }
+        
+        &:nth-child(2) {
+					grid-row-start: 3;
+        }
+        
+        &:nth-child(3) {
+          grid-row-start: 4;
+        }
+
+        &:nth-child(10) {
+          grid-row-start: 5;
+        }
+
+        &:nth-child(6) {
+          grid-row-start: 6;
+        }
+
+        &:nth-child(5) {
+          grid-row-start: 3;
+        }
+
+        &:nth-child(11) {
+          grid-row-start: 4;
+        }
+      }
+
+      &__project-item {
+        font-size: 50px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 440px) {
+    .we-create-it-project {
+      &__main-title {
+        font-size: 48px;
+      }
+
+      &__country-list_title {
+        font-size: 28px;
+
+        br {
+          display: block;
+        }
+      }
+
+      &__country-item {
+        font-size: 18px;
+      }
+
+      &__project-item {
+        font-size: 41px;
+      }
     }
   }
 </style>
