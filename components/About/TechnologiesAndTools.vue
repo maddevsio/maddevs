af4f99d3eefd447b67a61aeb1decc2a540e19267
AntonGrushkin <merynes345@gmail.com>
2020-05-01 14:08:29 +0600

    MND-484 added mark up for Technologies & Tools section

---
+++ components/About/TechnologiesAndTools.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id="technologies-and-tools" class="TechnologiesAndTools">
+    <div class="container">
+      <div class="technologiesAndTools__content-wrap">
+        <h2 class="we-create-it-project__main-title main-title">
+            Technologies <br> & Tools
+        </h2>
+        <div class="technologiesAndTools__technologies-list"></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TechnologiesAndTools'
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/styles/vars';
+</style>
