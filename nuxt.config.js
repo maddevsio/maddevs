65e21200b744fe093a2fd82e5d1c454e8e5d64d7
AntonGrushkin <merynes345@gmail.com>
2020-05-08 17:06:28 +0600

    MND-658 change meta title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Mad Devs - IT Outsourcing Company in UK',
+    title: 'Mad Devs',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
