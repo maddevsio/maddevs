43f3f3f623ba0991424be62a9d007e572500e854
AntonGrushkin <merynes345@gmail.com>
2020-05-13 15:07:37 +0600

    MND-619 added plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ module.exports = {
     '~/plugins/i18n.js',
     '~/plugins/vue-scrollto.js',
     '~/plugins/google-analytics.js',
+    '~/plugins/vue-js-modal.js',
     {
       src: '~plugins/slick.js',
       ssr: false
