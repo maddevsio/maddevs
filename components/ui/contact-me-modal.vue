0e3a85d759864c5f1bbb9de9def375c7bc869752
AntonGrushkin <merynes345@gmail.com>
2020-05-15 14:10:36 +0600

    MND-619 added media styles for laptop

--- components/ui/contact-me-modal.vue
+++ components/ui/contact-me-modal.vue
@@ -1,29 +1,29 @@
 <template>
   <modal name="contact-me" :clickToClose="false">
     <img src="../../assets/img/common/close-icon.svg" class="close-modal" alt="Close modal" @click="$modal.hide('contact-me')">
-    <form class="footer-form form"> 
+    <form class="form"> 
       <div class="fields-list">
-        <ValidationProvider class="field-item" rules="required" v-slot="{ errors }">
-          <p class="field-name required">Full Name</p>
-          <input type="text" class="entry-field" placeholder="John Smith" v-model="fullName">
-          <span class="error-text">{{ errors[0] }}</span>
+        <ValidationProvider class="modal-field-item field-item" rules="required" v-slot="{ errors }">
+          <p class="modal-field-name field-name required">Full Name</p>
+          <input type="text" class="modal-entry-field entry-field" placeholder="John Smith" v-model="fullName">
+          <span class="modal-error-text error-text">{{ errors[0] }}</span>
         </ValidationProvider>
-        <ValidationProvider class="field-item" rules="email|required" v-slot="{ errors }">
-          <p class="field-name required">Work email</p>
-          <input type="text" class="entry-field" placeholder="your@mail.com" v-model="email">
-          <span class="error-text">{{ errors[0] }}</span>
+        <ValidationProvider class="modal-field-item field-item" rules="email|required" v-slot="{ errors }">
+          <p class="modal-field-name field-name required">Work email</p>
+          <input type="text" class="modal-entry-field entry-field" placeholder="your@mail.com" v-model="email">
+          <span class="modal-error-text error-text">{{ errors[0] }}</span>
         </ValidationProvider>
-        <ValidationProvider class="field-item" rules="integer" v-slot="{ errors }">
-          <p class="field-name">Phone number</p>
-          <input type="text" class="entry-field" placeholder="Enter phone number" v-model="phoneNumber">
-          <span class="error-text">{{ errors[0] }}</span>
+        <ValidationProvider class="modal-field-item field-item" rules="integer" v-slot="{ errors }">
+          <p class="modal-field-name field-name">Phone number</p>
+          <input type="text" class="modal-entry-field entry-field" placeholder="Enter phone number" v-model="phoneNumber">
+          <span class="modal-error-text error-text">{{ errors[0] }}</span>
         </ValidationProvider>
       </div>
       <formCheckboxes 
         v-on:getPrivacyCheckboxState="getPrivacyCheckboxState($event)"
         v-on:getDiscountOffersCheckboxState="getDiscountOffersCheckboxState($event)"
       />
-      <button :class="`button-default red-text-and-border ${agreeWithPrivacyPolicy ? '' : 'disabled'}`">Сontact Me</button>
+      <button :class="`modal-button-default button-default red-text-and-border ${agreeWithPrivacyPolicy ? '' : 'disabled'}`">Сontact Me</button>
     </form>
   </modal>
 </template>
