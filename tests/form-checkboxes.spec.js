a82a790c8a506e856f1bc115b099e3334e31f111
AntonGrushkin <merynes345@gmail.com>
2020-05-18 14:49:36 +0600

    MND-619 added tests for form-checkboxes

---
+++ tests/form-checkboxes.spec.js
@@ -0,0 +1,50 @@
+import {
+  mount
+} from '@vue/test-utils';
+import formCheckboxes from '@/components/ui/form-checkboxes';
+
+describe('Form checkboxes', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(formCheckboxes);
+  });
+
+  // ------ IMPORTANT ----- //
+  test('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+  
+  test('renders correctly', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+  // --------------------- //
+
+  it('has a functions', () => {
+    expect(
+      typeof formCheckboxes.methods.privacyCheckboxChangeState && 
+      typeof formCheckboxes.methods.discountOffersCheckboxChangeState
+    ).toBe('function');
+  });
+
+  test('emits called with arguments', () => {
+    const firstCheckbox = {
+      target: {
+        checked: true
+      }
+    }
+
+    const secondCheckbox = {
+      target: {
+        checked: false
+      }
+    }
+
+    wrapper.vm.privacyCheckboxChangeState(firstCheckbox);
+    wrapper.vm.discountOffersCheckboxChangeState(secondCheckbox);
+
+    expect(wrapper.emitted()).toBeTruthy();
+    expect(wrapper.emitted().getPrivacyCheckboxState).toEqual([[true]]);
+    expect(wrapper.emitted().getDiscountOffersCheckboxState).toEqual([[false]]);
+  })
+});
