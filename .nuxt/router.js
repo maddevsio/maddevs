8b626348c961ce595c18fbc241c97670a48389de
AntonGrushkin <merynes345@gmail.com>
2020-05-18 15:46:41 +0600

    MND-619 fixed tests error

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,9 +3,11 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _6ad9b144 = () => interopDefault(import('../pages/projects.vue' /* webpackChunkName: "pages/projects" */))
 const _afeee57c = () => interopDefault(import('../pages/services.vue' /* webpackChunkName: "pages/services" */))
 const _638a5c9e = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 const _491ace48 = () => interopDefault(import('../pages/_lang/index.vue' /* webpackChunkName: "pages/_lang/index" */))
+const _f5e4cb40 = () => interopDefault(import('../pages/_lang/projects.vue' /* webpackChunkName: "pages/_lang/projects" */))
 const _62830044 = () => interopDefault(import('../pages/_lang/services.vue' /* webpackChunkName: "pages/_lang/services" */))
 
 // TODO: remove in Nuxt 3
@@ -25,6 +27,10 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/projects",
+    component: _6ad9b144,
+    name: "projects"
+  }, {
     path: "/services",
     component: _afeee57c,
     name: "services"
@@ -36,6 +42,10 @@ export const routerOptions = {
     path: "/:lang",
     component: _491ace48,
     name: "lang"
+  }, {
+    path: "/:lang/projects",
+    component: _f5e4cb40,
+    name: "lang-projects"
   }, {
     path: "/:lang/services",
     component: _62830044,
