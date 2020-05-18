99f53b7fc0c02e851ced069b6cc2e0fd92a4eff9
AntonGrushkin <merynes345@gmail.com>
2020-05-18 15:06:35 +0600

    MND-619 added new tests

---
+++ tests/buttonTrigger.spec.js
@@ -0,0 +1,37 @@
+import {
+  mount
+} from '@vue/test-utils';
+import buttonTrigger from '@/components/ui/button-trigger';
+
+describe('Form checkboxes', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(buttonTrigger, {
+      propsData: {
+        buttonInnerText: 'Some button name',
+        modalWindowName: 'Some modal window name'
+      }
+    });
+  });
+
+  // ------ IMPORTANT ----- //
+  test('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+  
+  test('renders correctly', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+  // --------------------- //
+
+  test('correctly props data', () => {
+    expect(wrapper.props().buttonInnerText).toBe('Some button name');
+    expect(wrapper.props().modalWindowName).toBe('Some modal window name');
+  });
+
+  test('sets the correctly button name', () => {
+    let button = wrapper.find('.button-default');
+    expect(button.text()).toBe('Some button name');
+  });
+});
