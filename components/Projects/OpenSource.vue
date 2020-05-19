79667bf2df08ced565eb007e0e91de016cbcfd6a
Toktonazar Shatmanaliev <tshatmanaliev@gmail.com>
2020-05-19 12:35:53 +0600

    MND-566 Layout of Open Source Projects

--- components/Projects/OpenSource.vue
+++ components/Projects/OpenSource.vue
@@ -6,16 +6,88 @@
           Our code - available in open source - is used by many other companies.
         </h3>
         <h4 class="open-source__sub-title sub-title">
-          70+ of Mad Devs' pet projects have been shared with the community via GitHub.
+          70+ of Mad Devs' pet projects have been shared with the community via
+          GitHub.
         </h4>
       </div>
-      <div class="open-source__projects"></div>
+      <div class="open-source__projects">
+        <div class="open-source__project">
+          <div class="open-source__content-wrap">
+            <img
+              class="open-source__logo"
+              src="../../assets/img/Projects/svg/OpenSource/mad_location.svg"
+              alt="img"
+            />
+            <span class="open-source__industry">Logistics</span>
+          </div>
+          <p class="open-source__paragraph paragraph">
+            А library for GPS and accelerometer data "fusion" with a Kalman
+            filter.
+          </p>
+        </div>
+
+        <div class="open-source__project">
+          <div class="open-source__content-wrap">
+            <img
+              class="open-source__logo"
+              src="../../assets/img/Projects/svg/OpenSource/mad_location.svg"
+              alt="img"
+            />
+            <span class="open-source__industry">Logistics</span>
+          </div>
+          <p class="open-source__paragraph paragraph">
+            А library for GPS and accelerometer data "fusion" with a Kalman
+            filter.
+          </p>
+        </div>
+        <div class="open-source__project">
+          <div class="open-source__content-wrap">
+            <img
+              class="open-source__logo"
+              src="../../assets/img/Projects/svg/OpenSource/mad_location.svg"
+              alt="img"
+            />
+            <span class="open-source__industry">Logistics</span>
+          </div>
+          <p class="open-source__paragraph paragraph">
+            А library for GPS and accelerometer data "fusion" with a Kalman
+            filter.
+          </p>
+        </div>
+        <div class="open-source__project">
+          <div class="open-source__content-wrap">
+            <img
+              class="open-source__logo"
+              src="../../assets/img/Projects/svg/OpenSource/mad_location.svg"
+              alt="img"
+            />
+            <span class="open-source__industry">Logistics</span>
+          </div>
+          <p class="open-source__paragraph paragraph">
+            А library for GPS and accelerometer data "fusion" with a Kalman
+            filter.
+          </p>
+        </div>
+        <div class="open-source__project">
+          <div class="open-source__content-wrap">
+            <img
+              class="open-source__logo"
+              src="../../assets/img/Projects/svg/OpenSource/mad_location.svg"
+              alt="img"
+            />
+            <span class="open-source__industry">Logistics</span>
+          </div>
+          <p class="open-source__paragraph paragraph">
+            А library for GPS and accelerometer data "fusion" with a Kalman
+            filter.
+          </p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-
 export default {
   name: 'OpenSource'
 };
@@ -36,6 +108,7 @@ export default {
 
   &__sub-title {
     color: $text-color--grey;
+
     &::after {
       content: url('../../assets/img/Projects/svg/github.svg');
       position: relative;
@@ -43,6 +116,42 @@ export default {
       top: 25px;
     }
   }
-}
 
+  &__projects {
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    margin-top: 76px;
+  }
+
+  &__project {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    background: $bgcolor--white;
+    height: 270px;
+    width: 33%;
+    box-sizing: border-box;
+    padding: 43px 30px 64px 43px;
+    border-right: 2px solid $border-color--grey-light;
+    border-bottom: 2px solid $border-color--grey-light;
+  }
+
+  &__content-wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 36px;
+  }
+
+  &__logo {
+  }
+
+  &__industry {
+  }
+
+  &__paragraph {
+    padding-right: 61px;
+  }
+}
 </style>
