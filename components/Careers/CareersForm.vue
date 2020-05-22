7d5a13d0ab97a2a223d230fa48c7e2231c1df870
Toktonazar Shatmanaliev <tshatmanaliev@gmail.com>
2020-05-22 18:18:22 +0600

    MND-678 - Add responsive fonts and global border size - Layout of input and labels

--- components/Careers/CareersForm.vue
+++ components/Careers/CareersForm.vue
@@ -7,6 +7,45 @@
           src="@/assets/img/Careers/svg/careers_logo.svg"
           alt="Careers Background Image"
         />
+        <form class="careers__form">
+          <label class="careers__form-name-label form-text"
+            >Hello, my name is</label
+          >
+          <input
+            class="careers__form-name-input form-text"
+            type="text"
+            placeholder="|John Smith"
+          />
+          <h4 class="careers__form-description form-text">
+            I want to work for you as a
+            <span class="careers__form-description--grey"
+              >desired position.</span
+            >
+          </h4>
+          <h4 class="careers__form-description form-text">
+            You can also consider me for your other
+          </h4>
+          <h4 class="careers__form-description form-text email-title">
+            Please reply to
+            <input
+              class="careers__form-email-input form-text"
+              type="text"
+              placeholder="your@mail.com"
+            />
+          </h4>
+          <h4 class="careers__form-description form-text">
+            To get more information on my skills, please
+          </h4>
+          <ul class="careers__form-list form-text">
+            <li class="careers__form-list-item">
+              – check out my
+              <a href="#" class="careers__linkedin-link">LinkedIn profile</a> OR
+            </li>
+            <li class="careers__form-list-item">
+              – <a href="#" class="careers__cv">find attached my CV.</a>
+            </li>
+          </ul>
+        </form>
       </div>
     </div>
   </section>
@@ -25,11 +64,14 @@ export default {
 @import '../../assets/styles/vars';
 
 .careers {
-  padding: 63px 102px 102px 92px;
-  background: $bgcolor--grey-light;
-
   .container {
     position: relative;
+  }
+
+  &__wrapper {
+    border-radius: $form-border-radius;
+    background: $bgcolor--grey-light;
+    padding: 63px 102px 102px 92px;
   }
 
   &__background-logo {
@@ -37,6 +79,40 @@ export default {
     bottom: 0;
     right: 0;
     height: auto;
+  }
+
+  &__form-name-label {
+  }
+
+  .email-title {
+    margin: 80px 0;
+  }
+
+  &__form-name-input,
+  &__form-email-input {
+    background-color: transparent;
+    border: 0px solid;
+    height: 65px;
+    width: 310px;
+    color: $text-color--grey;
+  }
+
+  &__form-description {
+    font-family: 'Hoves-Regular';
+    color: $text-color--black;
+    font-weight: 500;
+  }
+
+  &__linkedin-link,
+  &__cv {
+    color: $text-color--grey;
+  }
+
+  &__linkedin-link {
+    text-decoration: none;
+  }
+
+  &__cv {
   }
 }
 
