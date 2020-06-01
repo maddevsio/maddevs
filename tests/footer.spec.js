712a5ba4b5f4d67eaef08369b7c7ef4eed6a2b6a
Toktonazar Shatmanaliev <tshatmanaliev@gmail.com>
2020-06-01 10:25:42 +0600

    MND-667 Add tests for GDPR page and Footer component

--- tests/footer.spec.js
+++ tests/footer.spec.js
@@ -1,14 +1,17 @@
-import {
-  mount
-} from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import Footer from '@/components/Footer';
 
 describe('Footer', () => {
   let wrapper;
 
   beforeEach(() => {
+    global.$nuxt = {
+      $route: {
+        name: '/'
+      }
+    };
     wrapper = mount(Footer, {
-      stubs: ['ValidationProvider', 'ValidationObserver']
+      stubs: ['ValidationProvider', 'ValidationObserver', 'router-link']
     });
   });
 
@@ -16,9 +19,34 @@ describe('Footer', () => {
   test('is a Vue instance', () => {
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
-  
+
   test('renders correctly', () => {
     expect(wrapper.element).toMatchSnapshot();
   });
   // --------------------- //
+
+  it('correctly sets the route when mounted', () => {
+    expect(wrapper.vm.$data.currentPageName).toBe('/');
+  });
+
+  test('correctly sets the current pages name and class', () => {
+    const $route = {
+      path: '/gdpr'
+    };
+
+    wrapper = mount(Footer, {
+      mocks: {
+        $route
+      }
+    });
+
+    wrapper.vm.$data.currentPageName = 'gdpr';
+    expect(wrapper.vm.$route.path).toBe('/gdpr');
+    expect(wrapper.vm.$data.currentPageName).toBe('gdpr');
+
+    let wrapperClass = wrapper.findAll('.gdpr');
+    expect(wrapperClass).toEqual({
+      selector: '.gdpr'
+    });
+  });
 });
