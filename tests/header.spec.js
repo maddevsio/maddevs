1f90be95c894fa85d92363043322f24cd1183e50
AntonGrushkin <merynes345@gmail.com>
2020-06-23 15:18:56 +0600

    MND-819 added new styles and logic for header

--- tests/header.spec.js
+++ tests/header.spec.js
@@ -23,87 +23,32 @@ describe('Header', () => {
   // --------------------- //
 
   it('has a functions', () => {
-    expect(typeof wrapper.vm.mainPageHandleScroll).toBe('function');
-    expect(typeof wrapper.vm.servicesPageHandleScroll).toBe('function');
-    expect(typeof wrapper.vm.projectsPageHandleScroll).toBe('function');
+    expect(typeof wrapper.vm.handleScroll).toBe('function');
   });
 
   it('sets the correct default data', () => {
     expect(typeof Header.data).toBe('function');
     const defaultData = Header.data();
     expect(defaultData.modalWindowName).toBe('contact-me-modal');
-    expect(defaultData.headerIsWhite).toBe(false);
   });
 
-  it('call function mainPageHandleScroll with different params for: getBoundingClientRect()', () => {
-    global.$nuxt = {
-      $route: {
-        name: 'index'
+  it('headerLogoTextDisplayState should to be equal true', () => {
+    const event = {
+      target: {
+        scrollTop: 100
       }
     };
-    document.body.innerHTML =
-    `<div>
-      <div id="customer-testimonials"></div>
-      <div id="meet-our-experts"></div>
-      <div id="why-us"></div>
-     </div>`;
-
-    // if condition in functions correctly 
-    Element.prototype.getBoundingClientRect = jest.fn(() => Object({top: 0, bottom: 0}));
-    wrapper.vm.mainPageHandleScroll();
-    expect(wrapper.vm.$data.headerIsWhite).toBe(true);
-
-    // if condition in functions not correctly
-    Element.prototype.getBoundingClientRect = jest.fn(() => Object({top: 12, bottom: 34}));
-    wrapper.vm.mainPageHandleScroll();
-    expect(wrapper.vm.$data.headerIsWhite).toBe(false);
+    wrapper.vm.handleScroll(event);
+    expect(wrapper.vm.$data.headerLogoTextDisplayState).toBe(true);
   });
 
-  it('call function servicesPageHandleScroll with different params for: getBoundingClientRect()', () => {
-    global.$nuxt = {
-      $route: {
-        name: 'services'
+  it('headerLogoTextDisplayState should to be equal false', () => {
+    const event = {
+      target: {
+        scrollTop: 0
       }
     };
-    document.body.innerHTML =
-    `<div>
-      <div id="infrastructure-optimisation"></div>
-      <div id="it-consulting"></div>
-     </div>`;
-
-    // if condition in functions correctly 
-    Element.prototype.getBoundingClientRect = jest.fn(() => Object({top: 0, bottom: 0}));
-    wrapper.vm.servicesPageHandleScroll();
-    expect(wrapper.vm.$data.headerIsWhite).toBe(true);
-
-    // if condition in functions not correctly
-    Element.prototype.getBoundingClientRect = jest.fn(() => Object({top: 12, bottom: 34}));
-    wrapper.vm.servicesPageHandleScroll();
-    expect(wrapper.vm.$data.headerIsWhite).toBe(false);
-  });
-
-  it('call function projectsPageHandleScroll with different params for: getBoundingClientRect()', () => {
-    global.$nuxt = {
-      $route: {
-        name: 'projects'
-      }
-    };
-    document.body.innerHTML = '<div id="open-source"></div>';
-
-    // if condition in functions correctly 
-    Element.prototype.getBoundingClientRect = jest.fn(() => Object({top: 0, bottom: 0}));
-    wrapper.vm.projectsPageHandleScroll();
-    expect(wrapper.vm.$data.headerIsWhite).toBe(true);
-
-    // if condition in functions not correctly
-    Element.prototype.getBoundingClientRect = jest.fn(() => Object({top: 12, bottom: 34}));
-    wrapper.vm.projectsPageHandleScroll();
-    expect(wrapper.vm.$data.headerIsWhite).toBe(false);
-  });
-
-  it('headerIsWhite should equal false after call function', () => {
-    wrapper.vm.$data.headerIsWhite = true;
-    wrapper.vm.listenCareersLinkClick();
-    expect(wrapper.vm.$data.headerIsWhite).toBe(false);
+    wrapper.vm.handleScroll(event);
+    expect(wrapper.vm.$data.headerLogoTextDisplayState).toBe(false);
   });
 });
