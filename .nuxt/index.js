12ea49cc1ca9f3ee4bac1eb1476a0bf29e9ebdcd
AntonGrushkin <merynes345@gmail.com>
2020-06-30 15:32:02 +0600

    MDW-863 added minor fix in markup

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -12,8 +12,7 @@ import { createStore } from './store.js'
 
 /* Plugins */
 
-import nuxt_plugin_plugin_0d21699e from 'nuxt_plugin_plugin_0d21699e' // Source: ./components/plugin.js (mode: 'all')
-import nuxt_plugin_axios_44ce91b4 from 'nuxt_plugin_axios_44ce91b4' // Source: ./axios.js (mode: 'all')
+import nuxt_plugin_axios_f0f0c980 from 'nuxt_plugin_axios_f0f0c980' // Source: ./axios.js (mode: 'all')
 import nuxt_plugin_i18n_1fba523a from 'nuxt_plugin_i18n_1fba523a' // Source: ../client/plugins/i18n.js (mode: 'all')
 import nuxt_plugin_vuescrollto_44ce9a1c from 'nuxt_plugin_vuescrollto_44ce9a1c' // Source: ../client/plugins/vue-scrollto.js (mode: 'all')
 import nuxt_plugin_googleanalytics_2bcb2ee2 from 'nuxt_plugin_googleanalytics_2bcb2ee2' // Source: ../client/plugins/google-analytics.js (mode: 'all')
@@ -50,7 +49,7 @@ Vue.use(Meta, {"keyName":"head","attribute":"data-n-head","ssrAttribute":"data-n
 
 const defaultTransition = {"name":"page","mode":"out-in","appear":false,"appearClass":"appear","appearActiveClass":"appear-active","appearToClass":"appear-to"}
 
-async function createApp(ssrContext, config = {}) {
+async function createApp (ssrContext) {
   const router = await createRouter(ssrContext)
 
   const store = createStore(ssrContext)
@@ -139,7 +138,7 @@ async function createApp(ssrContext, config = {}) {
     ssrContext
   })
 
-  function inject(key, value) {
+  const inject = function (key, value) {
     if (!key) {
       throw new Error('inject(key, value) has no key provided')
     }
@@ -150,10 +149,6 @@ async function createApp(ssrContext, config = {}) {
     key = '$' + key
     // Add into app
     app[key] = value
-    // Add into context
-    if (!app.context[key]) {
-      app.context[key] = value
-    }
 
     // Add into store
     store[key] = app[key]
@@ -176,9 +171,6 @@ async function createApp(ssrContext, config = {}) {
     })
   }
 
-  // Inject runtime config as $config
-  inject('config', config)
-
   if (process.client) {
     // Replace store state before plugins execution
     if (window.__NUXT__ && window.__NUXT__.state) {
@@ -186,21 +178,10 @@ async function createApp(ssrContext, config = {}) {
     }
   }
 
-  // Add enablePreview(previewData = {}) in context for plugins
-  if (process.static && process.client) {
-    app.context.enablePreview = function (previewData = {}) {
-      app.previewData = Object.assign({}, previewData)
-      inject('preview', previewData)
-    }
-  }
   // Plugin execution
 
-  if (typeof nuxt_plugin_plugin_0d21699e === 'function') {
-    await nuxt_plugin_plugin_0d21699e(app.context, inject)
-  }
-
-  if (typeof nuxt_plugin_axios_44ce91b4 === 'function') {
-    await nuxt_plugin_axios_44ce91b4(app.context, inject)
+  if (typeof nuxt_plugin_axios_f0f0c980 === 'function') {
+    await nuxt_plugin_axios_f0f0c980(app.context, inject)
   }
 
   if (typeof nuxt_plugin_i18n_1fba523a === 'function') {
@@ -225,13 +206,6 @@ async function createApp(ssrContext, config = {}) {
 
   if (process.client && typeof nuxt_plugin_vuejsmodal_a01fef9e === 'function') {
     await nuxt_plugin_vuejsmodal_a01fef9e(app.context, inject)
-  }
-
-  // Lock enablePreview in context
-  if (process.static && process.client) {
-    app.context.enablePreview = function () {
-      console.warn('You cannot call enablePreview() outside a plugin.')
-    }
   }
 
   // If server-side, wait for async component to be resolved first
