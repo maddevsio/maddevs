0e7fdafe19adbb86608bd34599f8deebf5aba23c
AntonGrushkin <merynes345@gmail.com>
2020-06-30 18:59:08 +0600

    MDW-863 added new fixes in markup

--- client/containers/ModalContainer.vue
+++ client/containers/ModalContainer.vue
@@ -44,10 +44,14 @@ export default {
   methods: {
     hideModal() {
       this.isEmailSent = false;
-      this.$nuxt.$emit('tooglePageScrollBar', false);
+      this.enableScrollOnBody();
     },
     onClose() {
       this.$modal.hide(this.$props.name);
+      this.enableScrollOnBody();
+    },
+    enableScrollOnBody() {
+      document.body.classList.remove('scrollDisabled');
     }
   }
 };
@@ -110,7 +114,7 @@ export default {
   @media only screen and (max-width: 640px) {
 		/deep/.modal_container {
       width: 100% !important;
-      height: 100% !important;
+      height: 100vh !important;
       padding: 63px 0 0 12px;
 
       .form {
