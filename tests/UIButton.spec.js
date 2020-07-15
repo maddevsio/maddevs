f4024e08c4f43584b7fbb4b11e97c39f1d952566
AntonGrushkin <merynes345@gmail.com>
2020-07-15 20:05:33 +0600

    MDW-875 added tests for UIButton

---
+++ tests/UIButton.spec.js
@@ -0,0 +1,51 @@
+import {
+  mount
+} from '@vue/test-utils';
+import UIButton from '@/components/ui/UIButton';
+
+describe('Ui button modal trigger', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(UIButton, {
+      propsData: {
+        name: 'Button name',
+        disabled: false,
+      }
+    });
+  });
+
+  // ------ IMPORTANT ----- //
+  test('is a Vue instance', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+  
+  test('renders correctly', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+  // --------------------- //
+
+  test('Button slot renders a default button text', () => {
+    const btn = wrapper.find('.ui-button');
+    expect(btn.text()).toBe('Button name');
+  });
+
+  test('onClick should call $emit', () => {
+    wrapper.vm.onClick();
+    expect(wrapper.emitted().click).toBeTruthy();
+  });
+
+  test('if this.$props.disabled equal false, onClick should not call $emit and button should contain new class', () => {
+    wrapper = mount(UIButton, {
+      propsData: {
+        name: 'Button name',
+        disabled: true,
+      }
+    });
+    wrapper.vm.onClick();
+    expect(wrapper.emitted().click).toBeFalsy();
+
+    const button = wrapper.find('.ui-button');
+    expect(button.classes()).toContain('ui-button--disabled');
+  });
+});
