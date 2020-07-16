152680a411df05ac0515600af889d751478d2859
AntonGrushkin <merynes345@gmail.com>
2020-07-16 14:42:51 +0600

    MDW-934 fixed bug with body styles when modal is open

--- client/components/ui/UIButtonModalTrigger.vue
+++ client/components/ui/UIButtonModalTrigger.vue
@@ -56,7 +56,7 @@ export default {
       this.disableScrollOnBody();
     },
     disableScrollOnBody() {
-      document.body.style.position = 'fixed';
+      document.body.style.overflow = 'hidden';
       document.body.style.top = `-${this.scrollYPosition}`;
     },
     handleScroll() {
