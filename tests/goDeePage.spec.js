d095ce3dcfded8875b912a608990a756cf9cc34d
AntonGrushkin <merynes345@gmail.com>
2020-07-27 16:22:04 +0600

    MDW-1010 added new tests and fixed linter errors

---
+++ tests/goDeePage.spec.js
@@ -0,0 +1,28 @@
+import {
+  mount
+} from '@vue/test-utils';
+import GoDeePage from '@/pages/_lang/godee';
+
+describe('GoDee page', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(GoDeePage);
+  });
+
+  // ------ IMPORTANT ----- //
+  test('is a Vue instance', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+  
+  test('renders correctly', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+  // --------------------- //
+
+  test('Sets the correctly data', () => {
+    expect(wrapper.vm.$data.title).toEqual('Mad Devs: Software & Mobile App Development Company');
+    expect(wrapper.vm.$data.description).toEqual('');
+    expect(wrapper.vm.$data.ogUrl).toEqual('https://maddevs.io/godee');
+  });
+});
