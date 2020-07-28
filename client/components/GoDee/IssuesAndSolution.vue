c7b5b0efe0ce6a86be6c4652ca22684e248755dd
AntonGrushkin <merynes345@gmail.com>
2020-07-28 19:12:06 +0600

    MDW-1032 added markup for transportation section

--- client/components/GoDee/IssuesAndSolution.vue
+++ client/components/GoDee/IssuesAndSolution.vue
@@ -1,9 +1,35 @@
 <template>
   <section id="issues-and-solution" class="issues-and-solution">
-    <div class="container">
+    <div class="issues-and-solutio__head-content">
       <h2 class="issues-and-solution__main-title">Issues and Solution</h2>
+      <img src="@/assets/img/GoDee/png/issues-and-solution-bg.png" class="issues-and-solution__background-image" alt="Background">
     </div>
-    <img src="@/assets/img/GoDee/png/issues-and-solution-bg.png" class="issues-and-solution__background-image" alt="Background">
+    <div class="container">
+      <div class="issues-and-solution__transportation">
+        <div class="issues-and-solution__content-wrapper">
+          <div class="issues-and-solutio__left-text-content">
+            <div class="issues-and-solution__description-item">
+              <div class="issues-and-solution__description-title"></div>
+              <p class="issues-and-solution__description-paragraph"></p>
+            </div>
+            <div class="issues-and-solution__description-item">
+              <div class="issues-and-solution__description-title"></div>
+              <p class="issues-and-solution__description-paragraph"></p>
+              <div class="issues-and-solution__description-icons-group">
+                <div class="issues-and-solution__icon-item">
+                  <img src="" alt="">
+                  <p class="issues-and-solution__icon-designation"></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="issues-and-solutio__image-wrapper">
+            <img src="#" alt="">
+          </div>
+        </div>
+        <div class="issues-and-solution__empty-white-block"></div>
+      </div>
+    </div>
   </section>
 </template>
 
