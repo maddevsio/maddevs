cffa11ccd73d99fe63287a5a423823ee12d098b6
HEKTOdaleky <hektodaleky@gmail.com>
2020-08-10 23:29:17 +0600

    Revert "MDW-873 - cache control"

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ const routes = require('./server/routes');
 app.use('/', routes);
 app.use(function applyXFrame(req, res, next) {
   res.set('X-Frame-Options', 'DENY');
-  res.set('Cache-Control', 'public, max-age=3600000');
-  next();
+  next(); 
 });
 app.use((req, res, next) => {
   if (req.secure) {
