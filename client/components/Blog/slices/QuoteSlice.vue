b12c84f040bf5ae350355867f6e3292366420f07
AntonGrushkin <merynes345@gmail.com>
2020-09-16 18:33:22 +0600

    added client logic for blog

---
+++ client/components/Blog/slices/QuoteSlice.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class='post-part single'>
+    <blockquote class="block-quotation">
+      {{ $prismic.asText(slice.primary.quote) }}
+    </blockquote>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['slice'],
+  name: 'quote-slice'
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../../../assets/styles/_vars';
+
+  .block-quotation {
+    display: inline-block;
+    font-style: italic;
+    font-size: 24px;
+    margin: 0;
+    font-family: 'Hoves-Regular';
+    color: $text-color--white;
+
+    &:before {
+      content: "« ";
+    }
+
+    &:after {
+      content: " »";
+    }
+  }
+
+  .block-citation {
+    display: inline-block;
+    font-style: italic;
+    border-left: solid #B4B4B4 4px;
+  }
+</style>
---
+++ client/components/Blog/slices/TextSlice.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class='post-part single'>
+    <prismic-rich-text class="textslice" :class="className" :field="slice.primary.text"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      default: null
+    }
+  },
+  name: 'text-slice',
+  data() {
+    return {
+      className: ''
+    };
+  },
+  mounted() {
+    if (this.$props.slice.primary.text[0].type === 'heading1') {
+      this.className = this.$props.slice.primary.text[0].text.toLowerCase().replace(/\s/g , '-');
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../../../assets/styles/_vars';
+
+  .textslice {
+
+    /deep/ strong,
+    /deep/ p,
+    /deep/ h1,
+    /deep/ li {
+      color: $text-color--white;
+    }
+
+    /deep/ h1,
+    /deep/ li {
+      font-family: 'Hoves-Regular';
+    }
+
+    /deep/ h1 {
+      margin-top: 60px;
+      font-size: 1.9em;
+      font-weight: normal;
+    }
+
+    /deep/ h2,
+    /deep/ h3, 
+    /deep/ h4,
+    /deep/ h5,
+    /deep/ h6 {
+      margin-top: 40px;
+      margin-bottom: 30px;
+    }
+
+    /deep/ strong,
+    /deep/ p {
+      font-family: 'Hoves-Bold';
+    }
+
+    /deep/ p,
+    /deep/ li {
+      font-size: 18px;
+      line-height: 129%;
+      letter-spacing: -0.02em;
+    }
+
+
+    /deep/ p {
+      margin: 25px 0;
+      font-family: 'Hoves-Regular', sans-serif;
+    }
+
+    /deep/ em {
+        display: block;
+        padding: 5px 12px;
+        border-left: 3px solid $border-color--white;
+    }
+
+    /deep/ pre {
+      padding: 20px;
+      overflow: auto;
+      background: $bgcolor--grey-light;
+      border-radius: 2px;
+      font-family: 'Hoves-Regular', sans-serif;
+      line-height: 24px;
+    }
+
+    /deep/ ol {
+      margin: 35px 0;
+    }
+
+    /deep/ li {
+      margin-bottom: 20px;
+    }
+
+    /deep/ a {
+      text-decoration: underline;
+      color: $text-color--white;
+      font-family: 'Hoves-Regular', sans-serif;
+    }
+  }
+</style>
