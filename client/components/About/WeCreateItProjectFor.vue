34fab9c93a77da6c4d1044cae2c8ed7db617567d
AntonGrushkin <merynes345@gmail.com>
2020-10-12 11:56:50 +0600

    MDW-1383 added new styles for We create IT project section

--- client/components/About/WeCreateItProjectFor.vue
+++ client/components/About/WeCreateItProjectFor.vue
@@ -2,33 +2,31 @@
   <section id="we-create-it-project" class="we-create-it-project">
     <div class="container">
       <div class="we-create-it-project__content-wrapper">
-        <h2 class="we-create-it-project__title title-md">
+        <h2 class="we-create-it-project__main-title">
           We create <br> IT projects for:
         </h2>
         <div class="we-create-it-project__projects-type_list row gy-0">
           <div class="we-create-it-project__project-item col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
-            <div class="we-create-it-project__paragraph paragraph-lg">Content Streaming <br> Transportation</div>
+            <p class="we-create-it-project__paragraph">Content Streaming <br> Transportation</p>
           </div>
           <div class="we-create-it-project__project-item col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
-            <div class="we-create-it-project__paragraph paragraph-lg">HealthTech <br> IoTs</div>
+            <p class="we-create-it-project__paragraph">HealthTech <br> IoTs</p>
           </div>
           <div class="we-create-it-project__project-item col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
-            <div class="we-create-it-project__paragraph paragraph-lg">E-Commerce <br> AdTech</div>
+            <p class="we-create-it-project__paragraph">E-Commerce <br> AdTech</p>
           </div>
           <div class="we-create-it-project__project-item col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
-            <div class="we-create-it-project__paragraph paragraph-lg">E-Learning <br> FinTech</div>
+            <p class="we-create-it-project__paragraph">E-Learning <br> FinTech</p>
           </div>
           <div class="we-create-it-project__project-item col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
-            <div class="we-create-it-project__paragraph paragraph-lg">Serverless <br> BYOD</div>
+            <p class="we-create-it-project__paragraph">Serverless <br> BYOD</p>
           </div>
           <div class="we-create-it-project__project-item col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
-            <div class="we-create-it-project__paragraph paragraph-lg">Blockchain <br> FoodTech</div>
+            <p class="we-create-it-project__paragraph">Blockchain <br> FoodTech</p>
           </div>
         </div>
-        <ul class="we-create-it-project__country-list row">
-          <li v-for="(country, i) in countries" :key="i" class="we-create-it-project__country-item col-xl-1 col-lg-1 col-md-1 col-sm-1 col-2">
-            <span :class="`we-create-it-project__logo-country we-create-it-project__logo-country--${country.iconName}`"/>
-          </li>
+        <ul class="we-create-it-project__flags-list">
+          <li v-for="(flag, i) in flags" :key="i" class="we-create-it-project__flag-item" :class="flag"></li>
         </ul>
       </div>
     </div>
@@ -41,55 +39,29 @@ export default {
   name: 'WeCreateItProjectFor',
   data() {
     return {
-      countries: [
-        {
-          iconName: 'united-states',
-          countryName: 'USA'
-        },
-        {
-          iconName: 'australia',
-          countryName: 'Australia'
-        },
-        {
-          iconName: 'united-kingdom',
-          countryName: 'UK'
-        },
-        {
-          iconName: 'poland',
-          countryName: 'Poland'
-        },
-        {
-          iconName: 'austria',
-          countryName: 'Austria'
-        },
-        {
-          iconName: 'france',
-          countryName: 'France'
-        },
-        {
-          iconName: 'switzerland',
-          countryName: 'Switzerland'
-        },
-        {
-          iconName: 'belarus',
-          countryName: 'Belarus'
-        },
-        {
-          iconName: 'vietnam',
-          countryName: 'Vietnam'
-        },
-        {
-          iconName: 'kazakhstan',
-          countryName: 'Kazakhstan'
-        },
-        {
-          iconName: 'russia',
-          countryName: 'Russia'
-        },
-        {
-          iconName: 'singapore',
-          countryName: 'Singapore'
-        }
+      flags: [
+        'flag-usa',
+        'flag-australia',
+        'flag-vietnam',
+        'flag-france',
+        'flag-uk',
+        'flag-indonesia',
+        'flag-germany',
+        'flag-austria',
+        'flag-switzerland',
+        'flag-canada',
+        'flag-latvia',
+        'flag-kazakhstan',
+        'flag-sweden',
+        'flag-finland',
+        'flag-united-arab-emirates',
+        'flag-denmark',
+        'flag-japan',
+        'flag-norway',
+        'flag-portugal',
+        'flag-katar',
+        'flag-south-africa',
+        'flag-netherlands'
       ]
     };
   }
@@ -98,123 +70,128 @@ export default {
 
 <style lang="scss" scoped>
   @import '../../assets/styles/vars';
+  @import '../../assets/styles/_flagsIcons';
 
   .we-create-it-project {
     margin: 131px 0;
 
     &__content-wrapper {
-      padding: 51px 80px 73px;
+      padding: 40px 48px 48px;
       text-align: center;
       background-color: $bgcolor--white;
     }
 
-    &__title {
-      margin-bottom: 24px;
-
-      br {
-        display: none;
-      }
-    }
-
     &__projects-type_list {
-      margin: 0 0 45px;
+      margin: 0 0 63px;
     }
 
     &__project-item {
       padding: 0;
     }
 
-    &__logo-country {
-      display: block;
-      width: 28px;
-      height: 20px;
-      background-repeat: no-repeat;
-      background-size: contain;
+    &__flags-list {
+      display: grid;
+      grid-template-columns: repeat(22, 1fr);
+    }
 
-      &--united-kingdom {
-        background-image: url(../../assets/img/Flags/united-kingdom.svg);
+    &__flag-item {
+      &::before {
+        content: '';
+        display: block;
+        width: 32px;
+        height: 23px;
       }
+    }
 
-      &--united-states {
-        background-image: url(../../assets/img/Flags/united-states.svg);
-      }
+    &__main-title,
+    &__paragraph {
+      color: $text-color--black;
+    }
 
-      &--austria {
-        background-image: url(../../assets/img/Flags/austria.svg);
-      }
+    &__main-title {
+      margin-bottom: 25px;
+      font-size: 62px;
+      font-family: 'Poppins-Bold', sans-serif;
+      letter-spacing: -1px;
 
-      &--poland {
-        background-image: url(../../assets/img/Flags/poland.svg);
+      br {
+        display: none;
       }
+    }
 
-      &--russia {
-        background-image: url(../../assets/img/Flags/russia.svg);
-      }
-
-      &--vietnam {
-        background-image: url(../../assets/img/Flags/vietnam.svg);
-      }
-
-      &--switzerland {
-        background-image: url(../../assets/img/Flags/switzerland.svg);
-      }
-
-      &--australia {
-        background-image: url(../../assets/img/Flags/australia.svg);
-      }
-
-      &--belarus {
-        background-image: url(../../assets/img/Flags/belarus.svg);
-      }
-
-      &--france {
-        background-image: url(../../assets/img/Flags/france.svg);
-      }
-
-      &--singapore {
-        background-image: url(../../assets/img/Flags/singapore.svg);
-      }
-
-      &--kazakhstan {
-        background-image: url(../../assets/img/Flags/kazakhstan.svg);
-      }
+    &__paragraph {
+      font-family: 'Poppins-Regular', sans-serif;
+      font-size: 18px;
+      letter-spacing: -0.02em;
+      line-height: 34px;
     }
   }
 
-  @media only screen and (max-width: 1366px) {
+  @media only screen and (max-width: 1270px) {
     .we-create-it-project {
-      &__content-wrapper {
-        padding-left: 70px;
-        padding-right: 70px;
+      &__paragraph {
+        font-size: 16px;
       }
     }
   }
 
   @media only screen and (max-width: 1024px) {
     .we-create-it-project {
-      margin-top: 75px;
-      margin-bottom: 64px;
-
-      &__logo-country {
-        width: 21px;
-        height: 15px;
-      }
-    }
-  }
-
-  @media only screen and (max-width: 768px) {
-    .we-create-it-project {
-      &__projects-type_list {
-        margin-bottom: 40px;
-      }
-
       &__content-wrapper {
-        padding: 33px;
+        padding: 25px 33px 33px;
+      }
+
+      &__main-title {
+        font-size: 32px;
+      }
+
+      &__projects-type_list {
+        margin: 0 0 45px;
+      }
+
+      &__flag-item {
+        &::before {
+          width: 21px;
+          height: 15px;
+        }
+      }
+
+      &__paragraph {
+        font-size: 14px;
+        line-height: 25px;
       }
     }
   }
 
-  @media only screen and (max-width: 575px) {
+  @media only screen and (max-width: 720px) {
+    .we-create-it-project {
+      &__flags-list {
+        grid-template-columns: repeat(11, 1fr);
+        grid-row-gap: 19px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 576px) {
+    .we-create-it-project {
+      &__main-title {
+        br {
+          display: block;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 575px) and (max-width: 576px) {
+    .we-create-it-project {
+      &__content-wrapper {
+        padding-left: 13px;
+        padding-right: 13px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 480px) {
     .we-create-it-project {
       margin-top: 34px;
       margin-bottom: 45px;
@@ -226,10 +203,40 @@ export default {
         text-align: left;
       }
 
-      &__title {
-        br {
-          display: block;
+      &__main-title {
+        font-size: 36px;
+        line-height: 120%;
+      }
+
+      &__paragraph {
+        font-size: 16px;
+        line-height: 33px;
+        letter-spacing: -0.02em;
+      }
+
+      &__flag-item {
+        &::before {
+          width: 23px;
+          height: 19px;
         }
+      }
+
+      &__projects-type_list {
+        margin: 0 0 30px;
+      }
+
+      &__project-item {
+        &:nth-child(2n) {
+          padding-left: 26px;
+        }
+      }
+    }
+  }
+
+  @media only screen and (max-width: 345px) {
+    .we-create-it-project {
+      &__paragraph {
+        font-size: 14px;
       }
     }
   }
