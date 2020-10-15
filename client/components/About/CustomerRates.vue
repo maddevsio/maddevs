a0b6148adfea6a918087d8df4cd536cd5fc9983d
AntonGrushkin <merynes345@gmail.com>
2020-10-15 20:16:38 +0600

    MDW-1430 added new styles and markup for section

--- client/components/About/CustomerRates.vue
+++ client/components/About/CustomerRates.vue
@@ -1,66 +1,68 @@
 <template>
   <section id="customer-rates" class="customer-rates">
     <div class="container">
-      <h2 class="customer-rates__title title">
-        <span>Fantastic</span> <br> customer rates:
-        <img src="@/assets/img/Home/svg/CustomerRates/satisfaction-icon.svg" alt="Satisfaction" class="customer-rates__title-icon">
-      </h2>
+      <div class="customer-rates__main-title-wrapper">
+        <h2 class="customer-rates__main-title">
+          <span>Fantastic</span> <br> customer rates:
+        </h2>
+        <div class="customer-rates__title-icon" />
+      </div>
       <div class="customer-rates__top-row row">
-        <div class="col-xl-4 col-sm-4">
+        <div class="col-xl-4 col-md-4">
           <div class="customer-rates__rates-block customer-rates__rates-block--top-row">
             <p class="customer-rates__number customer-rates__number--top-row">92%</p>
-            <p class="customer-rates__description customer-rates__description--top-row">recommend us to <br class="customer-rates__line-break"> others.</p>
+            <p class="customer-rates__description customer-rates__description--top-row">recommend us <br class="customer-rates__line-break"> to others.</p>
           </div>
         </div>
-        <div class="col-xl-4 col-sm-4">
+        <div class="col-xl-4 col-md-4">
           <div class="customer-rates__rates-block customer-rates__rates-block--top-row">
             <p class="customer-rates__number customer-rates__number--top-row">87%</p>
             <p class="customer-rates__description customer-rates__description--top-row">stay with us for <br class="customer-rates__line-break"> more than 2 years</p>
           </div>
         </div>
-        <div class="col-xl-4 col-sm-4">
+        <div class="col-xl-4 col-md-4">
           <div class="customer-rates__rates-block customer-rates__rates-block--top-row">
             <p class="customer-rates__number customer-rates__number--top-row">98%</p>
             <p class="customer-rates__description customer-rates__description--top-row">are satisfied with <br class="customer-rates__line-break"> our pricing.</p>
           </div>
         </div>
       </div>
-      <h3 class="customer-rates__section-sub-title title-md">Mad Devs' key metrics:</h3>
+      <h3 class="customer-rates__section-sub-title">Mad Devs' key metrics:</h3>
       <div class="customer-rates__bottom-row row">
-        <div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
+        <div class="col-xl-2 col-lg-4 col-md-4 col-6">
           <div class="customer-rates__rates-block customer-rates__rates-block--bottom-row">
             <p class="customer-rates__number customer-rates__number--bottom-row">10M+</p>
             <p class="customer-rates__description customer-rates__description--bottom-row">US dollars raised by 5 customer startups in 2019.</p>
           </div>
         </div>
-        <div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
+        <div class="col-xl-2 col-lg-4 col-md-4 col-6">
           <div class="customer-rates__rates-block customer-rates__rates-block--bottom-row">
             <p class="customer-rates__number customer-rates__number--bottom-row">75%</p>
             <p class="customer-rates__description customer-rates__description--bottom-row">of customers are now paying 50% less for IT infrastructure.</p>
           </div>
         </div>
-        <div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
+        <div class="col-xl-2 col-lg-4 col-md-4 col-6">
           <div class="customer-rates__rates-block customer-rates__rates-block--bottom-row">
             <p class="customer-rates__number customer-rates__number--bottom-row">30+</p>
             <p class="customer-rates__description customer-rates__description--bottom-row">tech bloggers and conference speakers in our team.</p>
           </div>
         </div>
-        <div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
+        <div class="col-xl-2 col-lg-4 col-md-4 col-6">
           <div class="customer-rates__rates-block customer-rates__rates-block--bottom-row">
             <p class="customer-rates__number customer-rates__number--bottom-row">50+</p>
             <p class="customer-rates__description customer-rates__description--bottom-row">projects delivered to customers by our team.</p>
           </div>
         </div>
-        <div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
+        <div class="col-xl-2 col-lg-4 col-md-4 col-6">
           <div class="customer-rates__rates-block customer-rates__rates-block--bottom-row">
             <p class="customer-rates__number customer-rates__number--bottom-row">100+</p>
-            <p class="customer-rates__description customer-rates__description--bottom-row">staff members work for Mad Devs.</p>
+            <p class="customer-rates__description customer-rates__description--bottom-row">staff members work <br class="customer-rates__line-break"> for Mad Devs.</p>
           </div>
         </div>
-        <div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-6">
+        <div class="col-xl-2 col-lg-4 col-md-4 col-6">
           <div class="customer-rates__rates-block customer-rates__rates-block--bottom-row">
             <p class="customer-rates__number customer-rates__number--bottom-row">70+</p>
-            <p class="customer-rates__description customer-rates__description--bottom-row">open-source projects on GitHub.</p>
+            <p class="customer-rates__description customer-rates__description--bottom-row">open-source projects <br class="customer-rates__line-break"> on GitHub.</p>
           </div>
         </div>
       </div>
@@ -77,26 +79,49 @@ export default {
 
 <style lang="scss" scoped>
   @import '../../assets/styles/vars';
+  @import '../../assets/styles/_customerRatesIcon';
 
   .customer-rates {
-    padding-top: 86px;
-    padding-bottom: 92px;
+    padding: 100px 0;
     background-color: $bgcolor--white-darken;
 
-    &__title {
-      position: relative;
-      margin-bottom: 20px;
+    &__main-title-wrapper {
+      display: flex;
+      justify-content: center;
+    }
+
+    &__main-title,
+    &__section-sub-title,
+    &__number {
+      font-family: 'Poppins-Bold';
+    }
+
+    &__main-title,
+    &__section-sub-title {
+      letter-spacing: -1px;
+    }
+
+    &__main-title,
+    &__section-sub-title,
+    &__rates-block {
       text-align: center;
+    }
+
+    &__main-title,
+    &__section-sub-title,
+    &__description {
+      color: $text-color--black-lighter;
+    }
+
+    &__main-title {
+      position: relative;
+      margin-bottom: 36px;
+      font-size: 62px;
+      line-height: 74px;
 
       br {
         display: none;
       }
-    }
-
-    &__title-icon {
-      width: 200px;
-      margin-left: -268px;
-      margin-bottom: -19px;
     }
 
     &__top-row {
@@ -104,12 +129,9 @@ export default {
     }
 
     &__section-sub-title {
-      margin-bottom: 15px;
-    }
-
-    &__section-sub-title,
-    &__rates-block {
-      text-align: center;
+      margin-bottom: 22px;
+      font-size: 40px;
+      line-height: 48px;
     }
 
     &__rates-block {
@@ -122,138 +144,228 @@ export default {
 
       &--bottom-row {
         height: 128px;
-        padding: 24px 20px;
+        padding: 27px 20px;
       }
     }
 
     &__number {
-      font-family: 'Poppins-Bold', sans-serif;
-
       &--top-row {
+        margin-bottom: 11px;
         font-size: 102px;
+        line-height: 107px;
         color: $text-color--black-lighter;
       }
 
       &--bottom-row {
+        margin-top: 3px;
+        margin-bottom: 10px;
         font-size: 44px;
-        color: $text-color--red;
+        line-height: 46px;
         letter-spacing: -1.4px;
+        color: $text-color--red;
       }
     }
 
     &__description {
       font-family: 'Poppins-Regular', sans-serif;
-      color: $text-color--black-lighter;
       letter-spacing: -0.02em;
 
       &--top-row {
         font-size: 16px;
+        line-height: 24px;
       }
 
       &--bottom-row {
         font-size: 14px;
+        line-height: 22px;
       }
     }
 
     &__line-break {
       display: none;
     }
-  }
 
-  @media only screen and (max-width: 1180px) {
-    .customer-rates {
-      &__line-break {
-        display: block;
-      }
+    &__title-icon {
+      width: 220px;
+      height: 105px;
+      display: block;
+      margin-top: -23px;
+      margin-left: -262px;
+      z-index: 1;
+      @include satisfaction-icon;
     }
   }
 
-  @media only screen and (max-width: 1024px) {
+  @media only screen and (min-width: 1199px) and (max-width: 1370px) {
     .customer-rates {
-      padding-top: 51px;
-      padding-bottom: 76px;
-
       &__rates-block {
-        &--top-row {
-          padding: 25px 0;
-        }
-
         &--bottom-row {
-          padding: 20px 20px 10px;
-        }
-      }
-
-      &__title {
-        margin-bottom: 13px;
-        text-align: center;
-      }
-
-      &__title-icon {
-        width: 95px;
-        margin-left: -156px;
-        margin-bottom: -9px;
-      }
-
-      &__number {
-        &--top-row {
-          font-size: 50px;
-        }
-
-        &--bottom-row {
-          font-size: 38px;
-          letter-spacing: -1.45px;
+          padding-left: 10px;
+          padding-right: 10px;
         }
       }
 
       &__description {
-        margin-top: 10px;
-        font-size: 12px;
+        &--bottom-row {
+          font-size: 13px;
+        }
       }
     }
   }
 
-  @media only screen and (max-width: 720px) {
+  @media only screen and (max-width: 1199px) {
     .customer-rates {
-      &__title {
-        margin-left: -50px;
-        line-height: 40px;
-
-        span {
-          margin-right: -61px;
-        }
+      &__main-title {
+        margin-bottom: 55px;
 
         br {
           display: block;
         }
       }
 
-      &__rates-block {
-        &--bottom-row {
-          padding-bottom: 30px;
+      &__title-icon {
+        width: 184px;
+        margin-top: 50px;
+        margin-left: -262px;
+      }
+
+      &__number {
+        &--top-row {
+          margin-bottom: 0;
+          font-size: 80px;
         }
       }
-    }
-  }
 
-  @media only screen and (max-width: 570px) {
-    .customer-rates {
+      &__rates-block {
+        &--bottom-row {
+          height: 110px;
+        }
+      }
+
+      &__line-break {
+        display: block;
+      }
+
       &__top-row {
-        margin-bottom: 91px;
+        margin-bottom: 68px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 860px) {
+    .customer-rates {
+      padding: 67px 0;
+
+      &__main-title {
+        font-size: 36px;
+        line-height: 43px;
+        margin-bottom: 40px;
+      }
+
+      &__section-sub-title {
+        font-size: 30px;
+        line-height: 36px;
+        margin-bottom: 19px;
       }
 
       &__rates-block {
+        height: 103px;
+
+        &--top-row,
         &--bottom-row {
-          padding-bottom: 10px;
+          padding: 20px 10px;
+        }
+      }
+
+      &__number {
+        &--top-row {
+          font-size: 56px;
+          line-height: 75px;
+        }
+      }
+
+      &__title-icon {
+        width: 140px;
+        margin-top: 10px;
+        margin-left: -170px;
+      }
+
+      &__description {
+        &--top-row,
+        &--bottom-row {
+          font-size: 12px;
+          line-height: 18px;
         }
       }
     }
   }
 
-  @media only screen and (max-width: 360px) {
+  @media only screen and (max-width: 767px) {
     .customer-rates {
+
+      &__main-title {
+        margin-bottom: 32px;
+      }
+ 
+      &__number {
+        &--top-row {
+          font-size: 50px;
+          line-height: 65px;
+        }
+
+        &--bottom-row {
+          font-size: 38px;
+          line-height: 39px;
+          margin-top: 10px;
+        }
+      }
+
+      &__description {
+        &--top-row {
+          font-size: 14px;
+          line-height: 18px;
+        }
+      }
+
+      &__top-row {
+        margin-bottom: 74px;
+      }
+
+      &__title-icon {
+        width: 90px;
+        margin-left: -123px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 440px) {
+    .customer-rates {
+      padding: 45px 0;
+
+      &__section-sub-title {
+        margin-bottom: 11px;
+        line-height: 43px;
+      }
+
       &__rates-block {
         &--bottom-row {
-          padding-bottom: 30px;
+          padding-left: 5px;
+          padding-right: 5px;
+        }
+      }
+
+      &__title-icon {
+        margin: 15px 55px 0 -145px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 340px) {
+    .customer-rates {
+      &__description {
+        &--bottom-row {
+          font-size: 11px;
+          line-height: 17px;
         }
       }
     }
