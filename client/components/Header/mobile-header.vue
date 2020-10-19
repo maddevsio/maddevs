ddc34d9e8884b371a8f3287dbcbf620af853e27d
AntonGrushkin <merynes345@gmail.com>
2020-10-19 22:33:37 +0600

    MDW-1434 update styles

--- client/components/Header/mobile-header.vue
+++ client/components/Header/mobile-header.vue
@@ -8,7 +8,7 @@
 				<button class="mobile-header__toogle-btn" @click="toggleMobileHeader()" :class="mobileHeaderIsOpen ? 'mobile-header__close' : 'mobile-header__hamburger'"></button>
 			</div>
 		</div>
-		<div class="mobile-header__scrollbar">
+		<div class="mobile-header__scrollbar container">
 			<div class="mobile-header__content-wrap" v-show="mobileHeaderIsOpen">
 				<div class="mobile-header__nav-wrap">
 					<nav class="mobile-header__header-routes_links">
