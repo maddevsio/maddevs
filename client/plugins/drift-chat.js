6b0a3439f6674f3789847331d7ab12cf9094ccf6
AntonGrushkin <merynes345@gmail.com>
2020-10-22 21:09:26 +0600

    new-develop fix spaces

--- client/plugins/drift-chat.js
+++ client/plugins/drift-chat.js
@@ -3,13 +3,13 @@ export default () => {
     var t = window.driftt = window.drift = window.driftt || [];
     if (!t.init) {
       if (t.invoked) return void (window.console && console.error && console.error('Drift snippet included twice.'));
-      t.invoked = !0, t.methods = [ 'identify', 'config', 'track', 'reset', 'debug', 'show', 'ping', 'page', 'hide', 'off', 'on' ], 
+      t.invoked = !0, t.methods = ['identify', 'config', 'track', 'reset', 'debug', 'show', 'ping', 'page', 'hide', 'off', 'on'], 
       t.factory = function(e) {
         return function() {
           var n = Array.prototype.slice.call(arguments);
           return n.unshift(e), t.push(n), t;
         };
-      }, t.methods.forEach(function(e) {
+      }, t.methods.forEach(function (e) {
         t[e] = t.factory(e);
       }), t.load = function(t) {
         var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement('script');
