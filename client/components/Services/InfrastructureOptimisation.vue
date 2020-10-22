64484667c58300208b4f5a216a27f35730939ee5
AntonGrushkin <merynes345@gmail.com>
2020-10-22 20:30:00 +0600

    MDW-1455 added new styles for section

--- client/components/Services/InfrastructureOptimisation.vue
+++ client/components/Services/InfrastructureOptimisation.vue
@@ -1,29 +1,28 @@
 <template>
-  <section id="infrastructure-optimisation" class="infrastructure-optimisation main-parent-item">
+  <section id="infrastructure-optimisation" class="infrastructure-optimisation">
     <div class="container">
-      <div class="infrastructure-optimisation__header">
-        <h2 class="infrastructure-optimisation__main-title main-title">
-          <span>Infrastructure</span><br>Optimisation
-        </h2>
-        <div class="infrastructure-optimisation__bg"></div>
-      </div>
-        <div class="infrastructure-optimisation__flex-wrapper flex-wrapper">
+      <h2 class="infrastructure-optimisation__main-title">Infrastructure <br class="infrastructure-optimisation__small-screen-break"> Optimisation</h2>
+      <div class="infrastructure-optimisation__content">
+        <div class="infrastructure-optimisation__text-content">
           <InfrastructureContent
             :title="whatMakes.title" 
             :subTitle="whatMakes.subTitle" 
             :paragraph="whatMakes.paragraph"
+            :className="whatMakes.className"
           />
           <InfrastructureContent 
             :title="upToSaving.title" 
             :subTitle="upToSaving.subTitle" 
             :paragraph="upToSaving.paragraph"
+            :className="upToSaving.className"
           />
         </div>
         <UIButtonModalTrigger 
           :buttonInnerText="buttonInnerText"
           :modalWindowName="modalWindowName"
-          :isBlack="true"
+          class="infrastructure-optimisation__button"
         />
+      </div>
     </div>
   </section>
 </template>
@@ -43,12 +42,14 @@ export default {
       whatMakes: {
         title: 'What makes us stand out from others?',
         subTitle: 'Optimising teams don’t care for tomorrow’s traffic peaks? We care!',
-        paragraph: 'We don\'t just have experience with infrastructure management tools, our team helped develop some of them. We know where to get the cheapest price and the best service.'
+        paragraph: 'We don\'t just have experience with infrastructure management tools, our team helped develop some of them. We know where to get the cheapest price and the best service.',
+        className: 'first-item'
       },
       upToSaving: {
         title: 'Up to 50% savings on infrastructure',
         subTitle: 'Pay-as-you-go receipts from your cloud providers make you curse them?',
-        paragraph: 'Our fire brigade can help you optimise resource consumption like we\'ve done for 75% of our customers. We’ll reconfigure and disable every option or service that is not necessary.'
+        paragraph: 'Our fire brigade can help you optimise resource consumption like we\'ve done for 75% of our customers. We’ll reconfigure and disable every option or service that is not necessary.',
+        className: 'second-item'
       },
       buttonInnerText: 'Get an infrastructure audit',
       modalWindowName: 'infrastructure-modal'
@@ -61,135 +62,120 @@ export default {
   @import '../../assets/styles/vars';
 
   .infrastructure-optimisation {
-    padding-top: 117px;
-    padding-bottom: 154px;
-    background-color: $bgcolor--grey-light;
-
-    button {
-      width: 100%;
+    &__main-title {
+      width: max-content;
+      margin: auto;
+      padding-left: 32px;
+      padding-right: 32px;
+      position: relative;
+      text-align: center;
+      z-index: 1;
+      background-color: $bgcolor--black;
+      color: $text-color--red;
+      @include h2_title;
     }
 
-    .infrastructure-optimisation__content-item {
-      &:first-child {
-        /deep/.infrastructure-optimisation__title {
-          max-width: 100%;
-        }
-      }
-
-      &:nth-child(2) {
-        /deep/.infrastructure-optimisation__title {
-          max-width: 85%;
-        }
-      }
+    &__content {
+      margin-top: -69px;
+      margin-bottom: 136px;
+      padding: 83px 106px 52px 106px;
+      border: 2px solid $border-color--red;
     }
 
-    &__header {
+    &__text-content {
       display: flex;
       justify-content: space-between;
-      position: relative;
+      margin-bottom: 39px;
     }
 
-    &__bg {
-      background: url('../../assets/img/Services/svg/infrastructure-bg.svg') bottom right/contain no-repeat;
+    &__button {
       width: 100%;
-      height: 100%;
-      position: absolute;
-      top: 107px;
-      right: 0;
-    }
+      height: 56px;
+      font-size: 16px;
+      color: $text-color--white;
+      border-color: $button-border--white-opacity;
 
-    &__paragraph {
-      line-height: 119%;
-    }
+      &:hover {
+        background-color: $bgcolor--white;
+        color: $text-color--black !important;
+      }
 
-    &__flex-wrapper {
-      margin-top: 60px;
-    }
+      &:active {
+        background-color: $bgcolor--white !important;
+        border-color: $button-border--white-opacity !important;
+      }
 
-    &__main-title {
-      padding-bottom: 20px;
-      color: $text-color--black;
-      z-index: 99;
-
-      span {
-        color: $text-color--grey-light;
-        -webkit-text-stroke: $text-stroke--black;
+      @media screen and (max-width: 1280px) {
+        font-size: 14px;
       }
     }
-  }
 
-  @media only screen and (max-width: 1280px) {
-    .infrastructure-optimisation {
-      padding-top: 72px;
+    &__small-screen-break {
+      display: none;
+    }
 
+    @media screen and  (max-width: 1240px) {
+      &__content {
+        margin-top: -95px;
+        padding: 110px 70px 52px 70px;
+      }
+
+      &__text-content {
+        margin-bottom: 29px;
+      }
+
+      &__small-screen-break {
+        display: block;
+      }
+    }
+
+    @media screen and  (max-width: 1023px) {
+      &__text-content {
+        flex-direction: column;
+      }
+    }
+
+    @media screen and  (max-width: 834px) {
       &__main-title {
-        padding-bottom: 0;
+        padding-left: 22px;
+        padding-right: 22px;
+      }
+
+      &__content {
+        margin-top: -66px;
+        padding-top: 74px;
+        margin-bottom: 80px;
       }
     }
-  }
 
-  @media only screen and (max-width: 870px) {
-    /deep/.infrastructure-optimisation {
-      &__title {
-        max-width: 100%;
-      }
-
-      &__bg {
-        display: none;
-      }
-
-      .infrastructure-optimisation__content-item {
-        &:nth-child(2) {
-          /deep/.infrastructure-optimisation__title {
-            max-width: 100%;
-          }
-        }
+    @media screen and  (max-width: 768px) {
+      &__content {
+        padding-left: 56px;
+        padding-right: 56px;
       }
     }
-  }
 
-  @media only screen and (max-width: 768px) {
-    .infrastructure-optimisation {
-      padding-top: 65px;
-      padding-bottom: 65px;
-
-      &__flex-wrapper {
-        margin-top: 38px;
-      }
-    }
-  }
-
-  @media only screen and (max-width: 656px) {
-    .infrastructure-optimisation {
-      &__flex-wrapper {
-        display: flex;
-        flex-wrap: wrap;
-      }
-    }
-  }
-
-  @media only screen and (max-width: 520px) {
-    .infrastructure-optimisation {
-      padding: 60px 0;
-
-      &__flex-wrapper  {
-        margin-top: 20px;
-      }
-    }
-  }
-
-  @media only screen and (max-width: 375px) {
-    .infrastructure-optimisation {
-      &__flex-wrapper {
-        margin-top: 42px;
-      }
-    }
-  }
-
-  @media only screen and (max-width: 340px) {
-    .infrastructure-optimisation {
+    @media screen and  (max-width: 576px) {
       &__main-title {
-        font-size: 42px;
+        padding-left: 18px;
+        padding-right: 18px;
+      }
+
+      &__content {
+        padding: 70px 44px 44px;
+      }
+    }
+
+    @media screen and  (max-width: 440px) {
+      &__content {
+        padding: 64px 24px 32px 24px;
+      }
+    }
+
+    @media screen and  (max-width: 380px) {
+      &__main-title {
+        padding-left: 0;
+        padding-right: 0;
       }
     }
   }
