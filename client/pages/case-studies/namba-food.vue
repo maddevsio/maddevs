f155296576e87dc56296318bbbb72f183e339fa3
AntonGrushkin <merynes345@gmail.com>
2020-11-09 20:45:24 +0600

    added case studies folder and page file

---
+++ client/pages/case-studies/namba-food.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="main"></main>
+</template>
+
+<script>
+export default {
+  name: 'NambaFood',
+  data() {
+    return {
+      title: 'Mad Devs Namba Food',
+      description: '',
+      ogUrl: ''
+    };
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { name: 'description', content: this.description },
+        { property: 'og:url', content: this.ogUrl },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: this.title },
+        { property: 'og:description', content: this.description },
+        {
+          property: 'og:image',
+          content: 'https://maddevs.io/Open-Graph.png'
+        }
+      ]
+    };
+  }
+};
+</script>
