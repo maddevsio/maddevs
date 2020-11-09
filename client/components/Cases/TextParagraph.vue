a329116b294f29ed41195b3b2352971b1b3be944
m1rotvorez <volkik91@gmail.com>
2020-11-09 22:24:26 +0600

    Added custom components for Cases flow, added new components without styles - only data;

---
+++ client/components/Cases/TextParagraph.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="case_paragraph">
+    <slot></slot>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'TextParagraph'
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/components/Cases/TextQuote.vue
@@ -0,0 +1,15 @@
+<template>
+  <blockquote class="case_blockquote">
+    <slot></slot>
+  </blockquote>
+</template>
+
+<script>
+export default {
+  name: 'TextQuote'
+};
+</script>
+
+<style scoped>
+
+</style>
