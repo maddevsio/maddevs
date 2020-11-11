27bec2e730bdfe14d41291f60dbf4108b5e4b8c9
AntonGrushkin <merynes345@gmail.com>
2020-11-11 21:06:39 +0600

    MDW-1565 added styles for Team list

---
+++ client/components/Cases/TeamList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="case_team-list">
+    <slot></slot>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'TeamList'
+};
+</script>
+
+<style scoped lang="scss">
+  @import '../../assets/styles/cases/_mixins';
+
+  .case_team-list {
+    @include team_list;
+  }
+</style>
---
+++ client/components/Cases/TeamMember.vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="case_team-member">
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TeamMember'
+};
+</script>
+
+<style scoped lang="scss">
+  @import '../../assets/styles/cases/_mixins';
+  
+  .case_team-member {
+    @include team_member;
+
+    &__info-wrap {
+      @include team-member_info;
+    }
+
+    &__full-name {
+      @include team-member_full-name($text-color--black-cases); 
+    }
+
+    &__position {
+      @include team-member_position($text-color--black-cases);
+    }
+
+    &__image {
+      @include team-member_image;
+    }
+  }
+</style>
