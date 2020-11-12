ed77429091b50a0ac198235145bdceb8a68ba1fd
AntonGrushkin <merynes345@gmail.com>
2020-11-12 20:49:35 +0600

    MDW-1565 prepared component ListItemDot

---
+++ client/components/Cases/ListItemDot.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="list-item_dot">
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ListItemDot'
+};
+</script>
+
+<style scoped lang="scss">
+  @import '../../assets/styles/cases/_mixins';
+
+  .list-item_dot {
+    @include list-item_dot($text-color--black-cases);
+  }
+</style>
