1ddd6a99db67fa18e4089aa9f0f98377e9e4adbe
AntonGrushkin <merynes345@gmail.com>
2020-11-12 11:07:04 +0600

    MDW-1565 added new styles for List items

---
+++ client/components/Cases/ListGreyItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="list-item">
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ListGreyItem'
+};
+</script>
+
+<style scoped lang="scss">
+  @import '../../assets/styles/cases/_mixins';
+
+  .list-item {
+    @include list-grey-item_container($bgcolor--white);
+
+    &__text {
+      @include list-grey-item_text($text-color--black-cases);
+    }
+
+    &__checkmark {
+      @include list-grey-item_checkmark($text-color--black-cases);
+    }
+  }
+</style>
