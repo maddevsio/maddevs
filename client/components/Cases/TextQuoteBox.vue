384fefe644e88e7b87db094be49841243c377e8c
AntonGrushkin <merynes345@gmail.com>
2020-11-12 21:12:40 +0600

    MDW-1565 added styles for TextQouteBox

--- client/components/Cases/TextQuoteBox.vue
+++ client/components/Cases/TextQuoteBox.vue
@@ -10,6 +10,10 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  @import '../../assets/styles/cases/_mixins';
 
+  .case_blockquote__box {
+    @include text-quot_box($text-color--quote-box, $border-color--red);
+  }
 </style>
