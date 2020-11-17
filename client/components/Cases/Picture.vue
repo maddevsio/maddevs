6ded42ac83bfdf9230f90f680b7e2019fed5a504
AntonGrushkin <merynes345@gmail.com>
2020-11-17 12:19:50 +0600

    added logic for Picture component

--- client/components/Cases/Picture.vue
+++ client/components/Cases/Picture.vue
@@ -2,22 +2,40 @@
   <!-- Добавить цикл подключающий изображение на основе его имени в медиа дериктории взятое из родительского компонента -->
   <picture>
     <source
-      type='image/webp'
+      class="multi-image"
+      :srcset="[require(`@/assets/img/Cases/webp/${fileName}.webp`) + ' ', require(`@/assets/img/Cases/webp/${fileNameRetina}.webp`) + ' 2x']"
+      type='image/webp'>
     >
     <img
-      src=""
+      class="multi-image"
+      :src="[require(`@/assets/img/Cases/jpg/${fileName}.jpg`)]"
+      :srcset="[require(`@/assets/img/Cases/jpg/${fileNameRetina}.jpg`) + ' 2x']"
       alt=""
       loading="lazy"
     >
   </picture>
 </template>
 
+
 <script>
 export default {
-  name: 'Picture'
+  name: 'Picture',
+  props: {
+    fileName: {
+      type: String,
+      default: ''
+    },
+    fileNameRetina: {
+      type: String,
+      default: ''
+    }
+  }
 };
 </script>
 s
 <style scoped>
-
+  .multi-image {
+    width: 100%;
+    height: 100%;
+  }
 </style>
