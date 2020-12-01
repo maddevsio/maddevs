8a1425bfc5f95181e2c9e5d86d249f1ef918b8e5
AntonGrushkin <merynes345@gmail.com>
2020-12-01 13:11:08 +0600

    added new styles for NF case footer

--- client/components/Cases/Footer.vue
+++ client/components/Cases/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="case_footer">
+  <section class="case_footer container_full">
     <div class="case_logotype nambafood"></div>
     <h2 class="case_title case_title_h2">
       <slot></slot>
@@ -19,7 +19,6 @@ export default {
 
 .case {
   &_footer {
-    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -46,6 +45,28 @@ export default {
     border: 0;
     cursor: pointer;
     @include default_text($text-color--white-primary, 17px, 166%, -0.035em, normal);
+  }
+
+  @media screen and (max-width: 768px) {
+    &_footer {
+      padding-top: 70px;
+      padding-bottom: 100px;
+    }
+
+    &_logotype {
+      @include nambafood-footer;
+      width: 91px;
+      height: 81px;
+      margin-bottom: 31px;
+    }
+
+    &_title {
+      margin-bottom: 41px;
+    }
+
+    &_next-project-button {
+      @include default_text($text-color--white-primary, 16px, 150%, -0.02em, normal);
+    }
   }
 }
 </style>
