23bc2d3f05ebca147c5470e45d172dbbdf022d87
AntonGrushkin <merynes345@gmail.com>
2020-12-07 15:59:30 +0600

    added new styles for text in Quote box

--- client/components/Cases/TextQuoteBox.vue
+++ client/components/Cases/TextQuoteBox.vue
@@ -17,7 +17,7 @@ export default {
     &_blockquote-box {
       padding-left: 35px;
       border-left: 2px solid $border-color--red;
-      @include italic_text($text-color--quote-box, 18px, 33px, -0.035em);
+      @include italic_text($text-color--quote-box, 17px, 166%, -0.035em);
 
       &_text-modificator {
         color: $text-color--red;
