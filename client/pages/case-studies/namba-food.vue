5c3da1cc8e95c5abc760c44982e1871d33afbbe3
AntonGrushkin <merynes345@gmail.com>
2020-12-11 16:23:07 +0600

    update case page markup

--- client/pages/case-studies/namba-food.vue
+++ client/pages/case-studies/namba-food.vue
@@ -92,10 +92,7 @@
           As Namba Food’s delivery service grew more popular, the load on customer support and its operators increased exponentially.
         </TextParagraph>
         <TextParagraph class="m-24_bottom media-m-12_bottom">
-          Customer support challenge: The lack of transparent status updates led to the majority of users calling to check the delivery status.
-        </TextParagraph>
-        <TextParagraph class="m-24_bottom media-m-12_bottom">
-          Business challenge: A continuously growing number of customer support teams occurred in high operational expenses.
+          Customer support challenge: The lack of transparent status updates led to the majority of users calling to check the delivery status. Business challenge: A continuously growing number of customer support teams occurred in high operational expenses.
         </TextParagraph>
         <TextParagraph class="m-96_bottom media-m-48_bottom">
           Solution: The Mad Devs team has developed a user-friendly status feature that allows customers to easily track progress on both the mobile app and website.
