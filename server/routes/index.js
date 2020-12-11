05159ae56ae96ee973d74dad3e7050e296b4690e
dmhorbachov <digorbatchev@gmail.com>
2020-12-11 12:27:55 +0200

    Call npm run generate when prismic document is published

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,4 @@
+const exec = require('child_process').exec;
 const express = require('express');
 const router = express.Router();
 const sendpulse = require('sendpulse-api');
@@ -7,6 +8,18 @@ dotenv.config();
 const API_USER_ID = process.env.NODE_SENDPULSE_API_USER_ID;
 const API_KEY = process.env.NODE_SENDPULSE_API_KEY;
 const TOKEN_STORAGE = '/tmp/';
+
+async function call(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(stdout, stderr);
+      }
+    });
+  });
+}
 
 router.post('/send-email', (req, res) => {
   if (req.body.templateId === null || req.body.templateId === undefined) {
@@ -67,6 +80,32 @@ router.post('/send-email', (req, res) => {
       sendpulse.smtpSendMail(answerGetter, email);
     });
   }
+});
+
+router.post('/prismic-hook', (req, res) => {
+  if (!req.body.domain || req.body.domain !== 'superpupertest') {
+    res.status(404).json({
+      status: 403,
+      message: 'Access forbidden'
+    });
+  }
+
+  // Document is published or unpublished
+  if (req.body.type === 'api-update' && !!req.body.documents && req.body.documents.length) {
+    call('npm run generate')
+      .then((stdout, stderr) => console.log(stdout, stderr))
+      .catch(err => console.log('error', err));
+
+    res.status(200).json({
+      status: 200,
+      message: 'Success'
+    });
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'error'
+  });
 });
 
 module.exports = router;
