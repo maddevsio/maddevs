993047b6ad9f30d405f4d6661a1933fe1ed423a4
AntonGrushkin <merynes345@gmail.com>
2020-12-14 12:04:36 +0600

    added SJMC page and part of markup

---
+++ client/pages/case-studies/sir-john-monash-centre.vue
@@ -0,0 +1,210 @@
+<template>
+  <main class="main case">
+    <Header/>
+    <section class="case_body">
+      <section class="container_regular">
+        <TextParagraph class="m-24_bottom media-m-12_bottom">
+          Sir John Monash Centre (SJMC) tells the Australian and New Zealand army corps’ story of the Western Front in the words of those who served.
+        </TextParagraph>
+        <TextParagraph class="m-24_bottom media-m-12_bottom">
+          WWI was an incredible teacher. However, its wisdom is thought by dull, dehumanized historical books that explain events on a surface level. In contrast, the 480-square meter Centre takes visitors to a journey telling the stories of ordinary soldiers who went through those horrible events using interactive multimedia installations and technologies.
+        </TextParagraph>
+        <TextParagraph>
+          Technologies allow SJMC to personalize the experience for each visitor and tell ANZAC’s stories with "full immersion" that you've never seen anywhere else.  
+        </TextParagraph>
+        <TextQuote class="m-auto m-96_top m-96_bottom media-m-48_top media-m-72_bottom">
+          World-class BYOD technology deliver an enhanced understanding of the nature and context of the ANZAC contribution to World War I.
+        </TextQuote>
+      </section>
+      <section class="container_regular">
+        <h2 class="case_title_h2 m-12_bottom">SJMC and Mad Devs partnership</h2>
+        <TextParagraph class="m-24_bottom media-m-12_bottom">
+          SJMC officially opened its doors on April 16th, 2018. The entire centre with its underground building and cutting-edge technologies was already developed and deployed by an Australia-based IT vendor. Thus, the SJMC and Mad Devs partnership was based on enhancements and preventative maintenance contracts.
+        </TextParagraph>
+        <TextParagraph>
+          After conducting an independent audit by a previous IT vendor, a list of technical debt was formed.
+        </TextParagraph>
+        <TextQuoteBox class="m-auto m-48_top m-48_bottom media-m-24_top media-m-24_bottom">
+          Mad Devs team has successfully accomplished code refactoring as well as closing the technical debt. After that, system monitoring, new feature development, and maintenance lied on Mad Devs shoulders.
+        </TextQuoteBox>
+      </section>
+      <section class="container_full">
+        <!-- <Picture
+          pictureFolder=""
+          fileName=""
+          fileExtension=""
+          alt=""
+        /> -->
+      </section>
+      <section class="container_regular">
+        <!-- <List></List> -->
+      </section>
+      <section class="container_regular">
+        <h2 class="case_title_h2 m-12_bottom">SJMC and Mad Devs partnership</h2>
+        <TextParagraph class="m-48_bottom media-m-24_bottom">
+          Transferring a project to another software partner is never easy for all parties involved. So, let us introduce you to the new era of the interpretive centres. And walk you through the complexity of the SJMC project.
+        </TextParagraph>
+        <h3 class="case_title_h3 m-12_bottom">SJMC and Mad Devs partnership</h3>
+        <TextParagraph class="m-48_bottom media-m-24_bottom">
+          Personalized experience at your own speed and language
+        </TextParagraph>
+      </section>
+      <section class="container_middle">
+        <!-- <Picture
+          pictureFolder=""
+          fileName=""
+          fileExtension=""
+          alt=""
+        /> -->
+      </section>
+      <section class="container_regular">
+        <TextParagraph class="m-48_top media-m-24_top">
+          Since existing available solutions had limitations and restrictions, Sir John Monash Centre hired an Australia-based IT company to create an integrated multimedia solution to coordinate and communicate with every piece of technology in the centre.
+        </TextParagraph>
+        <TextParagraph>
+          MMIT Brain synchronizes two information systems: multimedia management system and smart building management system. Multimedia software and hardware management system controls, supervises, and monitors lighting, audio, video, and special effects for SJMC. Universal software infrastructure to building controls infrastructure integrators is the third-party framework that works as a foundation of SJMC intelligent building. It supports a complete IP-based information network for real-time management and controls SJMC's multiple facilities. It is a one-stop software that manages all building operations, energy, security, and operational systems.
+        </TextParagraph>
+        <TextQuote class="m-auto m-96_top m-96_bottom media-m-48_top media-m-72_bottom">
+          MMIT Brain is a coherent solution that brings together the monitoring and management of two information systems.
+        </TextQuote>
+        <TextParagraph class="m-56_bottom media-m-24_bottom">
+          MMIT Brain orchestrated the synchronization of every thread together into one coherent solution. Physically constructed as a single hardware and software, it performs various services under one hood. Here is a list of functions MMIT Brain performs.
+        </TextParagraph>
+        <List class="m-56_bottom media-m-24_bottom">
+          <ListItemDot>Delivers content to mobile applications and synchronizes subtitles and audio on multimedia screens in 3 languages (German, English, and French).</ListItemDot>
+          <ListItemDot>Orchestrates the experience of multiple SJMC visitors to avoid interference.</ListItemDot>
+          <ListItemDot>Performs interaction between two internal information systems.</ListItemDot>
+          <ListItemDot>Enables content managers to deploy new content and modify it.</ListItemDot>
+          <ListItemDot>Synchronizes content storage and other multimedia computers.</ListItemDot>
+        </List>
+      </section>
+      <section class="container_full">
+        <!-- <Picture
+          pictureFolder=""
+          fileName=""
+          fileExtension=""
+          alt=""
+        /> -->
+      </section>
+      <section class="container_regular">
+        <h2 class="case_title_h2 m-24_bottom media-m-12_bottom">Mobile applications</h2>
+        <TextParagraph class="m-48_bottom media-m-24_bottom">
+          Since existing available solutions had limitations and restrictions, Sir John Monash Centre hired an Australia-based IT company to create an integrated multimedia solution to coordinate and communicate with every piece of technology in the centre.
+        </TextParagraph>
+      </section>
+      <section class="container_full">
+        <!-- <Picture
+          pictureFolder=""
+          fileName=""
+          fileExtension=""
+          alt=""
+        /> -->
+      </section>
+      <section class="container_regular">
+        <TextQuoteBox class="m-auto m-48_top m-48_bottom media-m-24_top media-m-24_bottom">
+          As soon as visitors step into the SJMC’s mobile applications with the help of advanced positioning algorithms proactively cache content, cue up relevant multimedia, and guide visitors through the stories of ANZAC soldiers.
+        </TextQuoteBox>
+        <TextParagraph class="m-24_bottom media-m-12_bottom">
+          The gallery's small size and the volume and density of signal traffic from visitors is a challenge that was solved using a complex, layered positioning algorithm that was developed to help deal with the unique challenges of SJMC’s physical environment. 
+        </TextParagraph>
+        <TextParagraph class="m-48_bottom media-m-24_bottom">
+          When visitors move around the Centre, the app will continually refresh a list of what content is available nearby and allow them to dictate their own experience.
+        </TextParagraph>
+      </section>
+      <section class="container_regular">
+        <h2 class="case_title_h2 m-12_bottom">SJMC Hardware</h2>
+        <TextParagraph class="m-48_bottom media-m-24_bottom">
+          Hundreds of pieces of equipment are master-mindedly organized to transform content stored in audio, video, subtitles format to interactive monitors.
+        </TextParagraph>
+      </section>
+      <section class="container_full">
+        <!-- <Picture
+          pictureFolder=""
+          fileName=""
+          fileExtension=""
+          alt=""
+        /> -->
+      </section>
+      <section class="container_regular">
+        <h3 class="case_title_h3 m-12_bottom">WATCHOUT</h3>
+        <TextParagraph class="m-48_bottom media-m-24_bottom">
+          WATCHOUT is a multi-display production and presentation system that enables SJMC to arrange displays in unusual ways and create seamless images and videos. The absence of gaps between display surfaces is possible due to the WATCHOUT system that distributes the content and keeps everything in sync across all connected displays.
+        </TextParagraph>
+      </section>
+      <section class="container_middle">
+        <!-- <Picture
+          pictureFolder=""
+          fileName=""
+          fileExtension=""
+          alt=""
+        /> -->
+      </section>
+      <section class="container_regular">
+        <h3 class="case_title_h3 m-12_bottom">BRIX</h3>
+        <TextParagraph class="m-48_bottom media-m-24_bottom">
+          Essentially BRIX is an ultra-compact multimedia computer that can be hung on a wall. The advanced graphics card simultaneously delivers heavy content to multiple monitors. Moreover, each BRIX is managed by MMIT Brain to work in synchronization with mobile applications.
+        </TextParagraph>
+      </section>
+      <section class="container_middle">
+        <!-- <Picture
+          pictureFolder=""
+          fileName=""
+          fileExtension=""
+          alt=""
+        /> -->
+      </section>
+      <section class="container_regular">
+        <h3 class="case_title_h3 m-96_top media-m-48_top m-12_bottom">Bluetooth beacons</h3>
+        <TextParagraph class="m-24_bottom media-m-12_bottom">
+          Mobiles applications positioning algorithm collects the location data of each user’s mobile application via 150 Bluetooth beacons located all over the centre. 
+        </TextParagraph>
+        <TextParagraph class="m-48_bottom media-m-24_bottom">
+          The limited space of SJMC creates a challenge to be able to detect each visitors' location and continuously refresh a list of available content nearby.
+        </TextParagraph>
+      </section>
+    </section>
+  </main>
+</template>
+
+<script>
+import TextParagraph from '@/components/Cases/TextParagraph';
+import TextQuote from '@/components/Cases/TextQuote';
+import TextQuoteBox from '@/components/Cases/TextQuoteBox';
+import Picture from '@/components/Cases/Picture';
+import List from '@/components/Cases/List';
+import ListNumberedBox from '@/components/Cases/ListNumberedBox';
+import ListNumberedItemBox from '@/components/Cases/ListNumberedItemBox';
+import ListTeam from '@/components/Cases/ListTeam';
+import ListTeamItem from '@/components/Cases/ListTeamItem';
+import ListItemBoxCheckMark from '@/components/Cases/ListItemBoxCheckMark';
+import ListItemDot from '@/components/Cases/ListItemDot';
+import ListTechnologies from '@/components/Cases/ListTechnologies';
+import ListTechnologiesItem from '@/components/Cases/ListTechnologiesItem';
+import TextQuoteAuthor from '@/components/Cases/TextQuoteAuthor';
+
+export default {
+  components: {
+    TextParagraph,
+    TextQuote,
+    TextQuoteBox,
+    Picture,
+    List,
+    ListNumberedBox,
+    ListNumberedItemBox,
+    ListTeam,
+    ListTeamItem,
+    ListItemBoxCheckMark,
+    ListItemDot,
+    ListTechnologies,
+    ListTechnologiesItem
+  },
+  name: 'SirJohnMonashCentre'
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/styles/cases/base";
+@import "../../assets/styles/cases/mixins";
+@import "../../assets/styles/cases/components";
+@import "../../assets/styles/cases/media";
+</style>
