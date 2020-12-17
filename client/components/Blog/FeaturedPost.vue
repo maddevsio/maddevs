6b331177714218b306f731cf88d833b091f2f5d8
dmhorbachov <digorbatchev@gmail.com>
2020-12-17 15:30:26 +0200

    MDW-1770: Blog main page (#545)
    * MDW-1770: First section on blog main page
    
    * MDW-1770 replace filters with tabs
    
    * MDW-1770 change blog main page styles language to scss
    
    * MDW-1770: replace cover image in recommended post widget to background
    
    * MDW-1770: Layout responsiveness; one-row filters on mobile
    
    * MDW-1770: update recommended blog widget snapshot
    
    * hide scroll rales
    
    * MDW-1770: Spacings fix
    
    * MDW-1770: Featured post components tests
    
    * MR comments fixes
    
    * Tests fix

---
+++ client/components/Blog/FeaturedPost.vue
@@ -0,0 +1,173 @@
+<template>
+  <nuxt-link :to="link" class="featured-post">
+    <div class="row featured-post__wrapper">
+      <div class="col-12 col-lg-6 featured-post__main">
+        <h1 class="featured-post__title">{{ $prismic.asText(post.data.title) }}</h1>
+        <p class="featured-post__paragraph">{{ getFirstParagraph() }}</p>
+        <div class="featured-post__data d-flex justify-content-between">
+          <post-author :document="post.data"/>
+          <div class="featured-post__meta">
+            <post-tag :tag="post.tags[0]" theme="dark" v-if="post.tags.length"/>
+            <span class="created-at">{{ formattedDate }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6 featured-post__image-wrapper">
+        <img :src="post.data.featured_image.url" alt="" class="featured-post__image">
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+import PostAuthor from '@/components/Blog/PostAuthor';
+import PostTag from '@/components/Blog/PostTag';
+import linkResolver from '@/plugins/link-resolver.js';
+
+export default {
+  name: 'FeaturedPost',
+  components: {
+    PostAuthor,
+    PostTag
+  },
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    getFirstParagraph () {
+      const textLimit = 200;
+      const slices = this.post.data.body;
+      let firstParagraph = '';
+      let haveFirstParagraph = false;
+
+      slices.map(function(slice) {
+        if (!haveFirstParagraph && slice.slice_type == 'text') {
+          slice.primary.text.forEach(function(block){
+            if (block.type == 'paragraph' && !haveFirstParagraph) {
+              firstParagraph += block.text;
+              haveFirstParagraph = true;
+            }
+          });
+        }
+      });
+
+      const limitedText = firstParagraph.substr(0, textLimit);
+
+      return firstParagraph.length > textLimit
+        ? limitedText.substr(0, limitedText.lastIndexOf(' ')) + '...'
+        : firstParagraph;
+    }
+  },
+  computed: {
+    formattedDate: function(){
+      const date = this ? this.post.data.date : '';
+
+      return Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(date));
+    },
+    link: function () {
+      return linkResolver(this.post);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  @import '../../assets/styles/_vars';
+
+  .featured-post {
+    color: $text-color--white-primary;
+    text-decoration: none;
+    display: block;
+
+    &__wrapper {
+      margin: 96px 0;
+      align-items: center;
+    }
+
+    &__title {
+      font-size: 52px;
+      line-height: 67px;
+      margin-bottom: 15px;
+    }
+
+    &__paragraph {
+      font-size: 17px;
+      line-height: 28px;
+      margin-bottom: 44px;
+    }
+
+    &__data {
+      display: flex;
+      justify-content: space-between;
+
+      /deep/ .blog-post__author-name {
+        color: $text-color--white-primary;
+      }
+    }
+
+    &__meta {
+      display: flex;
+      align-items: center;
+      font-size: 13px;
+      font-family: 'Inter-Regular', sans-serif;
+
+      .created-at {
+        margin-left: 24px;
+        color: $text-color--grey
+      }
+    }
+
+    &__image-wrapper {
+      text-align: right;
+
+      img {
+        max-width: 90%;
+        height: auto;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 991px) {
+    .featured-post {
+
+      &__main {
+        order: 2;
+      }
+
+      &__paragraph {
+        margin-bottom: 38px;
+      }
+
+      &__data {
+        display: block;
+
+        /deep/ .blog-post__author {
+          margin-bottom: 16px;
+        }
+      }
+
+      &__meta {
+        justify-content: space-between;
+
+        /deep/ .post-tag {
+          padding: 8px 16px;
+          line-height: 22px;
+          margin-left: 0;
+        }
+      }
+
+      &__image-wrapper {
+        order: 1;
+        margin-bottom: 16px;
+        text-align: center;
+
+        img  {
+          max-width: 100%;
+        }
+      }
+    }
+  }
+</style>
