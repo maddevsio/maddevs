95867a370628f6f1e955a276c2e8486fff77af38
AntonGrushkin <merynes345@gmail.com>
2020-12-24 13:18:17 +0600

    added new tests for HeaderSJMC

--- tests/caseHeaderSJMC.spec.js
+++ tests/caseHeaderSJMC.spec.js
@@ -17,13 +17,15 @@ describe('HeaderSJMC', () => {
     wrapper = mount(HeaderSJMC, {
       mocks: {
         $nuxt: {
-          $on: jest.fn()
+          $on: jest.fn(),
+          $emit: jest.fn()
         }
       },
       stubs: {
         'SJMCVideo': VueVideoStub
       }
     });
+    navigator.__defineGetter__('userAgent', () => 'iPhone'); // Set custom userAgent
   });
 
   // ------ IMPORTANT ----- //
@@ -35,4 +37,13 @@ describe('HeaderSJMC', () => {
     expect(wrapper.element).toMatchSnapshot();
   });
   // --------------------- //
+
+  test('isIphone should equal true', () => {
+    expect(wrapper.vm.$data.isIphone).toEqual(true);
+  });
+
+  test('Have been called emit', () => {
+    wrapper.vm.$emit('open-fullscreen');
+    expect(wrapper.emitted('open-fullscreen')).toBeTruthy();
+  });
 });
