026261f51667aaf96adeb84e951041f733167303
dmhorbachov <digorbatchev@gmail.com>
2021-01-05 13:34:44 +0200

    Return 404 on blog pages (#567)

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,24 +59,24 @@ module.exports = {
   ],
   generate: {
     async routes() {
-      // const getPosts = async pageUrl => {
-      //   let posts = [];
-      //   const response = await axios.get(pageUrl);
-      //   posts = posts.concat(response.data.results);
-      //
-      //   if (response.data.next_page) {
-      //     posts = posts.concat(await getPosts(response.data.next_page));
-      //   }
-      //   return posts;
-      // };
+      const getPosts = async pageUrl => {
+        let posts = [];
+        const response = await axios.get(pageUrl);
+        posts = posts.concat(response.data.results);
 
-      return ['/', '/services', '/projects', '/careers', '/gdpr', '/nda', '/privacy', '/faq', '/case-studies/namba-food', '/case-studies/sir-john-monash-centre'];
-      // const prismicData = await axios.get(process.env.NODE_PRISMIC_API);
-      // const ref = prismicData.data.refs[0].ref;
-      // const blogPosts = await getPosts(`${process.env.NODE_PRISMIC_API}/documents/search?ref=${ref}#format=json`);
-      // const postRoutes = blogPosts.map(blogPost => '/blog/' + blogPost.uid);
+        if (response.data.next_page) {
+          posts = posts.concat(await getPosts(response.data.next_page));
+        }
+        return posts;
+      };
 
-      // return routes.concat(postRoutes);
+      const routes = ['/', '/services', '/projects', '/careers', '/gdpr', '/nda', '/privacy', '/faq', '/case-studies/namba-food', '/case-studies/sir-john-monash-centre'];
+      const prismicData = await axios.get(process.env.NODE_PRISMIC_API);
+      const ref = prismicData.data.refs[0].ref;
+      const blogPosts = await getPosts(`${process.env.NODE_PRISMIC_API}/documents/search?ref=${ref}#format=json`);
+      const postRoutes = blogPosts.map(blogPost => '/blog/' + blogPost.uid);
+
+      return routes.concat(postRoutes);
     }
   },
   css: [
