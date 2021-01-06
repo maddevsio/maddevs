7d47d48c92f9be27e7f52be4dcd2435b19b5583f
dmhorbachov <digorbatchev@gmail.com>
2021-01-06 13:35:15 +0200

    MDW-1883: Add lightbox option to ImageCaptionSlice component

--- client/components/Blog/slices/ImageCaptionSlice.vue
+++ client/components/Blog/slices/ImageCaptionSlice.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
-    <template v-if="size === 'image-full-width'">
-      <div class='blog-header single' :style="{ 'background-image': 'url(' + img.url + ')'}">
-        <template v-if="$prismic.asText(caption) != ''">
-          <div class="wrapper" v-html="$prismic.asHtml(caption)"/>
-        </template>
-      </div>
-    </template>
-    <template v-else>
-      <div class='post-part single'>
-        <p class="block-img" :class="size">
-          <prismic-image :field="img"/>
-        </p>
-        <template v-if="$prismic.asText(caption) != ''">
-          <p class="image-label">{{ $prismic.asText(caption) }}</p>
-        </template>
-      </div>
-    </template>
+    <div class='post-part single'>
+      <p class="block-img" :class="size">
+        <silent-box v-if="zoomEnabled" :gallery="[lightBoxImage]"/>
+        <prismic-image v-else :field="img"/>
+      </p>
+      <p class="image-label" v-if="caption">{{ caption }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+// import VueSilentbox from 'vue-silentbox';
+
 export default {
-  props: ['slice'],
+  props: {
+    slice: {
+      type: Object,
+      required: true
+    }
+  },
+  // components: {
+  //   VueSilentbox
+  // },
   name: 'image-caption-slice',
   data: function() {
     return {
@@ -33,8 +33,18 @@ export default {
   },
   created() {
     this.img = this.slice.primary.image;
-    this.caption = this.slice.primary.caption;
+    this.caption = this.$prismic.asText(this.slice.primary.caption);
     this.size = this.slice.slice_label;
+  },
+  computed: {
+    zoomEnabled: function () {
+      return this.slice.primary.enable_zoom === 'enable';
+    },
+    lightBoxImage: function () {
+      return {
+        src: this.img.url
+      };
+    }
   }
 };
 </script>
@@ -62,5 +72,14 @@ img {
   font-family: 'Poppins-Regular', sans-serif;
   color: $text-color--grey;
   margin-bottom: 12px;
+}
+
+/deep/ .silentbox-item {
+  img {
+    max-width: 100%;
+    max-height: 70vh;
+    height: auto;
+    width: auto;
+  }
 }
 </style>
