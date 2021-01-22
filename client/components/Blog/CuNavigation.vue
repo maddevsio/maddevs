cdd4402cb10283765206c40860be0ab5ad9dc3a9
dmhorbachov <digorbatchev@gmail.com>
2021-01-22 13:54:48 +0200

    MDW-1943: Add Customer University inner page support in blog (#597)
    * MDW-1940: handle blog.maddevs.io requests with redirects
    
    * MDW-1940: handle blog.maddevs.io requests with redirects
    
    * MDW-1940: Add redirect to blog root
    
    * MDW-1940: Return 404 page if no post found
    
    * MDW-1940: handle header change on post type change
    
    * MDW-1943: Move wrap blog headers to common component
    
    * MDW-1943: Add CU post type support in blog
    
    * MDW-1943: badges update
    
    * MDW-1943: add simple tests for new components
    
    * MDW-1943: Review comments processed
    
    * MDW-1943: more comments processed
    
    * MDW-1943: icons attachment fix
    
    * MDW-1943: snapshot update

---
+++ client/components/Blog/CuNavigation.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="cluster-navigation" v-if="clusterPosts">
+    <div class="container">
+      <div class="cluster-navigation__title">{{ $prismic.asText(cluster.primary.read_more_text) }}</div>
+      <div class="cluster-navigation__inner">
+        <div class="cluster-navigation__column cluster-navigation__column--left">
+          <router-link :to="`/blog/${post.cu_post.uid}`" class="cluster-navigation__nav-item" :class="isCurrent(post)" v-for="post in leftColumnPosts" :key="post.cu_post.id">
+            {{ $prismic.asText(post.chapter_name) }}
+          </router-link>
+        </div>
+        <div class="cluster-navigation__column cluster-navigation__column--right">
+          <router-link :to="`/blog/${post.cu_post.uid}`" class="cluster-navigation__nav-item" :class="isCurrent(post)" v-for="post in rightColumnPosts" :key="post.cu_post.id">
+            {{ $prismic.asText(post.chapter_name) }}
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CuNavigation',
+  props: {
+    clusterPosts: {
+      type: Array,
+      required: true
+    },
+    cluster: {
+      type: Object,
+      required: true
+    },
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    isCurrent(post) {
+      return post.cu_post.id === this.id ? 'current' : '';
+    }
+  },
+  computed: {
+    leftColumnPosts: function () {
+      return this.clusterPosts.slice(0, Math.ceil(this.clusterPosts.length / 2));
+    },
+    rightColumnPosts: function () {
+      return this.clusterPosts.slice(Math.ceil(this.clusterPosts.length / 2));
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  @import "../../assets/styles/vars";
+
+  .cluster-navigation {
+    background: $bgcolor--silver;
+    padding: 77px 0 85px;
+
+    .container {
+      max-width: 924px;
+    }
+
+    &__title {
+      font-size: 26px;
+      line-height: 130%;
+      font-family: 'Poppins-Medium', sans-serif;
+      font-style: normal;
+      font-weight: 600;
+      text-align: center;
+      letter-spacing: -0.04em;
+      color: $text-color--tech-label-black;
+      margin-bottom: 21px;
+    }
+
+    &__column {
+      width: 49.46%;
+    }
+
+    &__inner {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    &__nav-item {
+      text-decoration: none;
+      color: $text-color--black-cases;
+      display: block;
+      background: $bgcolor--white-primary;
+      border-radius: 8px;
+      font-family: Inter, sans-serif;
+      font-size: 16px;
+      line-height: 151%;
+      letter-spacing: -0.035em;
+      margin-bottom: 11px;
+      padding: 16px;
+      min-height: 48px;
+
+      &.current {
+        opacity: 0.5;
+        cursor: default;
+      }
+    }
+  }
+</style>
