bfce788c96420fbc02341c8e783d94d501ba1bc7
Denisoed <denisoed@gmail.com>
2021-02-16 12:22:33 +0600

    MDW-2053 commented amocrm route

--- server/routes.js
+++ server/routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const sendpulseRoutes = require('./sendpulse/routes');
-const amocrmRoutes = require('./amocrm/routes');
+// const amocrmRoutes = require('./amocrm/routes');
 
 router.use('/sendpulse', sendpulseRoutes);
-router.use('/amocrm', amocrmRoutes);
+// router.use('/amocrm', amocrmRoutes);
 
 module.exports = router;
