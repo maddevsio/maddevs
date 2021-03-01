1d4b0052070125ab6a9d0be0d74648f0156bb364
Anton Grushkin <merynes345@gmail.com>
2021-03-01 13:32:55 +0600

    MR-2451 added strict routing params for server js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 const port = process.env.PORT || 5000;
 
 app.enable('trust proxy');
+app.enable('strict routing');
 
 app.use('/', routes);
 app.use(function applyXFrame(req, res, next) {
