b997bc332876c8b42627bdfae27aaa75b60c07a5
kotyunin <kotyunin.k@gmail.com>
2021-03-04 15:37:11 +0600

    MR-2520 Corrected the comments

--- client/helpers/getExtension.js
+++ client/helpers/getExtension.js
@@ -1,4 +1,4 @@
-// Helper that gets the file extension from a string with a file name or URL text
+// Helper, that gets the file extension from a string with a file name or URL
 export default function(url) {
   if (Boolean(url) && typeof(url) === 'string') return url.split(/[#?]/)[0].split('.').pop().trim();
   return null;
--- tests/helpers/getExtension.spec.js
+++ tests/helpers/getExtension.spec.js
@@ -1,7 +1,7 @@
 import getExtension from '@/helpers/getExtension';
 
 describe('getExtension helper', () => {
-  test('should return the correct string with the extension of the image from prismic URL', () => {
+  test('should return the correct string with the extension of the image from prismic', () => {
     let url =
       'https://images.prismic.io/superpupertest/614c9f98-ac56-4aa4-8f78-afbeb5d8bbac_Frame+1938.jpg?auto=compress,format';
     let extension = getExtension(url);
@@ -14,7 +14,7 @@ describe('getExtension helper', () => {
     expect(extension).toBe('vue');
   });
 
-  test('should return null for param that is not a string', () => {
+  test('should return null for param, that is not a string', () => {
     let notString = 23;
     let extension = getExtension(notString);
     expect(extension).toBe(null);
