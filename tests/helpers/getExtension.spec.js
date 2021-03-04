616188a7843f72237da580cb5530d0831119a192
kotyunin <kotyunin.k@gmail.com>
2021-03-04 15:19:18 +0600

    MR-2520 Added tests for the getExtension helper

---
+++ tests/helpers/getExtension.spec.js
@@ -0,0 +1,22 @@
+import getExtension from '@/helpers/getExtension';
+
+describe('getExtension helper', () => {
+  test('should return the correct string with the extension of the image from prismic URL', () => {
+    let url =
+      'https://images.prismic.io/superpupertest/614c9f98-ac56-4aa4-8f78-afbeb5d8bbac_Frame+1938.jpg?auto=compress,format';
+    let extension = getExtension(url);
+    expect(extension).toBe('jpg');
+  });
+
+  test('should return the correct string with the extension from string with the filename', () => {
+    let fileName = 'test_filename.vue';
+    let extension = getExtension(fileName);
+    expect(extension).toBe('vue');
+  });
+
+  test('should return null for param that is not a string', () => {
+    let notString = 23;
+    let extension = getExtension(notString);
+    expect(extension).toBe(null);
+  });
+});
