dc5c0f13d1e73fee34977b49c960683de46d6188
Anton Grushkin <merynes345@gmail.com>
2021-03-16 18:02:37 +0600

    Added new redirect in rest.js

--- server/middleware/rest.js
+++ server/middleware/rest.js
@@ -33,10 +33,16 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  if (req.url[req.url.length - 1] !== '/' && req.method === 'GET' && req.url !== '/sitemap.xml') {
-    res.redirect(`https://${req.headers.host}${req.url}/`);
-  } else if(req.url === '/sitemap.xml') {
+  if (req.url === '/sitemap.xml') {
     res.redirect(`https://${req.headers.host}${req.url}`);
+  } else {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  if (req.url[req.url.length - 1] !== '/' && req.method === 'GET') {
+    res.redirect(`https://${req.headers.host}${req.url}/`);
   } else {
     next();
   }
