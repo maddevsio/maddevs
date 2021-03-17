e3342d7998015820eb494f4c97a7e5bc57f5e1d9
Roman Chasovitin <chasovitin.r@gmail.com>
2021-03-17 18:46:25 +0600

    Fixed pipeline error.

--- server/config/env.js
+++ server/config/env.js
@@ -11,8 +11,8 @@ const AMOCRM_REDIRECT_URL = process.env.NODE_AMOCRM_REDIRECT_URL;
 const DATABASE_URL = process.env.NODE_MONGO_URL;
 
 // Send pulse
-const SENDPULSE_API_USER_ID = process.env.NODE_SENDPULSE_API_USER_ID;
 const SENDPULSE_API_KEY = process.env.NODE_SENDPULSE_API_KEY;
+const SENDPULSE_API_USER_ID = process.env.NODE_SENDPULSE_API_USER_ID;
 const SENDPULSE_TOKEN_STORAGE = '/tmp/';
 
 module.exports = {
