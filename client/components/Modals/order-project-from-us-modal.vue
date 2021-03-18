4e3b636f8da5091beed26fe6439d3eb7bc380110
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-18 16:04:35 +0600

    MR-2669 - renamed

--- client/components/Modals/order-project-from-us-modal.vue
+++ client/components/Modals/order-project-from-us-modal.vue
@@ -83,7 +83,7 @@
 <script>
 import { required, email, maxLength } from 'vuelidate/lib/validators';
 import { phone } from '@/helpers/validators';
-import { phoneHandler } from '@/mixins/phoenHandler';
+import { phoneHandler } from '@/mixins/phoneHandler';
 import FormCheckboxes from '@/components/ui/form-checkboxes';
 import UIButton from '@/components/ui/UIButton';
 
