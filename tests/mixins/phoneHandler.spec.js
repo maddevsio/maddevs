f5eb57a0815b5eb5195230f708d3f915628690a1
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-19 13:36:53 +0600

    styles

--- tests/mixins/phoneHandler.spec.js
+++ tests/mixins/phoneHandler.spec.js
@@ -1,4 +1,4 @@
-import {phoneHandler} from '../../client/mixins/phoneHandler';
+import { phoneHandler } from '../../client/mixins/phoneHandler';
 
 const INPUT_VALUE = 'zzzzz1234567890aaaa';
 const OUTPUT_VALUE = '+1 2345-678-90';
