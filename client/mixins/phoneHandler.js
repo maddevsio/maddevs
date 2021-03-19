7007aeff24a8ac465c1ee1bd431cd17ed2662531
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-19 12:40:58 +0600

    removed space

--- client/mixins/phoneHandler.js
+++ client/mixins/phoneHandler.js
@@ -6,7 +6,7 @@ export const phoneHandler = {
     phoneChangeHandler(event) {
       const newEvent = event;
       const x = newEvent.target.value.replace(/\D/g, '').match(/(\d{0,1})(\d{0,4})(\d{0,3})(\d{0,4})(\d{0,15})/);
-      newEvent.target.value = !x[2] ? x[1] : '+' + x[1] + ' ' + x[2] + (x[3] ? '-' + x[3] : ' ') + (x[4] ? '-' + x[4] : '') + (x[5] ? '-' + x[5] : '');
+      newEvent.target.value = !x[2] ? x[1] : '+' + x[1] + ' ' + x[2] + (x[3] ? '-' + x[3] : '') + (x[4] ? '-' + x[4] : '') + (x[5] ? '-' + x[5] : '');
 
       this.phoneNumber = newEvent.target.value;
       this.$v && this.$v.phoneNumber.$touch(newEvent);
