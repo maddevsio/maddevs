0a8c05bdf1a84158fdade977b12089610b53dcd3
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-19 14:11:34 +0600

    Refactor tests

--- client/mixins/phoneHandler.js
+++ client/mixins/phoneHandler.js
@@ -4,14 +4,14 @@ export const phoneHandler = {
   }),
   methods: {
     phoneChangeHandler(event) {
-      const modernizedEvent = event;
-      const x = modernizedEvent.target.value.replace(/\D/g, '').match(/(\d{0,1})(\d{0,4})(\d{0,3})(\d{0,4})(\d{0,15})/);
-      modernizedEvent.target.value = !x[2] ? x[1] : '+' + x[1] + ' ' + x[2] + (x[3] ? '-' + x[3] : '') + (x[4] ? '-' + x[4] : '') + (x[5] ? '-' + x[5] : '');
+      if (event && event.target && event.target.value) {
+        const modernizedEvent = event;
+        const x = modernizedEvent.target.value.replace(/\D/g, '').match(/(\d{0,1})(\d{0,4})(\d{0,3})(\d{0,4})(\d{0,15})/);
+        modernizedEvent.target.value = !x[2] ? x[1] : '+' + x[1] + ' ' + x[2] + (x[3] ? '-' + x[3] : '') + (x[4] ? '-' + x[4] : '') + (x[5] ? '-' + x[5] : '');
 
-      this.phoneNumber = modernizedEvent.target.value;
-      this.$v && this.$v.phoneNumber.$touch(modernizedEvent);
-
-      return modernizedEvent;
+        this.phoneNumber = modernizedEvent.target.value;
+        this.$v && this.$v.phoneNumber.$touch(modernizedEvent);
+      }
     }
   }
 };
--- tests/mixins/phoneHandler.spec.js
+++ tests/mixins/phoneHandler.spec.js
@@ -1,11 +1,22 @@
 import { phoneHandler } from '../../client/mixins/phoneHandler';
+import { mount } from '@vue/test-utils';
 
 const INPUT_VALUE = 'zzzzz1234567890aaaa';
 const OUTPUT_VALUE = '+1 2345-678-90';
 
 describe('Phone Handler Mixin',   () => {
+  const wrapper = mount(phoneHandler);
+
+  test('empty', () => {
+    expect(wrapper.vm.$data.phoneNumber).toBe(null);
+    wrapper.vm.phoneChangeHandler([]);
+    expect(wrapper.vm.$data.phoneNumber).toBe(null);
+  });
+
   test('Should return phone without letter symbols and with mask', () => {
-    expect(phoneHandler.methods.phoneChangeHandler({target: {value: INPUT_VALUE}}).target.value).toBe(OUTPUT_VALUE);
+    expect(wrapper.vm.$data.phoneNumber).toBe(null);
+    wrapper.vm.phoneChangeHandler({target: {value: INPUT_VALUE}});
+    expect(wrapper.vm.$data.phoneNumber).toBe(OUTPUT_VALUE);
   });
 });
 
