2d5e4bc423eb26a8aa0de269ffa96bd953ab565c
Roman Chasovitin <chasovitin.r@gmail.com>
2021-03-23 11:08:42 +0600

    Fixed issue with sitemap. Added controller and route for sitemap.xml file.

--- server/routes/web.js
+++ server/routes/web.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 // controllers
 const { switchLanguage } = require('../controllers/LanguageController');
+const { getSitemap } = require('../controllers/SitemapController');
 
 const router = express.Router();
 
 router.get('/ru', switchLanguage);
 router.get('/en', switchLanguage);
+router.get('/sitemap.xml', getSitemap);
 
 module.exports = router;
