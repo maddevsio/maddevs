be03c7514a22a26d2d9d46551d02018a319f118c
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-23 12:07:42 +0600

    MR-2819 - added mount to component

--- tests/mixins/phoneHandler.spec.js
+++ tests/mixins/phoneHandler.spec.js
@@ -1,11 +1,16 @@
 import { phoneHandler } from '../../client/mixins/phoneHandler';
-import { mount } from '@vue/test-utils';
+import {shallowMount} from '@vue/test-utils';
+import FooterContacts from '@/components/Footer/footer-contacts';
 
 const INPUT_VALUE = 'zzzzz1234567890aaaa';
 const OUTPUT_VALUE = '+1 2345-678-90';
 
 describe('Phone Handler Mixin',   () => {
-  const wrapper = mount(phoneHandler);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(FooterContacts, { mixins: [phoneHandler] });
+  });
 
   test('If we pass an incorrect parameter. The function will work, but it will not make changes in the store', () => {
     expect(wrapper.vm.$data.phoneNumber).toBe(null);
