30ac390fc35a510877fe4e7c59d8e26fa47444c3
Roman Chasovitin <chasovitin.r@gmail.com>
2021-03-24 15:01:29 +0600

    Updated eslint config.

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,19 @@ function isBabelLoader(caller) {
   return caller && caller.name === 'babel-loader';
 }
 
-module.exports = function (api) {
+module.exports = api => {
   if (api.env('test') && !api.caller(isBabelLoader)) {
     return {
       presets: [
-        ['@babel/env', {
-          'targets': {
-            'ie': '11'
-          }
-        }]
-      ]
+        [
+          '@babel/env',
+          {
+            targets: {
+              ie: '11',
+            },
+          },
+        ],
+      ],
     };
   }
   return {};
