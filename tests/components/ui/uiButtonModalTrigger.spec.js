5a14e4b012102200673cc7ecfceb212e1fa5d99c
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-25 17:41:54 +0600

    MAR-389 - trigered modal tests

---
+++ tests/components/ui/uiButtonModalTrigger.spec.js
@@ -0,0 +1,38 @@
+import {render, fireEvent, screen} from '@testing-library/vue';
+import UIButtonModalTrigger from '@/components/ui/UIButtonModalTrigger';
+
+const props = {
+  buttonInnerText: 'Some button name',
+  isRed: true
+};
+
+describe('UIButton component', () => {
+  test('should render correctly with slot', () => {
+    const {container} = render(UIButtonModalTrigger, {
+      props
+    });
+
+    expect(screen.getByText(props.buttonInnerText)).not.toBeNull();
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should correctly handle click', async () => {
+    const {emitted, getByText} = render(UIButtonModalTrigger, {
+      props
+    });
+
+    const btn = getByText(props.buttonInnerText);
+    await fireEvent.click(btn);
+    expect(emitted().onClick.length).toBe(1);
+  });
+
+  test('should correctly render colour', async () => {
+    const { getByText} = render(UIButtonModalTrigger, {
+      props
+    });
+
+    const btn = getByText(props.buttonInnerText);
+    console.log(btn.className);
+    expect(btn.className).toContain('ui-button-modal-trigger--red');
+  });
+});
