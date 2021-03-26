b2a0781027a51992eef7683ecfa7d1345e2cb448
Denisoed <denisoed@gmail.com>
2021-03-26 13:49:56 +0600

    MAR-118 fixed func for get media from s3

--- client/plugins/get-media-from-s3.js
+++ client/plugins/get-media-from-s3.js
@@ -3,10 +3,11 @@ import Vue from 'vue'
 Vue.mixin({
   methods: {
     getMediaFromS3(path) {
-      if (!path) throw new ReferenceError('Add the path to the file in s3. Example: /images/file.jpg')
-      // if (path[0] !== '/') throw new ReferenceError('The path must begin with a slash. Example: /images/file.jpg')
+      let resulPath = path
+      if (!path) return null
+      if (path[0] !== '/') resulPath = `/${resulPath}`
       try {
-        return `${process.env.s3PublicUrl}${path}`
+        return `${process.env.s3PublicUrl}${resulPath.replace('//', '/')}`
       } catch (error) {
         throw new Error(error)
       }
