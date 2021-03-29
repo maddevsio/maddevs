8124241d75ab9ddbbbd0622baa35c439de9d47bd
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-29 15:39:37 +0600

    MAR-393 - blog tests

---
+++ tests/components/Blog/header/Blog.spec.js
@@ -0,0 +1,27 @@
+import Blog from '@/components/Blog/header/Blog';
+import {render, screen} from '@testing-library/vue';
+import {blogDoc} from './mocked/blog-document';
+
+
+const props = {
+  document: blogDoc,
+  tags: ['Project Management', 'Featured post'],
+  formattedDate: 'Mar 23, 2021'
+};
+
+describe('Header Blog', () => {
+
+  test('is a Vue instance', () => {
+    const {container} = render(Blog, {
+      props,
+      mocks: {
+        $prismic: {
+          asText: text => text[0].text
+        }
+      }
+    });
+
+    expect(screen.getByText('Project Management')).not.toBeNull();
+    expect(container).toMatchSnapshot();
+  });
+});
