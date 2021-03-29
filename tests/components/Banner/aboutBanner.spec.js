1f792fbca4ab10e2928f7b8c528061e08171fe22
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-29 12:09:09 +0600

    MAR-392 - moved banners to single directory. Tests about

---
+++ tests/components/Banner/aboutBanner.spec.js
@@ -0,0 +1,19 @@
+import AboutBanner from '@/components/Banner/AboutBanner';
+import {render, screen} from '@testing-library/vue';
+
+describe('Form checkboxes', () => {
+  global.$nuxt = {
+    $route: {
+      name: '/'
+    }
+  };
+
+  test('is a Vue instance', () => {
+    const {container} = render(AboutBanner, {
+      stubs: ['nuxt-link']
+    });
+
+    expect(screen.getByText('Mad Devs')).not.toBeNull();
+    expect(container).toMatchSnapshot();
+  });
+});
