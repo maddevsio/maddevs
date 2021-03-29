c4d1941977ee35d3f29b9146217876d427b23799
kotyunin <kotyunin.k@gmail.com>
2021-03-29 23:57:11 +0600

    MAR-76 Removed the black line between sections

--- client/components/About/CustomerRates.vue
+++ client/components/About/CustomerRates.vue
@@ -92,6 +92,9 @@ export default {
 
 .customer-rates {
   background-color: $bgcolor--white-darken;
+  // margins and paddings for removing the dark line between sections, that appears when Chome browser is scaled > 100%
+  margin: -1px 0 -1px;
+  padding: 2px 0;
 
   &__main-title-wrapper {
     display: flex;
