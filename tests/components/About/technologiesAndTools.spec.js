2f6623b081a9283966eb0164cc08017f255a2fb2
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-29 10:55:33 +0600

    MAR-391 - technologies and tools

--- tests/components/About/technologiesAndTools.spec.js
+++ tests/components/About/technologiesAndTools.spec.js
@@ -11,7 +11,12 @@ describe('Technologies and tools', () => {
   });
 
   test('should correctly detect click handler', async () => {
+    const {html} = render(TechnologiesAndTools);
+
     const btn = screen.getAllByTestId('test-tech_legend__item');
     await fireEvent.click(btn[0]);
+    expect(html()).toContain('<div class="tech_legend tech_legend-devops">');
+    await fireEvent.click(btn[0]);
+    expect(html()).not.toContain('<div class="tech_legend tech_legend-devops">');
   });
 });
