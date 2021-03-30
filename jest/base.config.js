0c6e75809c32c040dfc4b399c50addb59e930215
Anton Grushkin <merynes345@gmail.com>
2021-03-30 21:59:31 +0600

    MAR-375 added new package and update jest config

--- jest/base.config.js
+++ jest/base.config.js
@@ -14,7 +14,7 @@ module.exports = {
     // process js with `babel-jest`
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     // process `*.vue` files with `vue-jest`
-    '^[^.]+.vue$': '<rootDir>/node_modules/vue-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
     // process `*.svg` files with `imgTransform.js`
     '\\.(svg|png|webp|jpg)$': '<rootDir>/jest/imgTransformer.js',
   },
