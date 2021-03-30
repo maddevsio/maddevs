7f6688d4148fec9f5c17cea6765ca2a64943738f
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-03-30 12:13:49 +0600

    MAR-393 - updated snapshot

---
+++ tests/components/Blog/slices/GallerySlice.spec.js
@@ -0,0 +1,29 @@
+import GallerySlice from '@/components/Blog/slices/GallerySlice'
+import { render } from '@testing-library/vue'
+
+describe('galary slice component', () => {
+  const props = {
+    slice: {
+      items: [
+        {
+          gallery_image: {
+            Mobile: '',
+          },
+        },
+      ],
+      primary: {},
+    },
+  }
+  it('is a Vue instance', () => {
+    const { container } = render(GallerySlice, {
+      props,
+      mocks: {
+        $prismic: {
+          asText: name => name,
+        },
+      },
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+})
