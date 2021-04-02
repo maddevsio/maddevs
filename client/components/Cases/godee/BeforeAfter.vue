14777789beb0e22d4786a413c8791278ed5e53c1
Roman Chasovitin <chasovitin.r@gmail.com>
2021-04-02 13:17:13 +0600

    Fixed bugs with backgrounds in godee case. Also fixed BeforeAfter images urls.

--- client/components/Cases/godee/BeforeAfter.vue
+++ client/components/Cases/godee/BeforeAfter.vue
@@ -8,8 +8,8 @@
         <UIBeforeAfterImage
           :base-width="'1000'"
           :base-height="'578.47'"
-          :before-image="'Cases/godee/png/application-before.png'"
-          :after-image="'Cases/godee/png/application-after.png'"
+          before-image="/images/Cases/godee/png/application-before.png"
+          after-image="/images/Cases/godee/png/application-after.png"
           :alt="'GoDee Public Transportation App at 2018 and now.'"
         />
       </div>
