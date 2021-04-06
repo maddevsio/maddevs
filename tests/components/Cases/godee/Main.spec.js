5ce65c976516371488892eeee05301ae1c362aae
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-04-06 12:30:26 +0600

    MAR-395 - Main component

---
+++ tests/components/Cases/godee/Main.spec.js
@@ -0,0 +1,23 @@
+import Main from '@/components/Cases/godee/Main.vue'
+import { render } from '@testing-library/vue'
+import '../../../__mocks__/intersectionObserverMock'
+
+const containerToRender = document.createElement('div')
+containerToRender.setAttribute('id', 'case-scroll-container')
+containerToRender.setAttribute('data-testid', 'test-container')
+
+const mocks = {
+  getMediaFromS3: img => img,
+}
+
+describe('Main component', () => {
+  it('should render correctly', () => {
+    const { container } = render(Main, {
+      mocks,
+      stubs: ['client-only', 'nuxt-link', 'NuxtLink'],
+      container: document.body.appendChild(containerToRender),
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+})
