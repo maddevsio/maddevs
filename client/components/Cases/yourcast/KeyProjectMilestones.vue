1527b6720b1a3d04d05e3f6c27c96522e60f0142
Roman Chasovitin <chasovitin.r@gmail.com>
2021-04-07 15:20:24 +0600

    Merge new-develop into MAR-657.

--- client/components/Cases/yourcast/KeyProjectMilestones.vue
+++ client/components/Cases/yourcast/KeyProjectMilestones.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="container_regular">
-    <h2 class="case_title_h2 m-24_bottom media-m-12_bottom">
+    <h3 class="case_title_h2 m-24_bottom media-m-12_bottom">
       Key project milestones
-    </h2>
+    </h3>
     <div class="m-96_bottom media-m-48_bottom case_key-project-milestones">
       <CardProjectMilestone
         v-for="projectMilestone in keyProjectMilestones"
--- client/components/Cases/yourcast/Technologies.vue
+++ client/components/Cases/yourcast/Technologies.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="container_regular">
-    <h2 class="case_title_h2 m-24_bottom">
+    <h3 class="case_title_h2 m-24_bottom">
       Technology stack
-    </h2>
+    </h3>
     <ListTechnologies class="m-96_bottom media-m-48_bottom">
       <ListTechnologiesItem
         v-for="technologie in technologies"
