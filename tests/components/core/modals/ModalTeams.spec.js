d4a021cbdf716e74d18ee35ba548004698a76b73
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-04-09 11:18:02 +0600

    MAR-398 - modal teams test

---
+++ tests/components/core/modals/ModalTeams.spec.js
@@ -0,0 +1,31 @@
+import { render, fireEvent, screen } from '@testing-library/vue'
+import ModalTeams from '@/components/core/modals/ModalTeams'
+import formBaseProps from '../../../__mocks__/formBaseProps'
+
+const mocks = formBaseProps
+
+describe('ModalTeams component', () => {
+  it('should render correctly', () => {
+    const { container } = render(ModalTeams, {
+      mocks,
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should correct call handler', async () => {
+    render(ModalTeams, {
+      mocks,
+    })
+
+    await fireEvent.update(screen.getByTestId('test-base-form-phone'), '+3 4546-657-9098-7875645342245')
+    await fireEvent.update(screen.getByTestId('test-base-form-email'), 'someemail@maddevs.io')
+    await fireEvent.update(screen.getByTestId('test-base-form-full-name'), 'John Johnson')
+
+    await fireEvent.click(screen.queryByTestId('test-privacy-policy-checkbox-input'))
+    await fireEvent.click(screen.getByText(/Get a team of ultra fast coders/))
+
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    expect(mocks.$v.$reset).toHaveBeenCalledTimes(1)
+  })
+})
