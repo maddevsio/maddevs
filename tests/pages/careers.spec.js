9ad0fcf0e5451746b5a027832f51a90fc762b1b9
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-04-13 15:48:26 +0600

    MAR-403 - careers test metadata

--- tests/pages/careers.spec.js
+++ tests/pages/careers.spec.js
@@ -1,5 +1,15 @@
 import { render } from '@testing-library/vue'
 import MainCareers from '@/pages/careers.vue'
+import { shallowMount } from '@vue/test-utils'
+
+const META_DATA = {
+  description: 'Join our team of 100+ staff members working on unique projects, master new IT skills, and build a fantastic career path in software engineering, DevOps, or mobile app development.',
+  'og:url': 'https://maddevs.io/careers/',
+  'og:type': 'website',
+  'og:title': 'Mad Devs Careers: Work with Us',
+  'og:description': 'Join our team of 100+ staff members working on unique projects, master new IT skills, and build a fantastic career path in software engineering, DevOps, or mobile app development.',
+  'og:image': 'https://maddevs.io/Open-Graph.png',
+}
 
 const mocks = {
   $v: {
@@ -33,5 +43,18 @@ describe('CustomerRates', () => {
     })
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('should correct work head method', () => {
+    const wrapper = shallowMount(MainCareers, {
+      mocks,
+    })
+
+    const actual = wrapper.vm.$options.head.call(wrapper.vm)
+
+    expect(actual.meta).toHaveLength(Object.keys(META_DATA).length)
+    actual.meta.forEach(meta => {
+      expect(META_DATA[meta.name] || META_DATA[meta.property]).toBe(meta.content)
+    })
   })
 })
