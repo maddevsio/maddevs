7c6a8a3ae08c6c430421c57b6e43771d77b17705
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-04-14 22:55:15 +0600

    MAR-620 - added test for all posts

--- tests/components/Blog/Main/AllPostsSection.spec.js
+++ tests/components/Blog/Main/AllPostsSection.spec.js
@@ -1,14 +1,26 @@
 import { render } from '@testing-library/vue'
 import AllPostsSection from '@/components/Blog/Main/AllPostsSection'
+import Vuex from 'vuex'
+import { createLocalVue } from '@vue/test-utils'
 
-const stubs = [
-  'vuex',
-]
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+const recipeMock = jest.fn()
+recipeMock.mockReturnValue('homePageContent')
 
 describe('AllPostsSection component', () => {
   it('is a Vue instance', () => {
     const { container } = render(AllPostsSection, {
-      stubs,
+      localVue,
+      store: {
+        getters: {
+          filteredPosts: () => [{}],
+          allPosts: () => [],
+          postsCategory: jest.fn(),
+          postsPage: jest.fn(),
+        },
+      },
     })
 
     expect(container).toMatchSnapshot()
