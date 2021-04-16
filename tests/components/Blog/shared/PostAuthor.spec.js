6afec73bfe232c7a46365aec653d0f4e4a191a93
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-04-16 18:02:33 +0600

    MAR-729 - post auth tests

--- tests/components/Blog/shared/PostAuthor.spec.js
+++ tests/components/Blog/shared/PostAuthor.spec.js
@@ -1,25 +1,54 @@
 import { render } from '@testing-library/vue'
 import PostAuthor from '@/components/Blog/shared/PostAuthor'
+import { shallowMount } from '@vue/test-utils'
+
+const mocks = {
+  $prismic: {
+    asText: text => text,
+  },
+}
+
+const props = {
+  document: {
+    author: 'some-author',
+    author_image: {
+      url: 'some-url',
+    },
+    author_title: 'author_title',
+  },
+}
 
 describe('text slice component', () => {
-  it('is a Vue instance', () => {
+  it('should render correctly', () => {
     const { container } = render(PostAuthor, {
-      props: {
-        document: {
-          author: 'some-author',
-          author_image: {
-            url: 'some-url',
-          },
-          author_title: 'author_title',
-        },
-      },
-      mocks: {
-        $prismic: {
-          asText: text => text,
-        },
-      },
+      props,
+      mocks,
     })
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('should correct work watcher', () => {
+    const remove = jest.fn()
+    const add = jest.fn()
+    const callObject = {
+      $refs: {
+        authorImage: {
+          classList: {
+            remove,
+            add,
+          },
+        },
+      },
+    }
+    const wrapper = shallowMount(PostAuthor, {
+      propsData: props,
+      mocks,
+    })
+
+    wrapper.vm.$options.watch.document.call(callObject)
+
+    expect(remove).toHaveBeenCalledTimes(1)
+    expect(add).toHaveBeenCalledTimes(1)
   })
 })
