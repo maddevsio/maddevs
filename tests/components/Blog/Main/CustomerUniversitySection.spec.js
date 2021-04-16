f3e750ea258bee9b2b245174a445680766fe8bde
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-04-16 13:04:51 +0600

    MAR-729 - added test for custom university to 100

--- tests/components/Blog/Main/CustomerUniversitySection.spec.js
+++ tests/components/Blog/Main/CustomerUniversitySection.spec.js
@@ -64,8 +64,45 @@ describe('customer University section component', () => {
     expect(nextTick).toHaveBeenCalledTimes(1)
   })
 
-  it('correctly work api method', () => {
+  it('correctly work api method if haven\'t featured_cu', () => {
     const getSingleFunction = jest.fn()
+    const getByUIDFunction = jest.fn()
+
+    const callObject = {
+      $prismic: {
+        api: {
+          getSingle: () => {
+            getSingleFunction()
+            return {
+              data: {
+                featured_cu: {
+                },
+              },
+            }
+          },
+          getByUID: () => {
+            getByUIDFunction()
+            return {
+              data: {
+                date: '2021-01-21',
+              },
+            }
+          },
+        },
+      },
+    }
+    const wrapper = shallowMount(CustomerUniversitySection, {
+      mocks,
+    })
+
+    wrapper.vm.$options.fetch.call(callObject)
+    expect(getSingleFunction).toHaveBeenCalledTimes(1)
+    expect(getByUIDFunction).toHaveBeenCalledTimes(0)
+  })
+
+  it('correctly work api method', async () => {
+    const getSingleFunction = jest.fn()
+    const getByUIDFunction = jest.fn()
 
     const callObject = {
       $prismic: {
@@ -80,11 +117,14 @@ describe('customer University section component', () => {
               },
             }
           },
-          getByUID: () => ({
-            data: {
-              date: '2021-01-21',
-            },
-          }),
+          getByUID: () => {
+            getByUIDFunction()
+            return {
+              data: {
+                date: '2021-01-21',
+              },
+            }
+          },
         },
       },
     }
@@ -92,8 +132,9 @@ describe('customer University section component', () => {
       mocks,
     })
 
-    wrapper.vm.$options.fetch.call(callObject)
+    await wrapper.vm.$options.fetch.call(callObject)
     expect(getSingleFunction).toHaveBeenCalledTimes(1)
+    expect(getByUIDFunction).toHaveBeenCalledTimes(1)
   })
 
   it('should correct work computed firstParagraph with empty body', () => {
@@ -102,10 +143,37 @@ describe('customer University section component', () => {
     })
 
     const callParams = {
-      master: {
-      },
+      master: {},
     }
     const result = wrapper.vm.$options.computed.clusters.call(callParams)
     expect(result).toHaveLength(0)
+  })
+
+  it('should correct work computed clusters if show all param false', () => {
+    mocks.showAll = true
+    const wrapper = shallowMount(CustomerUniversitySection, {
+      mocks,
+    })
+
+    const callParams = {
+      showAll: false,
+      clusters: ['1', '2', '3', '4', '5', '6'],
+    }
+    const result = wrapper.vm.$options.computed.clustersToShow.call(callParams)
+    expect(result).toHaveLength(3)
+  })
+
+  it('should correct work computed clusters if show all param true', () => {
+    mocks.showAll = true
+    const wrapper = shallowMount(CustomerUniversitySection, {
+      mocks,
+    })
+
+    const callParams = {
+      showAll: true,
+      clusters: ['1', '2', '3', '4', '5', '6'],
+    }
+    const result = wrapper.vm.$options.computed.clustersToShow.call(callParams)
+    expect(result).toHaveLength(callParams.clusters.length)
   })
 })
