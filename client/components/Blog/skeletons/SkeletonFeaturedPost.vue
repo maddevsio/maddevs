397a4162c004d02f5e79c33fee9e7265b06be7db
kotyunin <kotyunin.k@gmail.com>
2021-04-19 11:15:36 +0600

    MAR-137 Refactored the FeaturedPostSkeleton component

--- client/components/Blog/skeletons/SkeletonFeaturedPost.vue
+++ client/components/Blog/skeletons/SkeletonFeaturedPost.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    :class="theme"
     class="featured-post"
+    :class="theme"
   >
     <div class="row featured-post__wrapper">
       <div class="col-12 col-lg-6 featured-post__main">
         <h1
-          :class="theme"
           class="featured-post__title background-grey animated"
+          :class="theme"
         />
         <p
-          :class="theme"
           class="featured-post__paragraph background-grey animated"
+          :class="theme"
         />
         <div class="featured-post__data d-flex justify-content-between">
           <div class="featured-post__meta">
@@ -21,8 +21,8 @@
       </div>
       <div class="col-12 col-lg-6 featured-post__image-wrapper">
         <div
-          :class="theme"
           class="featured-post__image background-grey animated"
+          :class="theme"
         />
       </div>
     </div>
@@ -43,7 +43,6 @@ export default {
 
 <style scoped lang="scss">
 @import '../../../assets/styles/_vars';
-
 @keyframes SkeletonLoading {
   0% {
     background-position: -600px 0;
@@ -52,11 +51,9 @@ export default {
     background-position: calc(600px + 100%) 0;
   }
 }
-
 .animated {
   animation: 1.5s ease-in-out 1s infinite normal none running SkeletonLoading;
 }
-
 .background-grey {
   &.dark {
     background-color: $bgcolor--black-light;
@@ -67,87 +64,70 @@ export default {
       $bgcolor--black-lighten
     );
   }
-
   &.light {
     background-color: $bgcolor--silver;
     background-image: linear-gradient(90deg, $bgcolor--grey-extra-dark, $bgcolor--silver, $bgcolor--grey-extra-dark);
   }
 }
-
 .featured-post {
   text-decoration: none;
   display: block;
-
   &.dark {
     color: $text-color--white-primary;
   }
-
   &.light {
     color: $text-color--black;
   }
-
   &__wrapper {
     align-items: center;
   }
-
   &__title {
     margin-bottom: 15px;
     height: 67px;
     border-radius: 7px;
   }
-
   &__paragraph {
     margin-bottom: 44px;
     height: 56px;
     border-radius: 7px;
   }
-
   &__image-wrapper {
     text-align: right;
     margin-top: 0;
   }
-
   &__image {
     width: 540px;
     height: 293px;
     display: inline-block;
   }
 }
-
 @media only screen and (max-width: 991px) {
   .featured-post {
     &__main {
       order: 2;
     }
-
     &__paragraph {
       margin-bottom: 38px;
     }
-
     &__data {
       display: block;
-
       /deep/ .blog-post__author {
         margin-bottom: 16px;
       }
     }
-
     &__meta {
       justify-content: space-between;
-
       /deep/ .post-tag {
         padding: 8px 16px;
         line-height: 22px;
         margin-left: 0;
       }
     }
-
     &__image-wrapper {
       order: 1;
       margin-bottom: 16px;
       text-align: center;
     }
-
     &__image {
       width: 100%;
       height: 42vw;
