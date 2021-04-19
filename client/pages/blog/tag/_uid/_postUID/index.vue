b16423e20658470b480cd9831580982f7f722c4e
kotyunin <kotyunin.k@gmail.com>
2021-04-19 13:26:39 +0600

    MAR-137 Removed unnecessary code

--- client/pages/blog/tag/_uid/_postUID/index.vue
+++ client/pages/blog/tag/_uid/_postUID/index.vue
@@ -16,20 +16,5 @@ export default {
   },
 
   mixins: [buildPostPageMixin('tag', 'post')],
-
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      const { post } = vm
-      const { params } = to
-      /**
-       * Prismic saves all previous UID and they both still resolve
-       * This condition checks the current uid and redirects to it
-       * https://community.prismic.io/t/when-does-cache-expire-uid-history/874 - about this issue
-       */
-      if (params.postUID !== post.uid || post.uid === 'string') {
-        next({ path: `/blog/tag/${params.uid}/${post.uid}/` })
-      }
-    })
-  },
 }
 </script>
