1149b95f132c459726a74439431d3ba031dd2f3b
kotyunin <kotyunin.k@gmail.com>
2021-04-19 07:57:44 +0600

    MAR-137 Created the blogPost vuex store

--- client/store/index.js
+++ client/store/index.js
@@ -1,9 +1,11 @@
 import blog from './modules/blog'
+import blogPost from './modules/blogPost'
 import leads from './modules/leads'
 import emails from './modules/emails'
 
 export const modules = {
   blog,
+  blogPost,
   leads,
   emails,
 }
