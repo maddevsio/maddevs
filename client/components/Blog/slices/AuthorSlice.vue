b59246dad151a1152f7dcc27b298a12483f58249
kotyunin <kotyunin.k@gmail.com>
2021-04-20 13:47:37 +0600

    MAR-559 Corrected layout for AuthorSlice

--- client/components/Blog/slices/AuthorSlice.vue
+++ client/components/Blog/slices/AuthorSlice.vue
@@ -201,10 +201,13 @@ export default {
       &:first-of-type {
         margin-left: -2px;
       }
+      a,
       img {
         display: block;
         width: 100%;
         height: 100%;
+      }
+      img {
         object-fit: cover;
       }
     }
