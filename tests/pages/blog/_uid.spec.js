84c0a626de60e6b0e38be50baf5c33b7dc353a31
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-04-22 17:20:31 +0600

    MAR-622 - blog page

---
+++ tests/pages/blog/_uid.spec.js
@@ -0,0 +1,80 @@
+import { render } from '@testing-library/vue'
+import Post from '@/pages/blog/_uid.vue'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuelidate from 'vuelidate'
+import blogPost from '../../__mocks__/blogPost'
+
+const DESCRIPTION = 'test description'
+
+const store = {
+  getters: {
+    homePageContent: () => ({ description: DESCRIPTION }),
+  },
+}
+
+const mocks = {
+  $prismic: {
+    asText: () => 'text',
+    asHtml: html => `<p>${html}</p>`,
+  },
+  post: blogPost,
+  $store: {
+    dispatch: jest.fn(),
+  },
+}
+
+const localVue = createLocalVue()
+
+localVue.use(Vuelidate)
+
+const stubs = ['ShareNetwork', 'PostView']
+
+describe('Post _uid component', () => {
+  it('should render correctly', () => {
+    const { container } = render(Post, {
+      store,
+      mocks,
+      stubs,
+      localVue,
+    })
+
+    expect(container).toMatchSnapshot()
+    expect(mocks.$store.dispatch).toHaveBeenCalledWith('getBlogAuthors')
+  })
+
+  it('should correctly work beforeRouteEnter method if from and to is empty', () => {
+    const callObject = {
+      next: jest.fn(),
+    }
+    const wrapper = shallowMount(Post, {
+      store,
+      mocks,
+      stubs,
+      localVue,
+    })
+
+    wrapper.vm.$options.beforeRouteEnter.call({}, null, null, callObject.next)
+    expect(callObject.next).toHaveBeenCalledTimes(1)
+  })
+
+  it('should correctly work beforeRouteEnter method if from and to correct', () => {
+    const callObject = {
+      from: {},
+      to: {
+        params: {
+          uid: null,
+        },
+      },
+      next: jest.fn(() => 'test'),
+    }
+    const wrapper = shallowMount(Post, {
+      store,
+      mocks,
+      stubs,
+      localVue,
+    })
+
+    wrapper.vm.$options.beforeRouteEnter.call({}, null, null, callObject.next)
+    expect(callObject.next).toHaveBeenCalledTimes(1)
+  })
+})
