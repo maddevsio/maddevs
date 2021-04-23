499b832c0dd7926302a6f84c8b33dbd0c49f90f3
Denisoed <denisoed@gmail.com>
2021-04-23 16:07:41 +0600

    MAR-825 added log for SEO analyzer

--- DOMAnalyzer.js
+++ DOMAnalyzer.js
@@ -3,6 +3,33 @@ const path = require('path')
 const SeoInspector = require('seo-inspector')
 const cliProgress = require('cli-progress')
 
+async function asyncSome(arr, predicate) {
+  for (const e of arr) {
+    /* eslint-disable no-await-in-loop */
+    if (await predicate(e)) return true
+  }
+  return false
+}
+
+async function log(report) {
+  const successMessage = 'No any SEO defect found.'
+  const green = '\x1b[32m'
+  const red = '\x1b[31m'
+  const yellow = '\x1b[33m%s\x1b[0m'
+  const withErrors = await asyncSome(report, async i => i.errors.trim() !== successMessage)
+
+  if (!withErrors) return console.log(`\n %s${successMessage}`, green)
+
+  for (const item of report) {
+    if (item.errors.trim() !== successMessage) {
+      console.log(yellow, `File: ${item.file.trim()}`)
+      console.log(`%s${item.errors}`, red)
+    }
+  }
+
+  return process.exit(1) // Stop process in termonal
+}
+
 function analyzeFile(pathFile) {
   return new Promise(resolve => new SeoInspector({
     done: (err, data) => {
@@ -31,19 +58,21 @@ function getFilesFromDist(base, ext, listFiles, result) {
   return res
 }
 
-async function analyzeAllFiles(array) {
+async function analyzeFiles(array) {
   const report = []
   const consoleProgressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
   consoleProgressBar.start(array.length, 0)
 
   for (const file of array) {
     consoleProgressBar.increment()
+    const errors = await analyzeFile(file)
     /* eslint-disable no-await-in-loop */
-    report.push({ data: await analyzeFile(file), file })
+    report.push({ file, errors })
   }
 
   consoleProgressBar.stop()
+  log(report)
 }
 
 const listFiles = getFilesFromDist('dist', 'html')
-analyzeAllFiles(listFiles)
+analyzeFiles(listFiles)
