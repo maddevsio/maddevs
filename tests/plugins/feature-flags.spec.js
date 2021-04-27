bee7eae056220758de6be0da2c1f9624563b5526
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-04-27 18:32:51 +0600

    MAR-804 - serializer tests

--- tests/plugins/feature-flags.spec.js
+++ tests/plugins/feature-flags.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 import flag from '@/plugins/feature-flags'
 
-describe('fAQ', () => {
+describe('feature flag function', () => {
   it('renders correctly', () => {
     const wrapper = shallowMount(flag)
     expect(wrapper.element).toMatchSnapshot()
