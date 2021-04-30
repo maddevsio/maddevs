3a9a1797c487ee5f01f6fbd805cfa38beb235bf9
Denisoed <denisoed@gmail.com>
2021-04-30 10:30:28 +0600

    MAR-907 added start page for Case Studies

---
+++ client/pages/case-studies/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="main"></main>
+</template>
+
+<script>
+import { getMetadata, buildHead } from '@/data/seo'
+import initLazyLoadMixin from '@/mixins/initLazyLoadMixin'
+
+export default {
+  name: 'MainCaseStudies',
+  components: {},
+
+  mixins: [initLazyLoadMixin],
+
+  head() {
+    return buildHead(getMetadata('caseStudies'))
+  },
+}
+</script>
