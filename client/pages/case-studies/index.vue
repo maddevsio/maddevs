fcf6b82c8cb871e46e9b244a5bb7f0f66effda4f
Denisoed <denisoed@gmail.com>
2021-04-30 14:15:45 +0600

    MAR-907 added section with Customers

--- client/pages/case-studies/index.vue
+++ client/pages/case-studies/index.vue
@@ -2,18 +2,24 @@
   <main class="main">
     <TitleDesc />
     <CasesList />
+    <Customers light />
   </main>
 </template>
 
 <script>
 import TitleDesc from '@/components/Cases/TitleDesc'
 import CasesList from '@/components/Cases/CasesList'
+import Customers from '@/components/Projects/Customers'
 import { getMetadata, buildHead } from '@/data/seo'
 import initLazyLoadMixin from '@/mixins/initLazyLoadMixin'
 
 export default {
   name: 'MainCaseStudies',
-  components: { TitleDesc, CasesList },
+  components: {
+    TitleDesc,
+    CasesList,
+    Customers,
+  },
 
   mixins: [initLazyLoadMixin],
 
