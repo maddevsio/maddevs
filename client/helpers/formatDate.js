be56af91a1a39fd04f76bb8172ea33b115fb56aa
AntonGrushkin <merynes345@gmail.com>
2021-04-30 14:33:52 +0600

    MAR-875 added new test for module formateDate

--- client/helpers/formatDate.js
+++ client/helpers/formatDate.js
@@ -1,7 +1,12 @@
 const options = { year: 'numeric', month: 'short', day: '2-digit' }
 
 function formatDate(raw) {
+  if (typeof raw !== 'string') return 'Invalid Date'
+
   const date = new Date(raw)
+
+  if (date === 'Invalid Date') return 'Invalid Date'
+
   return date.toLocaleString('en-US', options)
 }
 
--- tests/helpers/formatDate.spec.js
+++ tests/helpers/formatDate.spec.js
@@ -4,10 +4,34 @@ const newDateFormat = 'Apr 28, 2021'
 
 describe('formatDate helper', () => {
   it('function should return new date format', () => {
-    expect(formatDate('2021-04-28')).toEqual(newDateFormat)
+    expect(formatDate('2021-4-28')).toEqual(newDateFormat)
   })
 
   it('function should return invalid date if called without params', () => {
     expect(formatDate()).toEqual('Invalid Date')
+  })
+
+  it('function should return invalid date if pass null in params', () => {
+    expect(formatDate(null)).toEqual('Invalid Date')
+  })
+
+  it('function should return invalid date if pass undefined in params', () => {
+    expect(formatDate(undefined)).toEqual('Invalid Date')
+  })
+
+  it('function should return invalid date if pass empty string in params', () => {
+    expect(formatDate('')).toEqual('Invalid Date')
+  })
+
+  it('function should return invalid date if pass number in params', () => {
+    expect(formatDate(1)).toEqual('Invalid Date')
+  })
+
+  it('function should return invalid date if pass object in params', () => {
+    expect(formatDate({})).toEqual('Invalid Date')
+  })
+
+  it('function should return invalid date if pass array in params', () => {
+    expect(formatDate([])).toEqual('Invalid Date')
   })
 })
