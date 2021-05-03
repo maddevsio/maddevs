56107d8b4d8b3091c07b74b746dd48e9935f5395
Roman Chasovitin <chasovitin.r@gmail.com>
2021-05-03 14:04:43 +0600

    Replaced token.

--- radiator/telegram/index.js
+++ radiator/telegram/index.js
@@ -1,10 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api')
 const buildMessage = require('./buildMessage')
 
-const TOKEN = '1660809307:AAESS_Mf_u2jsQxnxg65jbb-R9QHT3T8GPw'
-
 async function main(data) {
-  const bot = new TelegramBot(TOKEN, { polling: true })
+  const bot = new TelegramBot(process.env.RADIATOR_TELEGRAM_TOKEN, { polling: true })
   const message = buildMessage(data)
 
   bot.sendMessage(data.telegramChannelId, message, { parse_mode: 'markdown' })
