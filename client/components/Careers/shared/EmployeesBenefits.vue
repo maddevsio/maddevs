7626192ea6272b29048667a42b6e72d2fa073539
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-10 10:22:31 +0600

    MAR-441 Created the EmployeesBenefits grid component

---
+++ client/components/Careers/shared/EmployeesBenefits.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="employees-benefits">
+    <div
+      v-for="benefit in benefits"
+      :key="benefit.icon"
+      class="employees-benefits__item"
+    >
+      <img
+        :src="require(`@/assets/img/EmployeesBenefits/svg/${benefit.icon}.svg`)"
+        class="employees-benefits__item-icon"
+      >
+      <p class="employees-benefits__item-title">
+        {{ benefit.title }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { employeesBenefits as benefits } from '@/data/employeesBenefits'
+
+export default {
+  name: 'EmployeesBenefits',
+  data() {
+    return {
+      benefits,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scroped>
+@import '../../../assets/styles/vars';
+
+.employees-benefits {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 8px 10px;
+  &__item {
+    padding: 28px 16px 32px 27px;
+    background-color: $bgcolor--silver;
+    border-radius: 4px;
+    &-icon {
+      display: block;
+    }
+    &-title {
+      margin-top: 20px;
+      font-size: 17px;
+      line-height: 140%;
+      letter-spacing: -0.013em;
+      color: $text-color--black-oil;
+    }
+  }
+}
+</style>
---
+++ client/data/employeesBenefits.js
@@ -0,0 +1,34 @@
+export const employeesBenefits = [
+  {
+    icon: 'hours',
+    title: 'Flexible working hours',
+  },
+  {
+    icon: 'trips',
+    title: 'Onsite business trips',
+  },
+  {
+    icon: 'lessons',
+    title: 'English lessons',
+  },
+  {
+    icon: 'training',
+    title: 'Training budget',
+  },
+  {
+    icon: 'culture',
+    title: 'Remote-first culture',
+  },
+  {
+    icon: 'term',
+    title: 'Long-term projects',
+  },
+  {
+    icon: 'conferences',
+    title: 'Paid conferences',
+  },
+  {
+    icon: 'parties',
+    title: 'Team parties',
+  },
+]
