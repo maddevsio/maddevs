2031c536f40a0bd02f6b5bf5791b2c7172ebd373
Maksim Gachevskiy <hektodaleky@gmail.com>
2021-05-10 16:15:11 +0600

    MAR-213 - removed comments

--- server/routes/web.js
+++ server/routes/web.js
@@ -2,12 +2,10 @@ const express = require('express')
 
 // controllers
 const { switchLanguage } = require('../controllers/LanguageController')
-// const { getSitemap } = require('../controllers/SitemapController')
 
 const router = express.Router()
 
 router.get('/ru', switchLanguage)
 router.get('/en', switchLanguage)
-// router.get('/sitemap.xml', getSitemap)
 
 module.exports = router
