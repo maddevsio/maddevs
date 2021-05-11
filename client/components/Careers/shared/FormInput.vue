c39329c2b4e48fd9c6cb12368c95b85cf21e8aa5
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-11 10:08:10 +0600

    MAR-441 Created the FormInput component

---
+++ client/components/Careers/shared/FormInput.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="career-form-input">
+    <span>{{ value || placeholder }}</span>
+    <input
+      :type="type"
+      :autofocus="autofocus"
+      :placeholder="placeholder"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+
+    autofocus: {
+      type: Boolean,
+      default: false,
+    },
+
+    value: {
+      type: String,
+      default: '',
+    },
+
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/styles/vars';
+
+.career-form-input {
+  position: relative;
+  display: inline-flex;
+  span,
+  input {
+    font-family: inherit;
+    font-size: inherit;
+    line-height: inherit;
+    letter-spacing: inherit;
+  }
+  span {
+    display: block;
+    opacity: 0;
+    visibility: hidden;
+    pointer-events: none;
+  }
+  input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 0px solid;
+    padding: 0;
+    background: transparent;
+    color: $text-color--red;
+    caret-color: $text-color--red;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    &::placeholder {
+      color: $text-color--grey;
+    }
+  }
+}
+</style>
