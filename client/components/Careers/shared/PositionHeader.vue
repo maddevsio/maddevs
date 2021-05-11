a9a6a5fb1312e9dd87536468d79d34a672938374
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-11 19:16:46 +0600

    MAR-441 Created the uid vacancy page

---
+++ client/components/Careers/shared/PositionHeader.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="careers-position__header">
+    <div class="container">
+      <div class="careers-position__container">
+        <h1 class="careers-position__title">
+          Data Analyst / Senior Data Analyst - Core Services
+        </h1>
+        <p class="careers-position__subtitle">
+          We're building a culture at MadDevs where amazing people can do their best work.
+          If you're ready to grow your career and help millions of organizations grow better,
+          you've come to the right place.
+        </p>
+        <div class="careers-position__tags">
+          <PositionTags />
+        </div>
+        <UIButton class="careers-position__apply-button">
+          Apply now
+        </UIButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PositionTags from './PositionTags'
+import UIButton from '../../shared/UIButton'
+
+export default {
+  name: 'PositionHeader',
+  components: {
+    PositionTags,
+    UIButton,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../../assets/styles/vars';
+
+.careers-position {
+  &__header {
+    padding: 95px 0;
+    background-color: $bgcolor--black;
+    color: $text-color--white-primary;
+  }
+  &__labels {
+    margin-bottom: 32px;
+  }
+  &__title {
+    font-size: 68px;
+    line-height: 109%;
+    letter-spacing: -3.6px;
+  }
+  &__subtitle {
+    margin-top: 24.5px;
+    font-size: 17px;
+    line-height: 141%;
+    letter-spacing: -0.013em;
+  }
+  &__tags {
+    margin-top: 23px;
+  }
+  &__apply-button {
+    font-family: Inter, sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 166%;
+    letter-spacing: -0.1px;
+    width: 190px;
+    height: 48px;
+    margin-top: 47.5px;
+    border-radius: 6px;
+    &::after {
+      content: '↓';
+      display: inline-block;
+      margin-left: 8px;
+    }
+  }
+}
+</style>
---
+++ client/pages/careers/_uid.vue
@@ -0,0 +1,64 @@
+<template>
+  <section
+    id="careers-position"
+    class="careers-position"
+  >
+    <PositionHeader />
+    <div class="careers-position__body">
+      <div class="container">
+        <div class="careers-position__container">
+          <EmployeesBenefits />
+          <div class="careers-position__content-divider" />
+          <CareersAuthor />
+          <div class="careers-position__form">
+            <CareersForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import PositionHeader from '@/components/Careers/shared/PositionHeader'
+import EmployeesBenefits from '@/components/Careers/shared/EmployeesBenefits'
+import CareersAuthor from '@/components/Careers/shared/CareersAuthor'
+import CareersForm from '@/components/Careers/shared/CareersForm'
+
+export default {
+  name: 'CareersPosition',
+  components: {
+    PositionHeader,
+    EmployeesBenefits,
+    CareersAuthor,
+    CareersForm,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/styles/vars';
+
+.careers-position {
+  padding-top: 62px;
+  font-family: 'Inter', sans-serif;
+  /deep/ &__container {
+    max-width: 924px;
+  }
+  &__body {
+    padding: 60px 0 79px;
+    background-color: $bgcolor--white-primary;
+    color: $text-color--black-oil;
+  }
+  &__content-divider {
+    display: block;
+    height: 1px;
+    margin-top: 60px;
+    margin-bottom: 79px;
+    background-color: $border-color--grey-20-percent;
+  }
+  &__form {
+    margin-top: 40px;
+  }
+}
+</style>
