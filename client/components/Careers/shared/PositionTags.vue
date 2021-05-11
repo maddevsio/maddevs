f57effa14a18e2fc9234ff2eaf33fb1303f74a16
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-11 19:14:15 +0600

    MAR-441 Created the position tags component

---
+++ client/components/Careers/shared/PositionTags.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="position-tags">
+    <span class="position-tags__item">HR</span>
+    <span class="position-tags__item">Marketing</span>
+    <span class="position-tags__item">DevOps</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PositionTags',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/styles/vars';
+
+.position-tags {
+  display: flex;
+  align-items: center;
+  &__item {
+    font-size: 15px;
+    line-height: 166%;
+    letter-spacing: -0.009em;
+    display: flex;
+    align-items: center;
+    color: $text-color--grey-opacity-40-percent;
+    &::before {
+      content: "";
+      display: block;
+      width: 5px;
+      height: 5px;
+      border-radius: 50%;
+      background: $text-color--grey-opacity-40-percent;
+      margin: 2px 8.5px 0;
+    }
+    &:first-child::before {
+      display: none;
+    }
+  }
+}
+</style>
