5a86796e1b204e1a204560f7c8a8339abcb8a86b
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-12 14:22:03 +0600

    MAR-441 Added the data for uid page

--- client/pages/careers/_uid.vue
+++ client/pages/careers/_uid.vue
@@ -7,11 +7,17 @@
     <div class="careers-position__body">
       <div class="container">
         <div class="careers-position__container">
-          <EmployeesBenefits />
+          <div class="careers-position__content">
+            <SlicesBlock
+              v-if="slices.length"
+              :slices="slices"
+            />
+            <EmployeesBenefits />
+          </div>
           <div class="careers-position__content-divider" />
-          <CareersAuthor />
+          <HRContact />
           <div class="careers-position__form">
-            <CareersForm />
+            <PositionForm />
           </div>
         </div>
       </div>
@@ -21,17 +27,72 @@
 
 <script>
 import PositionHeader from '@/components/Careers/shared/PositionHeader'
+import SlicesBlock from '@/components/slices'
 import EmployeesBenefits from '@/components/Careers/shared/EmployeesBenefits'
-import CareersAuthor from '@/components/Careers/shared/CareersAuthor'
-import CareersForm from '@/components/Careers/shared/CareersForm'
+import HRContact from '@/components/Careers/shared/HRContact'
+import PositionForm from '@/components/Careers/shared/PositionForm'
+
+import extractSchemaOrg from '@/helpers/extractSchemaOrg'
+import formatDate from '@/helpers/formatDate'
+import { buildHead } from '@/data/seo'
 
 export default {
   name: 'CareersPosition',
   components: {
     PositionHeader,
+    SlicesBlock,
     EmployeesBenefits,
-    CareersAuthor,
-    CareersForm,
+    HRContact,
+    PositionForm,
+  },
+
+  async asyncData({ store, params, error }) {
+    let schemaOrgSnippet = ''
+    const openGraphUrl = `${process.env.domain}/careers/${params.uid}/`
+
+    try {
+      const post = await store.dispatch('getVacancyPost', { type: 'vacancy', uid: params.uid })
+
+      // Schema org snippet
+      schemaOrgSnippet = extractSchemaOrg(post.data.schema_org_snippets)
+
+      return {
+        type: post.type,
+        id: post.id,
+        uid: post.uid,
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        slices: post.data.body,
+        tags: post.tags,
+        date: formatDate(post.data.date),
+        metaTitle: post.data.meta_title || post.data.title,
+        metaDescription: post.data.meta_description,
+        openGraphUrl,
+        schemaOrgSnippet,
+      }
+    } catch (e) {
+      // Returns error page
+      return error({ statusCode: 404, message: 'Page not found' })
+    }
+  },
+
+  data() {
+    return {
+      slices: [],
+      openGraphUrl: '',
+      schemaOrgSnippet: '',
+    }
+  },
+
+  head() {
+    return buildHead({
+      title: this.metaTitle || '',
+      metaTitle: this.metaTitle || '',
+      description: this.metaDescription || '',
+      image: '/favicon.ico',
+      url: this.openGraphUrl,
+      jsonLd: this.schemaOrgSnippet,
+    })
   },
 }
 </script>
@@ -41,7 +102,6 @@ export default {
 
 .careers-position {
   padding-top: 62px;
-  font-family: 'Inter', sans-serif;
   /deep/ &__container {
     max-width: 924px;
   }
