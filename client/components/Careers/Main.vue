8afd880a58118b36d9c888c5499140e56b0391ed
AntonGrushkin <merynes345@gmail.com>
2021-05-13 23:33:14 +0600

    MAR-1018 added markup and styles for new section

--- client/components/Careers/Main.vue
+++ client/components/Careers/Main.vue
@@ -2,18 +2,21 @@
   <div class="careers">
     <Banner />
     <ProjectsCountries />
+    <Benefits />
   </div>
 </template>
 
 <script>
 import Banner from '@/components/Careers/Banner'
 import ProjectsCountries from '@/components/Careers/ProjectsCountries'
+import Benefits from '@/components/Careers/Benefits'
 
 export default {
   name: 'Main',
   components: {
     Banner,
     ProjectsCountries,
+    Benefits,
   },
 
 }
