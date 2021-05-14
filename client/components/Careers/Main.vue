a185f675d40097466aceb78464147e9d4722951f
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-14 11:45:23 +0600

    MAR-958 Imprted the OpenPositions section to Main

--- client/components/Careers/Main.vue
+++ client/components/Careers/Main.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="careers">
-    <Banner />
-    <ProjectsCountries />
+    <div class="careers__content--dark-theme">
+      <Banner />
+      <ProjectsCountries />
+    </div>
+    <div class="careers__content--white-theme">
+      <OpenPositions />
+    </div>
   </div>
 </template>
 
 <script>
 import Banner from '@/components/Careers/Banner'
 import ProjectsCountries from '@/components/Careers/ProjectsCountries'
+import OpenPositions from '@/components/Careers/OpenPositions'
 
 export default {
   name: 'Main',
   components: {
     Banner,
     ProjectsCountries,
+    OpenPositions,
   },
 
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/_vars.scss';
+
+.careers__content {
+  &--white-theme {
+    background-color: $bgcolor--white-primary;
+  }
+}
+</style>
