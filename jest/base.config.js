a6598298e63633c5b5a7b5b92fc3bcc5d1223820
Denisoed <denisoed@gmail.com>
2021-05-14 10:09:56 +0600

    MAR-557 updated jest

--- jest/base.config.js
+++ jest/base.config.js
@@ -33,8 +33,8 @@ module.exports = {
   coverageThreshold: {
     global: {
       branches: 75,
-      statements: 90.95,
-      functions: 86.68,
+      statements: 90.71,
+      functions: 86.61,
       lines: 91,
     },
   },
