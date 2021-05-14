f9f1ea99639f31af7e773a5a95ac78564cc42c5f
Roman Chasovitin <chasovitin.r@gmail.com>
2021-05-14 18:33:37 +0600

    MAR-1064. Fixed problems with careers forms.

--- server/controllers/CareersController.js
+++ server/controllers/CareersController.js
@@ -1,17 +1,13 @@
 const { sendEmail } = require('../services/EmailsService')
-const { createLead } = require('../services/LeadsService')
 const { validate } = require('../utils/validation')
 
-async function create(req, res) {
+function index(req, res) {
   const { isValid, error } = validate(req)
   if (!isValid) return res.status(error.status).json(error)
-
-  sendEmail(req)
-  const response = await createLead(req)
-
-  return res.json(response)
+  sendEmail(req, data => res.json(data))
+  return null
 }
 
 module.exports = {
-  create,
+  index,
 }
