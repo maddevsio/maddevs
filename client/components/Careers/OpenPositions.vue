f37a3389db44d51024d795f568a6658b923b8397
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-14 17:09:04 +0600

    MAR-958 Created the PositionsGrid component

---
+++ client/components/Careers/OpenPositions.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="open-positions">
+    <div class="container">
+      <div class="open-positions__head">
+        <h2 class="open-positions__title">
+          Open Positions
+        </h2>
+      </div>
+      <PositionsGrid />
+    </div>
+  </div>
+</template>
+
+<script>
+import PositionsGrid from '@/components/Careers/shared/PositionsGrid'
+
+export default {
+  name: 'OpenPositions',
+  components: {
+    PositionsGrid,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/_vars';
+
+.open-positions {
+  padding: 82px 0 88px;
+  &__head {
+    text-align: center;
+    margin-bottom: 46px;
+  }
+  &__title {
+    @include font('Popins', 60px, 700);
+    line-height: 64px;
+    letter-spacing: -2.6px;
+  }
+}
+</style>
---
+++ client/components/Careers/shared/PositionsGrid.vue
@@ -0,0 +1,123 @@
+<template>
+  <ul class="positions-grid">
+    <li
+      v-for="item in items"
+      :key="item"
+      class="positions-grid__item"
+    >
+      <NuxtLink
+        to="/"
+        class="positions-grid__item-link"
+      >
+        <div class="positions-grid__item-labels">
+          <PositionLabels :labels="{remote: true, relocation: true}" />
+        </div>
+        <h3 class="positions-grid__item-title">
+          Position title
+        </h3>
+        <hr class="positions-grid__item-divider">
+        <p class="positions-grid__item-subtitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis fugiat est, modi veritatis in iste porro cum dolore harum doloremque.
+        </p>
+        <div class="positions-grid__item-tags">
+          <PositionTags :tags="['HR', 'DevOps']" />
+        </div>
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script>
+import PositionLabels from '@/components/Careers/shared/PositionLabels'
+import PositionTags from '@/components/Careers/shared/PositionTags'
+
+export default {
+  name: 'PositionsGrid',
+  components: {
+    PositionLabels,
+    PositionTags,
+  },
+
+  data() {
+    return {
+      items: [1, 2, 3, 4, 6, 7, 8, 10, 11],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/_vars';
+
+.positions-grid {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin-top: 0.5px;
+  margin-left: 0.5px;
+  &__item {
+    display: block;
+    margin-left: -1px;
+    margin-top: -1px;
+    border: 1px solid $border-color--grey-20-percent;
+    & {
+      transition: all 0.4s;
+    }
+    & * {
+      transition: all 0.2s;
+    }
+    &:first-child {
+      border-top-left-radius: 8px;
+    }
+    &:nth-child(3) {
+      border-top-right-radius: 8px;
+    }
+    &:nth-last-child(3) {
+      border-bottom-left-radius: 8px;
+    }
+    &:last-child {
+      border-bottom-right-radius: 8px;
+    }
+    &:hover {
+      background-color: $bgcolor--black-oil;
+      border-color: $bgcolor--black-oil;
+      .positions-grid__item {
+        &-link {
+          color: $text-color--white-primary;
+        }
+        &-subtitle {
+          color: $text-color--silver;
+        }
+      }
+    }
+    &-link {
+      display: block;
+      padding: 46px;
+      padding-bottom: 66.5px;
+      color: $text-color--black-oil;
+    }
+    &-labels {
+      margin-bottom: 20px;
+    }
+    &-title {
+      @include font('Poppins', 32px, 700);
+      letter-spacing: -0.03em;
+      line-height: 35px;
+    }
+    &-divider {
+      width: 75px;
+      height: 1px;
+      margin: 20px 0;
+      border: none;
+      background: $border-color--red;
+    }
+    &-subtitle {
+      line-height: 22px;
+      letter-spacing: -0.013em;
+    }
+    &-tags {
+      margin-top: 23.5px;
+    }
+  }
+}
+</style>
