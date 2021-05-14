2bf586dbf9c373906c0b02c3c4f000eb722bccd0
AntonGrushkin <merynes345@gmail.com>
2021-05-14 13:52:11 +0600

    MAR-1018 added lazy mixin for root careers component

--- client/pages/careers/index.vue
+++ client/pages/careers/index.vue
@@ -7,12 +7,15 @@
 <script>
 import Main from '@/components/Careers/Main'
 import { getMetadata, buildHead } from '@/data/seo'
+import initLazyLoadMixin from '@/mixins/initLazyLoadMixin'
 
 export default {
   name: 'MainCareers',
   components: {
     Main,
   },
+
+  mixins: [initLazyLoadMixin],
 
   head() {
     return buildHead(getMetadata('careers'))
