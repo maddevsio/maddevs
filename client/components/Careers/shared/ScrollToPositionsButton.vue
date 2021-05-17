d10d8f5494285c1a2e93d52c4fda4a64b5fe8b18
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-18 00:20:15 +0600

    MAR-1083 Improved logic of the "ScrollToPositionsButton" component

--- client/components/Careers/shared/ScrollToPositionsButton.vue
+++ client/components/Careers/shared/ScrollToPositionsButton.vue
@@ -1,14 +1,15 @@
 <template>
   <NuxtLink
     to="#open-positions"
-    :class="[hidden ? 'scroll-to-positions--hidden' : null]"
+    :class="[(show && vacanciesLoaded && vacanciesCount) ? null : 'scroll-to-positions--hidden']"
     class="scroll-to-positions"
   >
-    <UnderlinedButton>Scroll to open positions</UnderlinedButton>
+    <UnderlinedButton>See all {{ vacanciesCount }} open positions</UnderlinedButton>
   </NuxtLink>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import UnderlinedButton from '@/components/Careers/shared/UnderlinedButton'
 
 export default {
@@ -19,9 +20,17 @@ export default {
 
   data() {
     return {
-      hidden: false,
+      show: true,
       positionsSectionEl: null,
     }
+  },
+
+  computed: {
+    ...mapGetters(['vacancies', 'vacanciesLoaded']),
+
+    vacanciesCount() {
+      return (this.vacancies && this.vacancies.length) || 0
+    },
   },
 
   mounted() {
@@ -35,7 +44,7 @@ export default {
       if (this.positionsSectionEl) {
         const sectionRect = this.positionsSectionEl.getBoundingClientRect()
         const offsetTop = sectionRect.top - window.innerHeight
-        this.hidden = offsetTop < 0
+        this.show = offsetTop > 0
       }
     },
   },
