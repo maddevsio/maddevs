556e47b9e09116647030bd5064c4a32e8363b96b
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-19 11:38:41 +0600

    MAR-1085 Corrected the scrollToElement mixin

--- client/mixins/scrollToElementMixin.js
+++ client/mixins/scrollToElementMixin.js
@@ -2,8 +2,11 @@ const scrollToElementMixin = {
   methods: {
     scrollToElement(selector) {
       const targetEl = document.querySelector(selector)
-      if ('scrollBehavior' in document.documentElement.style) return window.scrollTo({ top: targetEl.offsetTop, behavior: 'smooth' })
-      return window.scrollTo(0, targetEl.offsetTop)
+      if (targetEl) {
+        if ('scrollBehavior' in document.documentElement.style) return window.scrollTo({ top: targetEl.offsetTop, behavior: 'smooth' })
+        return window.scrollTo(0, targetEl.offsetTop)
+      }
+      return null
     },
   },
 }
