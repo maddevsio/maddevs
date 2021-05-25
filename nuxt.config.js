e5afe4ef25047bab991d00818ba271fe50a2eef1
AntonGrushkin <merynes345@gmail.com>
2021-05-25 13:25:15 +0600

    Added ignore config for veeqo case

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,7 +65,7 @@ module.exports = {
   /*
    ** Component will be ignored in building
    */
-  // ignore: 'pages/case-studies/veeqo.vue',
+  ignore: 'pages/case-studies/veeqo.vue',
   generate: {
     ...getRoutes(),
     fallback: '404.html',
