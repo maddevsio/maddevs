f2349468ab3824394f2143624a9f3e8eca6692fe
Denisoed <denisoed@gmail.com>
2021-05-28 12:17:09 +0600

    MAR-1092 added status 301 for redirect method

--- server/middlewares/redirectToTrailingSlash.js
+++ server/middlewares/redirectToTrailingSlash.js
@@ -1,5 +1,8 @@
 function redirectToTrailingSlash(req, res, next) {
-  if (req.url[req.url.length - 1] !== '/' && req.method === 'GET' && req.url !== '/sitemap.xml') res.redirect(`https://${req.headers.host}${req.url}/`)
+  if (req.url[req.url.length - 1] !== '/'
+    && req.method === 'GET'
+    && req.url !== '/sitemap.xml'
+  ) res.redirect(301, `https://${req.headers.host}${req.url}/`)
   else next()
 }
 
