a14ffb129ba57e6715eb430327ecf2c57acbd4a6
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-31 14:09:12 +0600

    MAR-1100 Changed the CareersController

--- server/controllers/CareersController.js
+++ server/controllers/CareersController.js
@@ -2,11 +2,26 @@ const { sendEmail } = require('../services/EmailsService')
 const { sendApplication } = require('../services/HuntflowService')
 const { validate } = require('../utils/validation')
 
+const parseRequest = req => ({
+  ...req,
+  body: {
+    ...JSON.parse(req.body.payload),
+  },
+})
+
+const buildRequest = (req, key) => ({
+  ...req,
+  body: req.body[key],
+})
+
 async function index(req, res) {
-  const { isValid, error } = validate(req)
+  const parsedReqest = parseRequest(req)
+
+  const { isValid, error } = validate(parsedReqest)
   if (!isValid) return res.status(error.status).json(error)
-  const huntflowRes = await sendApplication(req)
-  const emailRes = await sendEmail(req, data => res.json(data))
+
+  const huntflowRes = await sendApplication(buildRequest('huntflow'))
+  const emailRes = await sendEmail(buildRequest('email'), data => res.json(data))
   return res.json({
     huntflowRes,
     emailRes,
