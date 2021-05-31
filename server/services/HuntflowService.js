89b3937116516ee9489fd739301e275ffcc9fd1e
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-31 12:46:49 +0600

    MAr-1100 Added logic to HuntflowService

--- server/services/HuntflowService.js
+++ server/services/HuntflowService.js
@@ -1,22 +1,78 @@
 const axios = require('axios')
+const fs = require('fs')
 const FormData = require('form-data')
 const { HUNTFLOW_API_URL, HUNTFLOW_TOKEN, HUNTFLOW_ACCOUNT_ID } = require('../config')
 
+const buildApplicant = (cvFileId, {
+  firstName, middleName, lastName, email, linkedinProfile, positionTitle, positionValue,
+}) => ({
+  first_name: firstName,
+  middle_name: middleName,
+  last_name: lastName,
+  position: `${positionTitle}. You can also consider me for your other: ${positionValue} roles.`,
+  email,
+  externals: [
+    {
+      data: {
+        body: `To get more information on my skills, please check out my linkedin profile: ${linkedinProfile}`,
+      },
+      auth_type: 'NATIVE',
+      files: [
+        {
+          id: cvFileId,
+        },
+      ],
+    },
+  ],
+})
+
+const buildVacancyRequest = (vacancyId, cvFileId) => ({
+  vacancy: vacancyId,
+  status: 75897, // "Новые"
+  comment: 'Заявка на вакансию отправлена с сайта https://maddevs.io/',
+  files: [
+    {
+      id: cvFileId,
+    },
+  ],
+})
+
 async function sendApplication(req) {
   try {
-    const data = new FormData()
-    data.append('file', req.file.buffer)
-    const response = await axios.post(`${HUNTFLOW_API_URL}/account/${HUNTFLOW_ACCOUNT_ID}/upload`, data, {
+    const payload = (JSON.parse(req.body.payload)).huntflow
+    const formData = new FormData()
+    formData.append('file', fs.createReadStream(req.file.path))
+
+    const uploadResponse = await axios.post(`${HUNTFLOW_API_URL}/account/${HUNTFLOW_ACCOUNT_ID}/upload`, formData, {
       headers: {
         Authorization: `Bearer ${HUNTFLOW_TOKEN}`,
         'Content-Type': 'multipart/form-data',
-        ...data.getHeaders(),
+        ...formData.getHeaders(),
       },
     })
-    console.log(response.data, '>>>>> success')
-    return response.data
+
+    const applicant = buildApplicant(uploadResponse.data.id, payload)
+
+    const applicantResponse = await axios.post(`${HUNTFLOW_API_URL}/account/${HUNTFLOW_ACCOUNT_ID}/applicants`, applicant, {
+      headers: {
+        Authorization: `Bearer ${HUNTFLOW_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const vacancyRequest = buildVacancyRequest(payload.vacancyId, uploadResponse.data.id)
+
+    const vacancyResponse = await axios.post(`${HUNTFLOW_API_URL}/account/${HUNTFLOW_ACCOUNT_ID}/applicants/${applicantResponse.data.id}/vacancy`,
+      vacancyRequest,
+      {
+        headers: {
+          Authorization: `Bearer ${HUNTFLOW_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+      })
+
+    return vacancyResponse.data
   } catch (error) {
-    console.log(error.response.data, '>>>>> error')
     return error.response.data
   }
 }
