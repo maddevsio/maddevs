f09ee434e0db13f956da8268dff8afa32589a2f2
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-05-31 19:08:32 +0600

    MAR-1100 Added validation for the careers controller

--- server/controllers/CareersController.js
+++ server/controllers/CareersController.js
@@ -1,5 +1,6 @@
 const { sendEmail } = require('../services/EmailsService')
 const { sendApplication } = require('../services/HuntflowService')
+const { validate } = require('../utils/validation')
 
 const parseRequest = req => ({
   ...req,
@@ -18,8 +19,16 @@ async function index(req, res) {
   const huntflowReq = buildRequest(parsedReq, 'huntflow')
   const emailReq = buildRequest(parsedReq, 'email')
 
+  const huntflowValidation = validate(huntflowReq, 'huntflow')
+  if (!huntflowValidation.isValid) return res.status(huntflowValidation.error.status).json(huntflowValidation.error)
+
+  const emailValidation = validate(emailReq, 'email')
+  if (!emailValidation.isValid) return res.status(emailValidation.error.status).json(emailValidation.error)
+
   const huntflowRes = await sendApplication(huntflowReq)
-  await sendEmail(emailReq, data => res.json({ email: data, huntflow: huntflowRes }))
+  const emailRes = await sendEmail(emailReq, data => res.json({ email: data, huntflow: huntflowRes }))
+
+  return emailRes
 }
 
 module.exports = {
