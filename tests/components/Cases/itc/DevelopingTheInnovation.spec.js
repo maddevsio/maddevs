fbbefe24bef87ed5a5b68c918fb6fb4e09cbba9f
AntonGrushkin <merynes345@gmail.com>
2021-06-03 19:11:10 +0600

    MAR-1164 update snap tests

--- tests/components/Cases/itc/DevelopingTheInnovation.spec.js
+++ tests/components/Cases/itc/DevelopingTheInnovation.spec.js
@@ -1,9 +1,13 @@
 import DevelopingTheInnovation from '@/components/Cases/itc/DevelopingTheInnovation'
 import { render } from '@testing-library/vue'
 
+const stubs = ['Lottie']
+
 describe('DevelopingTheInnovation component', () => {
   it('should render correctly', () => {
-    const { container } = render(DevelopingTheInnovation)
+    const { container } = render(DevelopingTheInnovation, {
+      stubs,
+    })
 
     expect(container).toMatchSnapshot()
   })
