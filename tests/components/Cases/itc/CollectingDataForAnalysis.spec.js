8852434c51850954212101c6fd5f03cfe2db7c80
AntonGrushkin <merynes345@gmail.com>
2021-06-07 17:05:53 +0600

    Update tests

--- tests/components/Cases/itc/CollectingDataForAnalysis.spec.js
+++ tests/components/Cases/itc/CollectingDataForAnalysis.spec.js
@@ -1,9 +1,15 @@
 import CollectingDataForAnalysis from '@/components/Cases/itc/CollectingDataForAnalysis'
 import { render } from '@testing-library/vue'
 
+const mocks = {
+  $getMediaFromS3: () => 'img.jpg',
+}
+
 describe('CollectingDataForAnalysis component', () => {
   it('should render correctly', () => {
-    const { container } = render(CollectingDataForAnalysis)
+    const { container } = render(CollectingDataForAnalysis, {
+      mocks,
+    })
 
     expect(container).toMatchSnapshot()
   })
