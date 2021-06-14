d1d5caf6b57b47401307d8c5cdd4eb5027fbb63b
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-06-14 07:28:57 +0600

    MAR-429 Created the Main component

---
+++ client/components/CooperationModels/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="cooperation-models">
+    <Banner />
+    <ModelsGrid />
+    <GetPresentation />
+    <div class="cooperation-models--white-sections">
+      <CustomerTestimonials />
+      <CTABanner />
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/CooperationModels/Banner'
+import ModelsGrid from '@/components/CooperationModels/ModelsGrid'
+import GetPresentation from '@/components/CooperationModels/GetPresentation'
+import CustomerTestimonials from '@/components/CooperationModels/CustomerTestimonials'
+import CTABanner from '@/components/CooperationModels/CTABanner'
+
+export default {
+  name: 'Main',
+  components: {
+    Banner,
+    ModelsGrid,
+    GetPresentation,
+    CustomerTestimonials,
+    CTABanner,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/_vars';
+
+.cooperation-models {
+  &--white-sections {
+    background-color: $bgcolor--white-primary;
+  }
+}
+</style>
