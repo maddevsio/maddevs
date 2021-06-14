0dd3c8422d51a7729d78c58b2acc3e21bc5f41d4
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-06-14 09:26:03 +0600

    MAR-429 Created the rounded button ui component

---
+++ client/components/CooperationModels/shared/RoundedButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="rounded-button">
+    →
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/_vars';
+
+.rounded-button {
+  display: inline-block;
+  cursor: pointer;
+  background: transparent;
+  padding: 0;
+  @include font('Inter', 16, 400);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  border: 1px solid $border-color--white;
+  color: $border-color--white;
+  transition: all .2s;
+  &:hover {
+    background-color: $border-color--white;
+    color: $text-color--black;
+  }
+}
+</style>
