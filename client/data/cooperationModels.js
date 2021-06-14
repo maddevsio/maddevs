17dc5f2019b5aae5f085bc826484e937012e87cb
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-06-14 13:12:37 +0600

    MAR-429 Created file with data for cooperation model items

---
+++ client/data/cooperationModels.js
@@ -0,0 +1,226 @@
+export const cooperationModels = {
+  'augmented-staff-members': {
+    title: 'Reach time-to-market targets and release deadlines faster with augmented staff members.',
+    subtitle: 'Best suited for customers with an existing team that has well-established development processes and knowledge management.',
+    optimalModelFor: [
+      'Organizations that need to add specialists to facilitate reaching time-to-market targets.',
+      'Companies that need to determine the best fit for the team and identify uncertainty in the scope of work.',
+      'Companies that cope with urgent tasks to meet release deadlines.',
+      'Organizations that want to avoid inefficient in-house hiring processes.',
+    ],
+    process: [
+      {
+        step: 'Define specialists’ competencies',
+        content: [
+          'Together with project stakeholders, we define the required competencies for specialists (hard and soft skills, expertise level) by assessing the product’s technological stack and collecting further information about the scope of work (tasks to be performed, product features).',
+        ],
+      },
+      {
+        step: 'Define specialists’ competencies',
+        content: [
+          'We propose a team composition with a cost-effective combination of seniority levels. Our approach to team headcount is based on our 15+ years of experience in custom software development.',
+          'Each selected candidate is available for additional interviews to ensure that the competence requirements are met.',
+          'In cases of absence of exact criteria, our senior developers or principals can assess the current team performance and suggest optimization options.',
+        ],
+      },
+      {
+        step: 'Approval',
+        content: [
+          'When the candidates are approved, they adopt the customer’s corporate processes and fully integrate as valued team members.',
+          'Our internal monitoring processes continuously collect feedback on augmented staff performance and develop action plans for changes if needed.',
+        ],
+      },
+      {
+        step: 'Extras',
+        content: [
+          'Augmented staff motivation, training, and additional vacation and sick leave expenses are the sole responsibility of Mad Devs.',
+        ],
+      },
+    ],
+  },
+  'dedicated-project-based-team': {
+    title: 'Hire a dedicated project-based team to meet all IT-related needs and bring your product idea to life.',
+    subtitle: '',
+    optimalModelFor: [
+      'Businesses that need to raise funds and quickly move to the next stage.',
+      'Companies that plan to reorganize their internal IT departments to optimize costs and performance.',
+      'Existing businesses that need teams with specific expertise to develop new products.',
+      'Companies that don’t have expertise in hiring and supervising big IT teams.',
+    ],
+    process: [
+      {
+        step: 'Product requirements',
+        content: [
+          'Together with project stakeholders, our SWOT team prepares a product requirements document (PRD).',
+          'We analyse the PRD and propose solutions and a team composition to deliver the project or one of its modules.',
+        ],
+      },
+      {
+        step: 'Planning',
+        content: [
+          'Jointly, we build a product roadmap and continuously adjust the vision and priorities during the project.',
+          'We ensure compliance with all organizational processes and value-driven development.',
+        ],
+      },
+      {
+        step: 'Monitoring and evaluation',
+        content: [
+          'Development teams don’t hide behind project managers, and we welcome customers to directly communicate with each team member.',
+          'The entire development team communicates directly with the customer to effectively align with business goals and get actively involved in the product itself.',
+          'Our managers internally monitor the whole team’s performance and ensure that it delivers the expected results.',
+          'We provide detailed weekly and monthly reports and arrange demo calls on the completed work.',
+          'To improve collaboration, our team can travel to your organization.',
+        ],
+      },
+    ],
+  },
+  'temp-to-hire': {
+    title: 'Temp to hire allows businesses to safely expand the hiring geography and diversity with minimal onboarding and recruiting risks.',
+    subtitle: '',
+    optimalModelFor: [
+      'Businesses whose offshore development is hindered by legal restrictions and corporate rules.',
+      'Companies that face long and difficult employee recruitment and onboarding processes due to governmental and corporate regulations.',
+      'Organizations that want to safely expand the hiring geography and diversify their corporate culture.',
+    ],
+    process: [
+      {
+        step: 'Define specialists’ competencies',
+        content: [
+          'Our SWOT team collects the specialist requirements (hard and soft skills, expertise level) and information about the project (tasks to be performed, product features).',
+        ],
+      },
+      {
+        step: 'Approval and onboarding of specialists',
+        content: [
+          'After an internal check, candidates have an additional interview with the customer.',
+          'We onboard approved specialists to fit in the team. They work under the existing organizational processes and the customer’s direct supervision. At this stage, we aim to make the specialist a part of your team.',
+          'Additionally, we offer consulting services for developing and integrating organizational and development processes.',
+        ],
+      },
+      {
+        step: 'Monitoring and improvement',
+        content: [
+          'The specialist’s performance is monitored internally through regular sync-up meetings with our tech lead.',
+          'Arrangements can be changed and added based on the customer’s and end users’ feedback.',
+          'We help specialists adapt to your corporate culture and adopt high standards of professional conduct.',
+          'Transparency is achieved by openly stating the specialist’s salary and our management fee.',
+          '',
+        ],
+      },
+      {
+        step: 'Transition',
+        content: [
+          'The customer can formally hire the specialist after 6 to 12 months if they fit in the team.',
+        ],
+      },
+      {
+        step: 'Extras',
+        content: [
+          'No flat rate. We fully transfer the specialist to your team.',
+        ],
+      },
+    ],
+  },
+  'technical-assessment-and-consulting': {
+    title: 'Technical assessment & consulting equip business with unbias expert assessment to solve technical issues and scale your business.',
+    subtitle: '',
+    optimalModelFor: [
+      'Organizations looking for an unbiased expert assessment.',
+      'Businesses that need to effectively scale up their products.',
+      'Businesses in search of new product development directions.',
+      'Companies dealing with overwhelming tech issues and technical debt.',
+      'Organizations that have had several failed MVP launches.',
+      'Business owners that doubt their current teams’ competencies.',
+      'Companies facing disappointing performance and ruined relationships with the current IT contractor or in-house team.',
+    ],
+    process: [
+      {
+        step: 'Define problems',
+        content: [
+          'We start the assessment with several problem interviews with all project stakeholders to better understand the issues and suggest workarounds.',
+        ],
+      },
+      {
+        step: 'Conduct analysis and work plan',
+        content: [
+          'Based on the issues identified and the customer’s requests, we decide whether our assessment should cover the complete product or only its critical components.',
+          'Our complex assessment involves analysing bottlenecks in the product stationing, UI/UX, architecture, and code, infrastructure, and organizational processes, including communication and knowledge management.',
+          'We onboard a team of top-notch specialists with 10+ years of experience in the IT industry.',
+        ],
+      },
+      {
+        step: 'Offer recommendations',
+        content: [
+          'At the final stage, the customer receives a 360-degree report on the current project state and recommendations for improvements.',
+        ],
+      },
+      {
+        step: 'Extras',
+        content: [
+          'To ensure implementation of our recommendations and superior execution, we offer additional supervision services.',
+        ],
+      },
+    ],
+  },
+  'effective-team-supervision': {
+    title: 'Boost technical team performance with effective team supervision.',
+    subtitle: '',
+    optimalModelFor: [
+      'Managers who don’t have a technical background.',
+      'Organizations whose current teams’ competencies don’t grow in proportion to their business needs.',
+      'Businesses facing technical challenges due to rapid growth.',
+      'Managers experiencing frustration due to a lack of transparency in teams’ development processes and communication.',
+      'Businesses whose lack of infrastructure planning and management incurs expenses.',
+    ],
+    process: [
+      {
+        step: 'Define problems',
+        content: [
+          'A team of top-notch specialists with 10+ years of experience in the IT industry reverse engineer the way development and organizational processes are coordinated and analyse team bottlenecks.',
+        ],
+      },
+      {
+        step: 'Conduct analysis and prepare an action plan',
+        content: [
+          'We devise and present an action plan to overcome the current issues and a detailed justification of the issue background.',
+          'The customer receives not only a solution but also detailed guidance and best practices advice to avoid a relapse.',
+        ],
+      },
+      {
+        step: 'Action plan execution',
+        content: [
+          'Our senior expert monitors the action plan implementation and makes necessary adjustments.',
+          'Regular meetings between our senior expert and the customer help us adjust the project plans and vision.',
+          'We provide monthly reports on completed work and maintain complete transparency in communication.',
+        ],
+      },
+    ],
+  },
+  'transferring-a-project': {
+    title: 'Protect intellectual property while transferring a project to another IT partner.',
+    subtitle: '',
+    optimalModelFor: [
+      'Companies facing disappointing performance and ruined relationships with the current IT contractor.',
+      'Companies whose end results bring no value to their owners or to end users.',
+      'Businesses looking to switch software development from in-house to an IT contractor model.',
+    ],
+    process: [
+      {
+        step: 'Step 1',
+        content: [
+          'A team of top-notch specialists with 10+ years of experience in the IT industry reverse engineer the way development and organizational processes are coordinated and analyse team bottlenecks.',
+          'We discuss the existing problems with the customer to be sure that they cannot be solved through supervision of the current team and that project transfer is the only solution.',
+        ],
+      },
+      {
+        step: 'Step 2',
+        content: [
+          'We make sure that the customer is the owner of the project’s intellectual property. We help restore the code in the repository and revise the available technical and business documentation.',
+          'Together with the customer, we calculate the project transfer costs, taking into account the estimated time of project takeover by the new team.',
+          'We elaborate the statement of work for the new team, i.e. the main objectives to achieve.',
+          'We can onboard our own team, or we can help you evaluate other candidates.',
+        ],
+      },
+    ],
+  },
+}
