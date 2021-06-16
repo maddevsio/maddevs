b06329512cd7f9c15033448e8b1fc8d1408ef985
Kirill Kotyunin <kotyunin.k@gmail.com>
2021-06-16 08:57:45 +0600

    MAR-429 Corrected the animations starting

--- client/data/cooperationModels.js
+++ client/data/cooperationModels.js
@@ -45,7 +45,7 @@ export const cooperationModels = {
   },
   'dedicated-project-based-team': {
     animationName: 'dedicated',
-    startAnimationOnScreenPercent: 70,
+    startAnimationOnScreenPercent: 82,
     title: 'Hire a <span>dedicated project-based team</span> to meet all IT-related needs and bring your product idea to life.',
     subtitle: '',
     optimalModelFor: [
@@ -86,7 +86,7 @@ export const cooperationModels = {
   },
   'temp-to-hire': {
     animationName: 'team',
-    startAnimationOnScreenPercent: 60,
+    startAnimationOnScreenPercent: 70,
     title: '<span>Temp to hire</span> allows businesses to safely expand the hiring geography and diversity with minimal onboarding and recruiting risks.',
     subtitle: '',
     optimalModelFor: [
@@ -138,7 +138,7 @@ export const cooperationModels = {
   },
   'technical-assessment-and-consulting': {
     animationName: 'technicalAssessment',
-    startAnimationOnScreenPercent: 60,
+    startAnimationOnScreenPercent: 70,
     title: '<span>Technical assessment & consulting</span> equip business with unbias expert assessment to solve technical issues and scale your business.',
     subtitle: '',
     optimalModelFor: [
@@ -223,7 +223,7 @@ export const cooperationModels = {
   },
   'transferring-a-project': {
     animationName: 'transfer',
-    startAnimationOnScreenPercent: 65,
+    startAnimationOnScreenPercent: 82,
     title: 'Protect intellectual property while <span>transferring a project</span> to another IT partner.',
     subtitle: 'The main aim of the cooperation is to prepare the customer’s project for transfer to another IT team.',
     optimalModelFor: [
