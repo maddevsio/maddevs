a87991f0f45ef3ec064f7411fc3dfad406771461
Kirill kotyunin <kotyunin.k@gmail.com>
2021-06-21 17:37:29 +0600

    MAR-1376 Added tests for TeamsForm component

---
+++ tests/components/core/forms/TeamsForm.spec.js
@@ -0,0 +1,33 @@
+import { render, fireEvent, screen } from '@testing-library/vue'
+import TeamsForm from '@/components/core/forms/TeamsForm'
+import formBaseProps from '../../../__mocks__/formBaseProps'
+
+import delay from '../../../../client/helpers/delay'
+
+const mocks = formBaseProps
+
+describe('TeamsForm component', () => {
+  it('should render correctly', () => {
+    const { container } = render(TeamsForm, {
+      mocks,
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should correct call handler', async () => {
+    render(TeamsForm, {
+      mocks,
+    })
+
+    await fireEvent.update(screen.getByTestId('test-base-form-phoneNumber'), '+3 4546-657-9098-7875645342245')
+    await fireEvent.update(screen.getByTestId('test-base-form-email'), 'someemail@maddevs.io')
+    await fireEvent.update(screen.getByTestId('test-base-form-fullName'), 'John Johnson')
+
+    await fireEvent.click(screen.queryByTestId('test-privacy-policy-checkbox-input'))
+    await fireEvent.click(screen.getByText(/Get a team of ultra fast coders/))
+
+    await delay(1000)
+    expect(mocks.$v.$reset).toHaveBeenCalledTimes(1)
+  })
+})
