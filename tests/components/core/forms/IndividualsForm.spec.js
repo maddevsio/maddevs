6633c6d1b2b003978f074bc58c16eedcec3d4718
Kirill kotyunin <kotyunin.k@gmail.com>
2021-06-21 17:29:00 +0600

    MAR-1376 Added tests for IndividualsForm component

---
+++ tests/components/core/forms/IndividualsForm.spec.js
@@ -0,0 +1,33 @@
+import { render, fireEvent, screen } from '@testing-library/vue'
+import IndividualsForm from '@/components/core/forms/IndividualsForm'
+import formBaseProps from '../../../__mocks__/formBaseProps'
+
+import delay from '../../../../client/helpers/delay'
+
+const mocks = formBaseProps
+
+describe('ModalIndividuals component', () => {
+  it('should render correctly', () => {
+    const { container } = render(IndividualsForm, {
+      mocks,
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should correct call handler', async () => {
+    render(IndividualsForm, {
+      mocks,
+    })
+
+    await fireEvent.update(screen.getByTestId('test-base-form-phoneNumber'), '+3 4546-657-9098-7875645342245')
+    await fireEvent.update(screen.getByTestId('test-base-form-email'), 'someemail@maddevs.io')
+    await fireEvent.update(screen.getByTestId('test-base-form-fullName'), 'John Johnson')
+
+    await fireEvent.click(screen.queryByTestId('test-privacy-policy-checkbox-input'))
+    await fireEvent.click(screen.getByText('Get individual proactive rockets'))
+
+    await delay(1000)
+    expect(mocks.$v.$reset).toHaveBeenCalledTimes(1)
+  })
+})
