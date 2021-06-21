703d6fa08a1edbae521ba8b040ccd6ef5a7f7e8b
Kirill kotyunin <kotyunin.k@gmail.com>
2021-06-21 17:33:59 +0600

    MAR-1376 Added tests for InfrastructureAuditForm component

---
+++ tests/components/core/forms/InfrastructureAuditForm.spec.js
@@ -0,0 +1,33 @@
+import { render, fireEvent, screen } from '@testing-library/vue'
+import InfrastructureAuditForm from '@/components/core/forms/InfrastructureAuditForm'
+import formBaseProps from '../../../__mocks__/formBaseProps'
+
+import delay from '../../../../client/helpers/delay'
+
+const mocks = formBaseProps
+
+describe('InfrastructureAuditForm component', () => {
+  it('should render correctly', () => {
+    const { container } = render(InfrastructureAuditForm, {
+      mocks,
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should correct call handler', async () => {
+    render(InfrastructureAuditForm, {
+      mocks,
+    })
+
+    await fireEvent.update(screen.getByTestId('test-base-form-phoneNumber'), '+3 4546-657-9098-7875645342245')
+    await fireEvent.update(screen.getByTestId('test-base-form-email'), 'someemail@maddevs.io')
+    await fireEvent.update(screen.getByTestId('test-base-form-fullName'), 'John Johnson')
+
+    await fireEvent.click(screen.queryByTestId('test-privacy-policy-checkbox-input'))
+    await fireEvent.click(screen.getByText(/Get an infrastructure audit/))
+
+    await delay(1000)
+    expect(mocks.$v.$reset).toHaveBeenCalledTimes(1)
+  })
+})
