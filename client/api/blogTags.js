8ffc2b2432761af4f78674384228d6b92275c386
Kirill kotyunin <kotyunin.k@gmail.com>
2021-06-29 09:31:17 +0600

    MAR-1265 Added the getBlogTags api method

--- client/api/blogTags.js
+++ client/api/blogTags.js
@@ -1,3 +1,31 @@
+import { getBlogPosts } from '@/api/blog'
+import convertStringToSlug from '@/helpers/convertStringToSlug'
+
+export const getBlogTags = async prismic => {
+  try {
+    const blogTags = []
+    const blogPosts = await getBlogPosts(prismic)
+    for (const post of blogPosts) {
+      for (const tag of post.tags) {
+        if (!blogTags.includes(tag)) blogTags.push(tag)
+      }
+    }
+
+    return blogTags
+  } catch (error) {
+    return error
+  }
+}
+
+export const getBlogTag = async (prismic, payload) => {
+  try {
+    const blogTags = await getBlogTags(prismic)
+    return blogTags.find(tag => convertStringToSlug(tag) === payload)
+  } catch (error) {
+    return error
+  }
+}
+
 export const getPostsByTag = async (prismic, payload) => {
   const prismicQuery = async (page = 1) => {
     let posts = []
