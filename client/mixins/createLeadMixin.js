f50122dce61321236009018c842f0b427d486c8a
Kirill kotyunin <kotyunin.k@gmail.com>
2021-07-06 16:08:04 +0600

    MAR-1298 Added the new variables to leadsMixin

--- client/mixins/createLeadMixin.js
+++ client/mixins/createLeadMixin.js
@@ -8,10 +8,16 @@ const createLeadMixin = (templateId, title = 'Individuals', subject = 'Marketing
     ...mapActions(['sendLead']),
 
     async submitLead(variables) {
-      let browserName = 'Unknown'
       if (!templateId) throw new Error('Template ID was not provided')
+
+      let userBrowser = 'Unknown'
+      let userOS = 'Unknown'
+      let userPlatform = 'Unknown'
       if (window && window.navigator && window.navigator.userAgent) {
-        browserName = bowser.getParser(window.navigator.userAgent).getBrowserName()
+        const { browser, os, platform } = bowser.parse(window.navigator.userAgent)
+        userBrowser = `name: ${browser.name}, version: ${browser.version}`
+        userOS = `name: ${os.name}, version: ${os.version}, versionName: ${os.versionName}`
+        userPlatform = `type: ${platform.type}, vendor: ${platform.vendor}`
       }
 
       const baseTitle = 'Mad Devs Website Forms'
@@ -20,11 +26,13 @@ const createLeadMixin = (templateId, title = 'Individuals', subject = 'Marketing
         variables: {
           emailTo: process.env.emailContact,
           subject,
-          browserName,
           modalTitle: title ? `${baseTitle} - ${title}` : baseTitle,
           projectDescriber: variables.description,
           agreeToGetMadDevsDiscountOffers: variables.agreeToGetMadDevsDiscountOffers ? 'Yes' : 'No',
           agreeWithPrivacyPolicy: variables.agreeWithPrivacyPolicy ? 'Yes' : 'No',
+          userBrowser,
+          userOS,
+          userPlatform,
           ...exceptKeys(variables, ['agreeToGetMadDevsDiscountOffers', 'agreeWithPrivacyPolicy', 'description']),
         },
       }
