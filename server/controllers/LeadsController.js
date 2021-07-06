7668250eb6cabfc91d6fb82e5c29ee535f7aa67a
Kirill kotyunin <kotyunin.k@gmail.com>
2021-07-06 13:14:28 +0600

    MAR-1298 Added the ipInfo variable

--- server/controllers/LeadsController.js
+++ server/controllers/LeadsController.js
@@ -1,10 +1,14 @@
 const { sendEmail } = require('../services/EmailsService')
 const { createLead } = require('../services/LeadsService')
+const { getIPInfo } = require('../services/IPService')
 const { validate } = require('../utils/validation')
 
 async function create(req, res) {
   const { isValid, error } = validate(req, 'email')
   if (!isValid) return res.status(error.status).json(error)
+
+  const ipInfo = await getIPInfo()
+  req.body.variables.ipInfo = ipInfo
 
   sendEmail(req)
   const response = await createLead(req)
