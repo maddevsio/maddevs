8edb81af2921586061af17c4526caa5326e5c453
Kirill kotyunin <kotyunin.k@gmail.com>
2021-07-07 12:26:48 +0600

    MAR-1298 Added the unit-test for IPService file

---
+++ server/services/tests/IPService.spec.js
@@ -0,0 +1,21 @@
+/* eslint-disable prefer-promise-reject-errors */
+import 'regenerator-runtime'
+import axios from 'axios'
+import { getIPInfo } from '../IPService'
+
+const response = { data: 'ip data' }
+jest.spyOn(axios, 'get').mockImplementation(() => Promise.resolve(response))
+
+describe('Huntflow service', () => {
+  it('should correctly return data from response', async () => {
+    const data = await getIPInfo()
+    expect(data).toBe('ip data')
+  })
+
+  it('should return the empty object if axios failed', async () => {
+    axios.get.mockImplementation(() => Promise.reject('error'))
+
+    const error = await getIPInfo()
+    expect(error).toEqual({})
+  })
+})
