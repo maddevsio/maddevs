761f5b814f69f730d358124aa41c00fc5f00013d
Kirill kotyunin <kotyunin.k@gmail.com>
2021-07-08 14:49:29 +0600

    MAR-1298 Minor change

--- client/store/modules/ipInfo.js
+++ client/store/modules/ipInfo.js
@@ -1,8 +1,8 @@
-import { getIPInfo } from '@/api/ipinfo'
+import { getIPInfo } from '@/api/ipInfo'
 
 export const actions = {
   async getIPInfo() {
     const response = await getIPInfo(this.$axios)
-    return response
+    return response.data
   },
 }
