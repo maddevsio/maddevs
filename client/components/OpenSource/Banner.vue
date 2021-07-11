9dc46d690c29ef2ab6a9145fad3670c69b464c89
Kirill kotyunin <kotyunin.k@gmail.com>
2021-07-11 11:56:09 +0600

    MAR-1400 Added layout for Banner section

--- client/components/OpenSource/Banner.vue
+++ client/components/OpenSource/Banner.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>banner</div>
+  <div class="banner">
+    <picture>
+      <source
+        :srcset="[$getMediaFromS3('/images/OpenSource/webp/banner.webp') + ' ', $getMediaFromS3('/images/OpenSource/webp/banner@2x.webp 2x')]"
+        type="image/webp"
+        class="banner__image media_lazy"
+      >
+      <img
+        :srcset="$getMediaFromS3('/images/OpenSource/jpg/banner@2x.jpg')"
+        :src="$getMediaFromS3('/images/OpenSource/jpg/banner.jpg')"
+        width="1680"
+        height="969"
+        class="banner__image media_lazy"
+        alt="Developer"
+      >
+    </picture>
+    <div class="container">
+      <div class="banner__content">
+        <h1 class="banner__title">
+          The power <br> of open source
+        </h1>
+        <p class="banner__subtitle">
+          70+ of Mad Devs' open source projects have been shared with the community via GitHub.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,4 +37,73 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/styles/_vars';
 
+.banner {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 62px;
+  height: 100vh;
+  min-height: 568px;
+  background: linear-gradient(180deg, rgba(17, 18, 19, 0) 75%, #111213 100%);
+  overflow: hidden;
+  &__image {
+    display: block;
+    z-index: -1;
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    object-fit: cover;
+    object-position: top;
+    opacity: 0.39;
+  }
+  &__content {
+    z-index: 2;
+    display: block;
+    margin: 0 auto;
+    max-width: 816px;
+    text-align: center;
+  }
+  &__title {
+    color: $text-color--white;
+    font-size: 100px;
+    line-height: 96px;
+    letter-spacing: -0.04em;
+  }
+  &__subtitle {
+    margin-top: 64px;
+    font-weight: 600;
+    font-size: 32px;
+    letter-spacing: -0.013em;
+    line-height: 44px;
+    color: $text-color--silver;
+  }
+
+  @media screen and (max-width: 1024px) {
+    &__title {
+      font-size: 80px;
+      line-height: 88px;
+    }
+    &__subtitle {
+      margin-top: 62px;
+      font-size: 24px;
+      line-height: 34px;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    &__title {
+      font-size: 52px;
+      line-height: 57px;
+    }
+    &__subtitle {
+      margin-top: 21px;
+      font-size: 21px;
+      line-height: 30px;
+    }
+  }
+}
 </style>
