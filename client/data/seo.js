9107e833c652b3b955f8e61aaec3a54a4da6e57f
Denisoed <denisoed@gmail.com>
2021-07-15 16:17:04 +0600

    MAR-1585 Added desctop banner for Ebook

--- client/data/seo.js
+++ client/data/seo.js
@@ -106,6 +106,12 @@ export const meta = {
     url: 'https://maddevs.io/open-source/',
     jsonLd: '{"@context": "http://www.schema.org", "@type": "Organization", "name": "Mad Devs Group LTD - Custom Software Development Company", "url": "https://maddevs.io", "logo": "https://maddevs.io/Open-Graph.png", "foundingDate": "2016", "email": "team@maddevs.io", "description": "Mad Devs Group LTD is a software development company headquartered in London, which specializes in developing highly scalable, enterprise-level software solutions. Since the year 2016, the Mad Devs Group LTD team has developed 50+ projects and online services for finance, transportation & logistics, security, edtech, and advertising industries. The company’s customers are based in the United States, Australia, United Kingdom, France, Germany, Switzerland, Austria, Poland, Vietnam, Singapore, Russia, and CIS countries.", "address": {"@type": "PostalAddress", "streetAddress": "27 Old Gloucester Street", "addressLocality": "London", "addressRegion": "UK", "postalCode": "WC1N3AX"}, "contactPoint": {"@type": "ContactPoint", "contactType": "customer service", "telephone": "+442039848555"}, "sameAs": ["https://www.facebook.com/maddevsllc", "http://www.linkedin.com/company/mad-devs", "https://blog.maddevs.io", "https://clutch.co/profile/mad-devs", "https://www.glassdoor.com/Overview/Working-at-Mad-Devs-EI_IE2507466.11,19.htm", "https://twitter.com/MadDevsIO", "https://www.instagram.com/maddevsio", "https://www.crunchbase.com/organization/mad-devs", "https://github.com/maddevsio", "https://www.behance.net/maddevs"]}',
   },
+  ebook: {
+    title: 'Ebook | Mad Devs',
+    description: '',
+    url: 'https://maddevs.io/ebook/',
+    jsonLd: '',
+  },
   ...deliveryModelsSeo,
   ...tagPagesSeo,
 }
---
+++ client/pages/ebook.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="main">
+    <Banner />
+  </main>
+</template>
+
+<script>
+import Banner from '@/components/Ebook/Banner'
+import { getMetadata, buildHead } from '@/data/seo'
+import initLazyLoadMixin from '@/mixins/initLazyLoadMixin'
+
+export default {
+  name: 'MainEbook',
+  components: {
+    Banner,
+  },
+
+  mixins: [initLazyLoadMixin],
+
+  head() {
+    return buildHead(getMetadata('ebook'))
+  },
+}
+</script>
