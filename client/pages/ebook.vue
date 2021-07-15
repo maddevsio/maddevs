f2550c4c6780accfa89d12c4853ef5531e860315
Denisoed <denisoed@gmail.com>
2021-07-15 16:30:30 +0600

    MAR-1585 added feature flag for ebook page

--- client/pages/ebook.vue
+++ client/pages/ebook.vue
@@ -9,6 +9,8 @@ import Banner from '@/components/Ebook/Banner'
 import { getMetadata, buildHead } from '@/data/seo'
 import initLazyLoadMixin from '@/mixins/initLazyLoadMixin'
 
+import featureFlag from '@/featureFlags/featureFlag'
+
 export default {
   name: 'MainEbook',
   components: {
@@ -16,6 +18,12 @@ export default {
   },
 
   mixins: [initLazyLoadMixin],
+
+  // NOTE: Remove if need show this page on production
+  asyncData({ error }) {
+    if (!featureFlag('ebookPage')) return error({ statusCode: 404, message: 'Page not found' })
+    return {}
+  },
 
   head() {
     return buildHead(getMetadata('ebook'))
